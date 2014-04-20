332d988cf50525d41400018a5481afbe69acab0d
julianguyen <tojulianguyen@gmail.com>
2014-04-20 00:42:36 -0400

    Adjustments for medication and alert controllers

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,3 +1,5 @@
 class Alert < ActiveRecord::Base
 	attr_accessible :userid, :trigger, :medication, :message, :means, :days, :times, :name
+
+    validates :means, inclusion: %w(sms email calendar)
 end
