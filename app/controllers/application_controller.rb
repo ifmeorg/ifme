007dc7d7cf7ad4944f7f393ef6fe36e70bedec39
julianguyen <tojulianguyen@gmail.com>
2014-04-24 00:22:01 -0400

    Fixed bug with name not being saved during registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,7 @@ class ApplicationController < ActionController::Base
 
   	def configure_permitted_parameters
   		devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation, :current_password) }
+
+  		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation, :current_password) }
 	end
 end
