bdb2004b011f0fa6ce0b44fcb647e7305558423e
julianguyen <tojulianguyen@gmail.com>
2014-05-12 14:15:20 -0400

    Trying to get images to show on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
 end
