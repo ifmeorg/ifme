9192335032dac356789f41c2e20e2bdcb3bfbede
julianguyen <tojulianguyen@gmail.com>
2015-05-05 17:08:17 -0700

    Added unit tests for strategy model

--- spec/models/strategy_spec.rb
+++ spec/models/strategy_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Strategy, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Strategy do
+ 	it "creates a strategy" do
+ 		new_user1 = create(:user1)
+ 		new_category = create(:category, userid: new_user1.id)
+ 		new_user2 = create(:user2)
+ 		new_strategy = create(:strategy, userid: new_user1.id, category: Array.new(1, new_category.id), viewers: Array.new(1, new_user2.id))
+	  	expect(Strategy.count).to eq(1)
+ 	end
+
+ 	it "does not create a strategy" do
+ 		new_user1 = create(:user1)
+ 		new_category = create(:category, userid: new_user1.id)
+ 		new_user2 = create(:user2)
+ 		new_strategy = build(:strategy, category: Array.new(1, new_category.id), viewers: Array.new(1, new_user2.id))
+	  	expect(new_strategy).to have(1).error_on(:userid)
+ 	end
 end
