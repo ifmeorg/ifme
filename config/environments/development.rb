39bbb18b25a8f63176716e17a4492a937cb13d0d
George Toothman <george.toothman@flatironschool.com>
2015-11-15 15:03:48 -0500

    add fix for errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.consider_all_requests_local = false
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
+  # config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
