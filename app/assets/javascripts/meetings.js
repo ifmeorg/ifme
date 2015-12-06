f2ba2fb800aa3f0772872a47eaf52347f4986129
julianguyen <tojulianguyen@gmail.com>
2015-12-05 19:29:38 -0800

    Cleaned up UI in all forms, fixed all empty field indicators

--- app/assets/javascripts/meetings.js
+++ app/assets/javascripts/meetings.js
@@ -1,5 +1,5 @@
 $(document).on("page:load ready", function() {
-	if ($('body').hasClass('meetings new') || $('body').hasClass('meetings edit')) {
+	if ($('body').hasClass('meetings new') || $('body').hasClass('meetings edit') || $('body').hasClass('meetings create') || || $('body').hasClass('meetings update')) {
 		console.log("Got here");
 		$('#meeting_date').datepicker();
 		//TODO: not working, need to find working gem
