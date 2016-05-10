5bd5138dca3b7ca9ed032c44246aa2c6ef607344
julianguyen <tojulianguyen@gmail.com>
2016-05-10 16:25:52 -0700

    Fixed responsive bugs in Medications form

--- app/assets/javascripts/medications.js
+++ app/assets/javascripts/medications.js
@@ -5,32 +5,33 @@ $(document).on("page:load ready", function() {
       return str.replace(/\b[a-z]/g, function(f){return f.toUpperCase()});
     }
 
-    $("#medication_name").autocomplete({
-      source: function (request, response) {
-        $.getJSON(
-          "http://dailymed.nlm.nih.gov/dailymed/services/v2/drugnames.json?drug_name=" + request.term,
-          function (data) {
-            var cleanedData = [];
-            _.each(data.data, function(dataItem) {
-              cleanedData.push(toTitleCase(dataItem.drug_name));
-            })
-            response(cleanedData);
-          });
-      },
-      minLength: 3,
-      select: function (event, ui) {
-        var selectedObj = ui.item;
-        $("#medication_name").val(selectedObj.value);
-        return false;
-      },
-      open: function () {
-        $(this).removeClass("ui-corner-all").addClass("ui-corner-top");
-      },
-      close: function () {
-        $(this).removeClass("ui-corner-top").addClass("ui-corner-all");
-      }
-    });
-    $("#medication_name").autocomplete("option", "delay", 100);
+    // Comment out for now because no longer working
+    // $("#medication_name").autocomplete({
+    //   source: function (request, response) {
+    //     $.getJSON(
+    //       "http://dailymed.nlm.nih.gov/dailymed/services/v2/drugnames.json?drug_name=" + request.term,
+    //       function (data) {
+    //         var cleanedData = [];
+    //         _.each(data.data, function(dataItem) {
+    //           cleanedData.push(toTitleCase(dataItem.drug_name));
+    //         })
+    //         response(cleanedData);
+    //       });
+    //   },
+    //   minLength: 3,
+    //   select: function (event, ui) {
+    //     var selectedObj = ui.item;
+    //     $("#medication_name").val(selectedObj.value);
+    //     return false;
+    //   },
+    //   open: function () {
+    //     $(this).removeClass("ui-corner-all").addClass("ui-corner-top");
+    //   },
+    //   close: function () {
+    //     $(this).removeClass("ui-corner-top").addClass("ui-corner-all");
+    //   }
+    // });
+    // $("#medication_name").autocomplete("option", "delay", 100);
     $("#medication_refill").datepicker();
   }
 });
