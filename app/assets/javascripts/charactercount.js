570c7131052820d08251c4129ff225249178d52c
julianguyen <tojulianguyen@gmail.com>
2016-05-12 12:05:33 -0700

    Added character count to quick create forms

--- app/assets/javascripts/charactercount.js
+++ app/assets/javascripts/charactercount.js
@@ -11,8 +11,36 @@ $(document).on("page:load ready", function() {
 	        if (remaining <= 0) {
 	        	$(editorCount).css('color', '#990019');
 	        } else {
-	        	$(editorCount).css('color', '#E1CDD7');
+	        	$(editorCount).css('color', '#b5839b');
 	        }
 	  	});
 	});
+
+	if ($('body').hasClass('pages home')) {
+		$('#moment_why').bind('keyup', function() {
+			noCKEditor($(this));
+		});
+	}
+
+	if ($('body').hasClass('moments create') ||
+		$('body').hasClass('moments new')) {
+		$('#strategy_description').bind('keyup', function() {
+			noCKEditor($(this));
+		});
+	}
 });
+
+function noCKEditor(editor) {
+	var editorName = editor.attr('id');
+	var editorData = editor.val();
+  	var editorCount = '#' + editorName + '_count';
+	var remaining = 2000 - editorData.length;
+
+    $(editorCount).html(remaining);
+
+    if (remaining <= 0) {
+    	$(editorCount).css('color', '#990019');
+    } else {
+    	$(editorCount).css('color', '#b5839b');
+    }
+}
