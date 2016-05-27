92ce76ec50ecf11e8975a58db594380e83fc6af3
julianguyen <tojulianguyen@gmail.com>
2016-05-27 12:59:40 -0700

    Added timepicker to Meetings form

--- app/assets/javascripts/meetings.js
+++ app/assets/javascripts/meetings.js
@@ -2,7 +2,6 @@ $(document).on("page:load ready", function() {
 	if ($('body').hasClass('meetings new') || $('body').hasClass('meetings edit') ||
 		$('body').hasClass('meetings create') || $('body').hasClass('meetings update')) {
 		$('#meeting_date').datepicker();
-		//TODO: not working, need to find working gem
-		//$('#meeting_time').timepicker();
+		$('#meeting_time').timepicker({ 'scrollDefault': 'now', 'step': 15 });
 	}
 });
