a80a1107d5f9c2550cb59c24187c750aac1bdbcf
Karen Kelley <karenkelley11@gmail.com>
2016-05-28 12:44:09 -0500

    Further refactoring groups/show by adding helpers
    * Cleaning up meetings loop
    * Changing t('groups.show.delete') to t('groups.leave_group') because
     the page was displaying 'Delete' because en.yml had two translations
    for delete, so Delete was being displayed instead of Leave Group

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,11 +1,21 @@
 module GroupsHelper
-  def user_is_leader_of?(group)
-    group.group_members.where(userid: current_user.id, leader: true).exists?
+  def user_is_leader_of?(object)
+    # object can be either group or meeting
+    object.leaders.include? current_user
+  end
+
+  def group_is_deletable?(group)
+    user_is_leader_of?(group) && group.group_members.count == 1
   end
 
   def edit_group_link(group)
     link_to t('groups.index.edit'), edit_group_path(group),
             class: 'small_margin_right'
+  end
+
+  def leader_link(leader)
+    name = leader == current_user ? t('.self') : leader.name
+    link_to name, profile_index_path(uid: leader.uid)
   end
 
   def delete_group_link(group, attrs = {})
@@ -25,8 +35,20 @@ module GroupsHelper
             { id: 'join' }.merge(attrs)
   end
 
+  def edit_meeting_link(meeting, html_options = {})
+    return unless user_is_leader_of? meeting
+    link_to edit_meeting_path(meeting), html_options do
+      raw '<i class="fa fa-pencil"></i>'
+    end
+  end
+
   def render_group_member_partial(members)
     render partial: '/notifications/members',
            locals: { data: members }
+  end
+
+  def render_meeting_partial(meeting)
+    render partial: '/shared/meeting_info',
+           locals: { meeting: meeting, is_leader: user_is_leader_of?(meeting) }
   end
 end
