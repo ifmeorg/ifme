c37d5b8d05b58304b8887f3f1ac8ec2dfa5e0b54
julianguyen <tojulianguyen@gmail.com>
2016-05-27 17:38:36 -0700

    Updated Error pages

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,11 @@
 class ErrorsController < ApplicationController
   def not_found
+  	@page_title = 'Error'
     render :status => 404
   end
 
   def internal_server_error
+  	@page_title = 'Error'
     render :status => 500
   end
 end
