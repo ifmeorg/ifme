594925d972f679255b782b92ad03f2bba12525d8
julianguyen <tojulianguyen@gmail.com>
2016-05-29 18:08:59 -0700

    Display allies list in alphabetical order

--- app/controllers/allies_controller.rb
+++ app/controllers/allies_controller.rb
@@ -5,9 +5,9 @@ class AlliesController < ApplicationController
   # GET /allies.json
   def index
     @page_search = true
-    @accepted_allies = current_user.allies_by_status(:accepted)
-    @incoming_ally_requests = current_user.allies_by_status(:pending_from_user)
-    @outgoing_ally_requests = current_user.allies_by_status(:pending_from_ally)
+    @accepted_allies = current_user.allies_by_status(:accepted).sort_by!{ |n| n.name.downcase }
+    @incoming_ally_requests = current_user.allies_by_status(:pending_from_user).sort_by!{ |n| n.name.downcase }
+    @outgoing_ally_requests = current_user.allies_by_status(:pending_from_ally).sort_by!{ |n| n.name.downcase }
     @page_title = "Allies"
   end
 
