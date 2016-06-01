40c496bfe4661250e0a13cdd53df764b0007bb3e
Karen Kelley <karenkelley11@gmail.com>
2016-06-01 14:06:53 -0500

    Adding associations to factories
    I think this is how you are supposed to handle associations in
    factories. Hardcoding an id might lead to issues later on.
    
    This change broke the tests in group_member and meeting_member, so I
    fixed those.

--- spec/models/meeting_member_spec.rb
+++ spec/models/meeting_member_spec.rb
@@ -13,16 +13,15 @@
 require 'rails_helper'
 
 describe MeetingMember do
- 	it "creates a meeting member" do
- 		new_group = create(:group, description: 'Test Description')
- 		new_meeting = create(:meeting, groupid: new_group.id, date: 'May 1, 2015')
- 		new_meeting_member = create(:meeting_member, meetingid: new_meeting.id, leader: true)
-	  	expect(MeetingMember.count).to eq(1)
- 	end
- 	it "does not create a meetin member" do
- 		new_group = create(:group, description: 'Test Description')
- 		new_meeting = create(:meeting, groupid: new_group.id, date: 'May 1, 2015')
- 		new_meeting_member = build(:meeting_member, leader: true)
-	  	expect(new_meeting_member).to have(1).error_on(:meetingid)
- 	end
+  it "has a valid factory" do
+    meeting_member = build :meeting_member
+    expect(meeting_member).to be_valid
+  end
+
+  context "when meetingid is nil" do
+    it "is invalid" do
+      meeting_member = build :meeting_member, meetingid: nil
+      expect(meeting_member).to have(1).error_on(:meetingid)
+    end
+  end
 end
