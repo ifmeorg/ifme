c44315ccfefe48ed42928db7bf4a165e04540493
Karen Kelley <karenkelley11@gmail.com>
2016-06-01 14:21:13 -0500

    Adding smoke tests
    Added smoke test for visiting groups index and meetings#show

---
+++ spec/features/user_visits_group_show_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "UserVisitsGroupsPages", type: :feature do
+  feature 'User vists groups page' do
+    scenario 'successfully' do
+      user = create :user1
+      login_as user
+      group = create :group_with_member, userid: user.id
+      meeting = create :meeting, groupid: group.id
+      create :meeting_member, userid: user.id, meetingid: meeting.id
+
+      visit group_path(group)
+
+      expect(page).to have_content group.name
+    end
+  end
+end
