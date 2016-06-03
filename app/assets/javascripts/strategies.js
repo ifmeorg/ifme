a5a4c87a6254df1bc7071c19e08d55f922da2f4a
piecedigital <piecedigitalstudios@gmail.com>
2016-06-02 23:45:02 -0400

    added self as contributor

--- app/assets/javascripts/strategies.js
+++ app/assets/javascripts/strategies.js
@@ -19,21 +19,15 @@ var onReadyStrategies = function() {
 
 	if (isShow(['strategies'])) {
 		$('#showTaggedMoments').click(function() {
-			$('#moment_tag_usage').removeClass('display_none');
-			$('#moment_tag_usage').addClass('display_block');
-			$('#showTaggedMoments').addClass('display_none');
-			$('#showTaggedMoments').removeClass('display_inline_block');
-			$('#hideTaggedMoments').removeClass('display_none');
-			$('#hideTaggedMoments').addClass('display_inline_block');
+			$('#moment_tag_usage').removeClass('display_none').addClass('display_block');
+			$('#showTaggedMoments').addClass('display_none').removeClass('display_inline_block');
+			$('#hideTaggedMoments').removeClass('display_none').addClass('display_inline_block');
 		});
 
 		$('#hideTaggedMoments').click(function() {
-			$('#moment_tag_usage').removeClass('display_block');
-			$('#moment_tag_usage').addClass('display_none');
-			$('#showTaggedMoments').removeClass('display_none');
-			$('#showTaggedMoments').addClass('display_inline_block');
-			$('#hideTaggedMoments').addClass('display_none');
-			$('#hideTaggedMoments').removeClass('display_inline_block');
+			$('#moment_tag_usage').removeClass('display_block').addClass('display_none');
+			$('#showTaggedMoments').removeClass('display_none').addClass('display_inline_block');
+			$('#hideTaggedMoments').addClass('display_none').removeClass('display_inline_block');
 		});
 	}
 }
