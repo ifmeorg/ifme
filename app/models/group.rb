ad7a7e01edac72e2ddee5e55512271da382bea64
Karen Kelley <karenkelley11@gmail.com>
2016-06-06 17:23:13 -0500

    Refactoring groups#destroy
    Adding dependent: :destroy associations so @group.destroy will destroy
    associated meeting and meeting_members

--- app/models/group.rb
+++ app/models/group.rb
@@ -12,10 +12,10 @@
 class Group < ActiveRecord::Base
   validates_presence_of :name, :description
 
-  has_many :group_members, foreign_key: :groupid
+  has_many :group_members, foreign_key: :groupid, dependent: :destroy
   has_many :members, -> { order 'name' }, through: :group_members, source: :user
-  has_many :meetings, -> { order 'created_at DESC' }, foreign_key: :groupid
-  has_many :meetings, foreign_key: :groupid
+  has_many :meetings, -> { order 'meetings.created_at DESC' },
+           foreign_key: :groupid, dependent: :destroy
   has_many :leaders, -> { where(group_members: { leader: true }) },
            through: :group_members, source: :user
 
