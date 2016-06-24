369d6508fa3424e3a45e220ded4f344626aebd65
Jennifer Sardina <jenni.sardina@gmail.com>
2016-06-24 09:20:14 -0400

    Add controller specs for AlliesController
    This commit:
      - [x] introduces test coverage for the three actions in the
    AlliesController (`:index`, `:add`, `:remove`).
      - [x] creates a Users factory
      - [x] introduces a pattern for shared_contexts and shared_examples
      - [x] adds `StubCurrentUserHelper` to RSpec configurations for easy
    accessibility.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,7 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, :type => :controller
-
+  config.include StubCurrentUserHelper
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
