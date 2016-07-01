7a22b5a845fa0629080407a68be9a7a1d013606d
Nicolette Chambers <nicolette3883@gmail.com>
2016-07-01 15:49:49 -0500

    #180 | Add one test to momentsSpec && refactor duplication in moments.js

--- app/assets/javascripts/moments.js
+++ app/assets/javascripts/moments.js
@@ -4,18 +4,25 @@ var onReadyMoments = function() {
 		var ALL_ALLIES = "Select all";
 		$('#viewers_label').text(ALL_ALLIES);
 		$('#viewers').change(function() {
-			if ($(this).is(":checked") && $('#viewers_label').text() == ALL_ALLIES) {
-				$(":checkbox[name='moment[viewers][]']").prop("checked", true);
-				$(":checkbox[id='viewers']").prop("checked", false);
+			if (isChecked($(this), ALL_ALLIES)) {
+         selectAuthorizedViewers(true);
 				$('#viewers_label').text(NO_ALLIES);
-			} else if ($(this).is(":checked") && $('#viewers_label').text() == NO_ALLIES) {
-				$(":checkbox[name='moment[viewers][]']").prop("checked", false);
-				$(":checkbox[id='viewers']").prop("checked", false);
+			} else if (isChecked($(this), NO_ALLIES)) {
+         selectAuthorizedViewers(false);
 				$('#viewers_label').text(ALL_ALLIES);
 				$(":checkbox[name='moment[comment]']").prop("checked", false);
 			}
 		});
 	}
+}
+
+function isChecked(input, allyStatus) {
+		return input.is(":checked") && $('#viewers_label').text() === allyStatus;
+	}
+
+function selectAuthorizedViewers(checkedFlag) {
+		$(":checkbox[name='moment[viewers][]']").prop("checked", checkedFlag);
+		$(":checkbox[id='viewers']").prop("checked", false);
 }
 
 $(document).on("page:load ready", onReadyMoments);
--- spec/javascripts/moments.spec.js
+++ spec/javascripts/moments.spec.js
@@ -1,26 +1,27 @@
 describe("Moments", function() {
   var NO_ALLIES;
   var ALL_ALLIES;
+  var newOrEdit;
 
   beforeAll(function() {
     var elements = [];
     elements.push("<div id='test_body'></div>");
     elements.push("<label id='viewers_label'></label>");
+    elements.push("<input type='checkbox' id='viewers'></input>");
     for (var i = 0; i < elements.length; i++) {
       $(document.body).append(elements[i]);
     }
 
     NO_ALLIES = "Unselect all";
     ALL_ALLIES = "Select all";
+    newOrEdit = spyOn(window, "newOrEdit");
   });
 
   it("test no functions called", function() {
-    var newOrEdit = spyOn(window, "newOrEdit");
     expect(newOrEdit).not.toHaveBeenCalled();
   });
 
 	it("test onReadyMoments to be called", function() {
-    var newOrEdit = spyOn(window, "newOrEdit");
     onReadyMoments();
     expect(newOrEdit).toHaveBeenCalled();
     expect($('#viewers_label').text()).toBe("");
@@ -28,15 +29,29 @@ describe("Moments", function() {
 
   it("test onReadyMoments and newOrEdit to be called", function() {
     $("body").addClass("moments new");
+    newOrEdit.and.returnValue(true);
+
     onReadyMoments();
+
     expect($('#viewers_label').text()).toBe(ALL_ALLIES);
-    onReadyMoments();
   });
 
   it("test newOeEdit in onReadyMoments to be called", function() {
-    var newOrEdit = spyOn(window, "newOrEdit");
     onReadyMoments();
+
     expect(newOrEdit).toHaveBeenCalled();
   });
+
+   it("has marked the check box when viewers_label text is ALL_ALLIES", function() {
+      var isChecked = spyOn(window, 'isChecked').and.returnValue(true);
+      var selectAuthorizedViewers = spyOn(window, 'selectAuthorizedViewers');
+      newOrEdit.and.returnValue = true;
+
+      onReadyMoments();
+      $('#viewers').change();
+
+      expect(selectAuthorizedViewers).toHaveBeenCalled();
+  });
+
 
 });
