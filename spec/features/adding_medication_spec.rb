34d990ab9834cc4ca8a381134fedd7192a279897
Andrew Schutt <schutt.andrew@gmail.com>
2016-07-19 23:14:00 -0500

    Fix tests to correctly test for adding google calendar medication reminder

--- spec/features/adding_medication_spec.rb
+++ spec/features/adding_medication_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 require "rails_helper"
 
 describe "user adds a new medication" do
-  let!(:user) { FactoryGirl.create(:user1) }
+  let!(:user) { FactoryGirl.create(:user_oauth) }
   before do
     login_as user
     visit new_medication_path
@@ -41,18 +41,20 @@ describe "user adds a new medication" do
     end
   end
 
-  context "and turns adds reminder to Google Calendar" do
-    it "creates a new medication with Google Calendar reminder" do
-      fill_in "Name", with: "A medication name"
-      fill_in "medication_comments", with: "A comment"
-      fill_in "Strength", with: 100
-      fill_in "Total", with: 30
-      fill_in "Dosage", with: 30
-      fill_in "Refill", with: "05/25/2016"
-      find(:css, "#medication_add_to_google_cal").set(true)
-      click_on "Create Medication"
-      expect(page).to have_conten("A medication name")
-      new_medication = user.medications.last
-    end
+  it "creates a new medication with Google Calendar reminder" do
+    fill_in "Name", with: "A medication name"
+    fill_in "medication_comments", with: "A comment"
+    fill_in "Strength", with: 100
+    fill_in "Total", with: 30
+    fill_in "Dosage", with: 30
+    fill_in "Refill", with: "05/25/2016"
+    find(:css, "#take_medication_reminder").set(true)
+    find(:css, "#refill_reminder").set(true)
+    find(:css, "#medication_add_to_google_cal").set(true)
+    click_on "Create Medication"
+    expect(page).to have_content("A medication name")
+    new_medication = user.medications.last
+    expect(new_medication.take_medication_reminder.active?).to eq(true)
+    expect(new_medication.refill_reminder.active?).to eq(true)
   end
 end
