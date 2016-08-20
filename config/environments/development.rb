61c6560c8baab1d79b3012bdd76c287220e8429f
Siena Aguayo <siena.aguayo@gmail.com>
2016-08-20 13:54:31 -0700

    raise exceptions for missing translations in test and dev, and fix indent of notifications strings in en.yml

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+  config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
