1efd0faca6b1707270684486b2eab0b1581dbd9d
julianguyen <tojulianguyen@gmail.com>
2016-09-09 21:37:00 -0700

    Cleaned up Contributors, Blog, and About pages
    - Cleaned up print_contributors and print_partners methods in Contributors page
    - Put Blog posts into an array
    - Added Linebreak grant to About page
    - Added Facebook link to footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,51 +19,74 @@ class PagesController < ApplicationController
 
   def contributors
     @contributors = [
-      ['Elizabeth Mitchell', 'https://github.com/emitche'],
-      ['William Horton', 'https://github.com/wdhorton'],
-      ['Bella Woo', 'https://github.com/bellawoo'],
-      ['Julie Pagano', 'https://github.com/juliapagano'],
-      ['Fiona Conn', 'https://github.com/fpcyan'],
-      ['Iuliia Kotlenko', 'https://github.com/IuliiaKot'],
-      ['Daniel Levenson', 'https://github.com/dleve123'],
-      ['Dawa Sherpa', 'https://github.com/Dawa12'],
-      ['Tien Yuan', 'https://github.com/tienyuan'],
-      ['chenghw', 'https://github.com/chenghw'],
-      ['Danny Burgoyne', 'https://github.com/dburgoyne'],
-      ['Jellene Khoh', 'https://github.com/jellene4eva'],
-      ['John Lim', 'https://github.com/jolim24601'],
-      ['Danny Glatstein', 'https://github.com/danielglatstein'],
-      ['Tess Gadwa', 'http://www.yesexactly.com'],
-      ['Mark Farrell', 'https://github.com/markfarrell'],
-      ['Laura Evans', 'https://github.com/lksevans12'],
-      ['Christopher De Borja', 'https://github.com/cdeborja'],
-      ['Dominic Prescod', 'https://github.com/dominicprescod'],
-      ['Noah Finnerman', 'https://github.com/nonothetoad'],
-      ['David Tomberlin', 'https://github.com/siyegen'],
-      ['Elana Hashman', 'https://github.com/ehashman'],
-      ['Caleb Thompson', 'https://github.com/calebthompson'],
-      ['Stella Cotton', 'https://github.com/stellacotton'],
-      ['Karen Kelley', 'https://github.com/kkelleey'],
-      ['Gwen Weston', 'https://github.com/gwengrid'],
-      ['Mark Freeman', 'http://markfreeman.ca'],
-      ['Catherine Vendryes', 'http://catherinevendryes.com'],
-      ['Michelle Liauw', 'https://github.com/techiemichelle'],
-      ['Kerry Benjamin', 'https://twitter.com/kerry_benjamin1'],
-      ['Sylvia Pereira', 'https://github.com/sylviapereira'],
-      ['Jonathan Garnaas-Holmes', 'https://github.com/finitem'],
-      ['Rashmi Agarwal', 'https://github.com/rashmiagar']
+      {name: 'Elizabeth Mitchell', link: 'https://github.com/emitche'},
+      {name: 'William Horton', link: 'https://github.com/wdhorton'},
+      {name: 'Bella Woo', link: 'https://github.com/bellawoo'},
+      {name: 'Julie Pagano', link: 'https://github.com/juliapagano'},
+      {name: 'Fiona Conn', link: 'https://github.com/fpcyan'},
+      {name: 'Iuliia Kotlenko', link: 'https://github.com/IuliiaKot'},
+      {name: 'Daniel Levenson', link: 'https://github.com/dleve123'},
+      {name: 'Dawa Sherpa', link: 'https://github.com/Dawa12'},
+      {name: 'Tien Yuan', link: 'https://github.com/tienyuan'},
+      {name: 'chenghw', link: 'https://github.com/chenghw'},
+      {name: 'Danny Burgoyne', link: 'https://github.com/dburgoyne'},
+      {name: 'Jellene Khoh', link: 'https://github.com/jellene4eva'},
+      {name: 'John Lim', link: 'https://github.com/jolim24601'},
+      {name: 'Danny Glatstein', link: 'https://github.com/danielglatstein'},
+      {name: 'Tess Gadwa', link: 'http://www.yesexactly.com'},
+      {name: 'Mark Farrell', link: 'https://github.com/markfarrell'},
+      {name: 'Laura Evans', link: 'https://github.com/lksevans12'},
+      {name: 'Christopher De Borja', link: 'https://github.com/cdeborja'},
+      {name: 'Dominic Prescod', link: 'https://github.com/dominicprescod'},
+      {name: 'Noah Finnerman', link: 'https://github.com/nonothetoad'},
+      {name: 'David Tomberlin', link: 'https://github.com/siyegen'},
+      {name: 'Elana Hashman', link: 'https://github.com/ehashman'},
+      {name: 'Caleb Thompson', link: 'https://github.com/calebthompson'},
+      {name: 'Stella Cotton', link: 'https://github.com/stellacotton'},
+      {name: 'Karen Kelley', link: 'https://github.com/kkelleey'},
+      {name: 'Gwen Weston', link: 'https://github.com/gwengrid'},
+      {name: 'Mark Freeman', link: 'http://markfreeman.ca'},
+      {name: 'Catherine Vendryes', link: 'http://catherinevendryes.com'},
+      {name: 'Michelle Liauw', link: 'https://github.com/techiemichelle'},
+      {name: 'Kerry Benjamin', link: 'https://twitter.com/kerry_benjamin1'},
+      {name: 'Sylvia Pereira', link: 'https://github.com/sylviapereira'},
+      {name: 'Jonathan Garnaas-Holmes', link: 'https://github.com/finitem'},
+      {name: 'Rashmi Agarwal', link: 'https://github.com/rashmiagar'},
+      {name: 'Nicolette Chambers', link: 'https://github.com/nchambe2'},
+      {name: 'Siena Aguayo', link: 'https://github.com/sienatime'},
+      {name: 'Łukasz Domański', link: 'https://github.com/maestromusica'},
+      {name: 'SashaTlr', link: 'https://github.com/SashaTlr'},
+      {name: 'Jon Friestedt', link: 'https://github.com/jfriestedt'},
+      {name: 'Andy Fry', link: 'https://github.com/andyfry01'}
     ]
 
+    @contributors.sort_by!{ |c| c[:name].downcase }
+
     @organizations = [
-      ['Hacker Hours', 'http://hackerhours.org', '/assets/partners/hacker_hours.png'],
-      ['Open Sourcing Mental Illness', 'https://osmihelp.org', '/assets/partners/osmi.png'],
-      ['Contributor Convenant', 'http://contributor-covenant.org', '/assets/partners/contributor_convenant.png'],
-      ['Everybody Has a Brain', 'http://everybodyhasabrain.com', '/assets/partners/everybody_has_a_brain.png'],
-      ['Brown Sisters Speak', 'http://brownsistersspeak.org', '/assets/partners/brown_sisters_speak.png'],
+      {name: 'Hacker Hours', link: 'http://hackerhours.org', image_link: '/assets/partners/hacker_hours.png'},
+      {name: 'Open Sourcing Mental Illness', link: 'https://osmihelp.org', image_link: '/assets/partners/osmi.png'},
+      {name: 'Contributor Convenant', link: 'http://contributor-covenant.org', image_link: '/assets/partners/contributor_convenant.png'},
+      {name: 'Everybody Has a Brain', link: 'http://everybodyhasabrain.com', image_link: '/assets/partners/everybody_has_a_brain.png'},
+      {name: 'Brown Sisters Speak', link: 'http://brownsistersspeak.org', image_link: '/assets/partners/brown_sisters_speak.png'},
     ]
+
+    @organizations.sort_by!{ |o| o[:name].downcase }
   end
 
   def blog
+    @posts = [
+      {link_name: 'Exploring options for help', link: 'http://julianguyen.org/exploring-options-for-help/', author: 'Julia Nguyen'},
+      {link_name: 'Thoughts on support groups for mental illness', link: 'http://julianguyen.org/thoughts-on-support-groups-for-mental-illness/', author: 'Julia Nguyen'},
+      {link_name: "Immigrants also have to challenge their own culture's mental health stigma", link: 'https://medium.com/aj-global-engagers/immigrants-also-have-to-challenge-their-own-culture-s-mental-health-stigmas-21944ee53f01', author: 'Julia Nguyen'},
+      {link_name: 'How do you ignore your OCD compulsions?', link: 'http://everybodyhasabrain.tumblr.com/post/117512957629/how-do-you-ignore-ocd-compulsions-its-sort-of', author: 'Mark Freeman, Everybody Has a Brain'},
+      {link_name: 'Nail biting and anxiety', link: 'http://everybodyhasabrain.tumblr.com/post/60429876832/i-dont-remember-exactly-when-i-started-to-bite-my', author: 'Daniela Pichardo, Everybody Has a Brain'},
+      {link_name: 'Opening up your beliefs', link: 'http://everybodyhasabrain.tumblr.com/post/111290701414/beliefs-tend-to-change-when-theyre-brought-out', author: 'Matt Schroeter, Everybody Has a Brain'},
+      {link_name: 'Undercover Mental Patient Survival Guide', link: 'https://medium.com/@rosecheval_74352/stigma-equals-death-7a7d94b638ae#.bor3nudib', author: 'Rose Cheval'},
+      {link_name: 'On Depression', link: 'https://medium.com/invisible-illness/on-depression-33b3ebf2c7e1#.3l6rjzmb4', author: 'Eloisa Guerrero'},
+      {link_name: 'Speaking Her Truth: Q& A with Julia Nguyen founder of if me', link: 'http://www.huffingtonpost.com/entry/speaking-her-truth-q-a-with-julia-nguyen-founder_us_57d2bbd3e4b0f831f7071b1e', author: 'Myisha T. Hill, Brown Sisters Speak'}
+    ]
+
+    @posts.reverse!
   end
 
   def privacy
@@ -73,21 +96,16 @@ class PagesController < ApplicationController
   end
 
   def print_contributors(data)
-    first_element = 0
     return_this = ''
-    data.each do |d|
-      name = d[0]
-      link = d[1]
-      if d.kind_of?(Array) && name.kind_of?(String) && link.kind_of?(String)
-        first_element = first_element + 1
-        if first_element == 1
-          return_this = link_to name, link
-        else
+    data.each_with_index do |d, index|
+      if d.kind_of?(Hash) && d[:name].kind_of?(String) && d[:link].kind_of?(String)
+        if data.length > 2 && index + 1 == data.length
+          return_this += ", and "
+        elsif index != 0
           return_this += ", "
-          return_this += link_to name, link
         end
+        return_this += link_to d[:name], d[:link], target: 'blank'
       else
-        return_this = ''
         break
       end
     end
@@ -96,22 +114,19 @@ class PagesController < ApplicationController
   end
 
   def print_partners(data)
-    last_element = 0
     return_this = ''
-    data.each do |d|
-      name = d[0]
-      link = d[1]
-      image_link = d[2]
-      if d.kind_of?(Array) && name.kind_of?(String) && link.kind_of?(String) && image_link.kind_of?(String)
-        image = image_tag(image_link, alt: name)
+    data.each_with_index do |d, index|
+      if d.kind_of?(Hash) && d[:name].kind_of?(String) && d[:link].kind_of?(String) && d[:image_link].kind_of?(String)
+        image = image_tag(d[:image_link], alt: d[:name])
         return_this += '<div class="partner">'
-        return_this += link_to image, link, target: 'blank'
+        return_this += link_to image, d[:link], target: 'blank'
         return_this += '</div>'
 
-        last_element = last_element + 1
-        if last_element != data.length
+        if index + 1 != data.length
           return_this += '<div class="spacer"></div>'
         end
+      else
+        break
       end
     end
 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,10 +8,13 @@ describe PagesController do
 			expect(controller.print_contributors([['test']])).to eq('')
 		end
 		it "returns correct result for one link" do
-			expect(controller.print_contributors([['test', 'http://if-me.org']])).to eq('<a href="http://if-me.org">test</a>')
+			expect(controller.print_contributors([{name: 'test', link: 'http://if-me.org'}])).to eq('<a target="blank" href="http://if-me.org">test</a>')
 		end
 		it "returns correct result for two links" do
-			expect(controller.print_contributors([['test1', 'http://if-me.org'], ['test2', 'http://if-me.org']])).to eq('<a href="http://if-me.org">test1</a>, <a href="http://if-me.org">test2</a>')
+			expect(controller.print_contributors([{name: 'test1', link: 'http://if-me.org'}, {name: 'test2', link: 'http://if-me.org'}])).to eq('<a target="blank" href="http://if-me.org">test1</a>, <a target="blank" href="http://if-me.org">test2</a>')
+		end
+		it "returns correct result for three links" do
+			expect(controller.print_contributors([{name: 'test1', link: 'http://if-me.org'}, {name: 'test2', link: 'http://if-me.org'}, {name: 'test3', link: 'http://if-me.org'}])).to eq('<a target="blank" href="http://if-me.org">test1</a>, <a target="blank" href="http://if-me.org">test2</a>, and <a target="blank" href="http://if-me.org">test3</a>')
 		end
 	end
 
@@ -22,13 +25,13 @@ describe PagesController do
 		it "returns empty result for malformed array" do
 			expect(controller.print_partners(['test'])).to eq('')
 			expect(controller.print_partners([['test']])).to eq('')
-			expect(controller.print_partners([['test', 'http://if-me.org']])).to eq('')
+			expect(controller.print_partners([{name: 'test', link: 'http://if-me.org'}])).to eq('')
 		end
 		it "returns correct result for one link" do
-			expect(controller.print_partners([['test', 'http://if-me.org', 'test.png']])).to eq('<div class="partner"><a target="blank" href="http://if-me.org"><img alt="test" src="/images/test.png" /></a></div>')
+			expect(controller.print_partners([{name: 'test', link: 'http://if-me.org', image_link: 'test.png'}])).to eq('<div class="partner"><a target="blank" href="http://if-me.org"><img alt="test" src="/images/test.png" /></a></div>')
 		end
 		it "returns correct result for two links" do
-			expect(controller.print_partners([['test1', 'http://if-me.org', 'test1.png'], ['test2', 'http://if-me.org', 'test1.png']])).to eq('<div class="partner"><a target="blank" href="http://if-me.org"><img alt="test1" src="/images/test1.png" /></a></div><div class="spacer"></div><div class="partner"><a target="blank" href="http://if-me.org"><img alt="test2" src="/images/test1.png" /></a></div>')
+			expect(controller.print_partners([{name: 'test1', link: 'http://if-me.org', image_link: 'test1.png'}, {name: 'test2', link: 'http://if-me.org', image_link: 'test1.png'}])).to eq('<div class="partner"><a target="blank" href="http://if-me.org"><img alt="test1" src="/images/test1.png" /></a></div><div class="spacer"></div><div class="partner"><a target="blank" href="http://if-me.org"><img alt="test2" src="/images/test1.png" /></a></div>')
 		end
 	end
 end
