8f4421b4020cc5624dc63d03978957021aed6f1b
NAGMA KAPOOR <nagmakapoor@gmail.com>
2016-09-11 22:59:08 -0400

    Created a separate readMoreShow function for jasmine test

--- app/assets/javascripts/contributors_readmore.js
+++ app/assets/javascripts/contributors_readmore.js
@@ -1,25 +1,4 @@
-var contributorReadMoreFeature = (function(){
-    var profileTextLength = 0;
-	const maxProfileLength = 250;
-	var visibleProfileText, invisibleProfileText;
-
-	$('.contributor-profile').each(function(i){
-		profileTextLength = $(this).text().length;
-
-        if (profileTextLength > maxProfileLength){
-            visibleProfileText = $(this).text().substr(0, maxProfileLength);
-            invisibleProfileText = $(this).text().substr(maxProfileLength, profileTextLength - maxProfileLength);
-
-            var profileContent = visibleProfileText + '<a href="#" class="read-more-show hide">...</a><span class="read-more-content">' 
-                + invisibleProfileText + '</span>';
-            
-            $(this).html(profileContent);
-
-            $('.read-more-content').addClass('hide')
-            $('.read-more-show, .read-more-hide').removeClass('hide')
-        }
-	});
-
+var readMoreShow = (function(){
     $('.read-more-show').on('click', function(e){
         $(this).fadeOut(400, function(){
             $(this).next('.read-more-content').removeClass('hide').slideDown(1000);
@@ -29,5 +8,31 @@ var contributorReadMoreFeature = (function(){
     });
 });
 
+var contributorReadMoreFeature = (function(){
+    
+        var profileTextLength = 0;
+    	const maxProfileLength = 250;
+    	var visibleProfileText, invisibleProfileText;
+        var contributorProfle = $('.contributor-profile');
+
+    	contributorProfle.each(function(i){
+    		profileTextLength = $(this).text().length;
+
+            if (profileTextLength > maxProfileLength){
+                visibleProfileText = $(this).text().substr(0, maxProfileLength);
+                invisibleProfileText = $(this).text().substr(maxProfileLength, profileTextLength - maxProfileLength);
+
+                var profileContent = visibleProfileText + '<a href="#" class="read-more-show hide">...</a><span class="read-more-content">' 
+                    + invisibleProfileText + '</span>';
+                
+                $(this).html(profileContent);
+
+                $('.read-more-content').addClass('hide')
+                $('.read-more-show, .read-more-hide').removeClass('hide')
+            }
+    	});
+
+        readMoreShow();        
+});
 
 $(document).on("page:load ready", contributorReadMoreFeature);
