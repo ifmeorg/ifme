2d9158e5b608e795833d9b9ffd9e43c17cc8e0d2
Necrostic <damiong28@gmail.com>
2016-09-24 14:52:44 +0000

    Display leader names correctlyon group show page

--- spec/features/user_visits_group_show_spec.rb
+++ spec/features/user_visits_group_show_spec.rb
@@ -2,15 +2,18 @@ RSpec.feature "UserVisitsGroupsPages", type: :feature do
   feature 'User vists groups page' do
     scenario 'successfully' do
       user = create :user1
+      leader = create :user2
       login_as user
       group = create :group_with_member, userid: user.id
       meeting = create :meeting, groupid: group.id
       create :meeting_member, userid: user.id, meetingid: meeting.id
+      create :group_leader, userid: leader.id, groupid: group.id
 
       visit group_path(group)
 
       expect(page.title).to match group.name
       expect(page).to have_content group.name
+      expect(page).to have_content "Led by: #{leader.name}"
     end
   end
 end
