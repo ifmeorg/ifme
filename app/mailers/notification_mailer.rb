37df2634be17e156045aca9e54c0f3b803304708
Chris <chrisrp@inventati.org>
2016-09-26 17:50:41 -0300

    adding tests and little refactoring

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,166 +1,161 @@
 class NotificationMailer < ApplicationMailer
    default from: ENV["DEFAULT_FROM_ADDRESS"]
 
+   ALLY_NOTIFY_TYPES = ["new_ally_request", "accepted_ally_request"]
+
   def take_medication(reminder)
     @medication = reminder.medication
     @user = @medication.user
-    mail(
-      to: @user.email,
-      subject: "Don't forget to take #{@medication.name}!"
-    )
+    mail(to: @user.email,
+         subject: "Don't forget to take #{@medication.name}!")
   end
 
   def refill_medication(reminder)
     @medication = reminder.medication
     @user = @medication.user
-    mail(
-      to: @user.email,
-      subject: "Your refill for #{@medication.name} is coming up soon!"
-    )
+    mail(to: @user.email,
+         subject: "Your refill for #{@medication.name} is coming up soon!")
   end
 
-
   def notification_email(recipientid, data)
-		@data = JSON.parse(data)
-		@recipient = User.where(id: recipientid).first
-		subject = 'if me | '
+    @data = JSON.parse(data)
+    @recipient = User.where(id: recipientid).first
+    subject = 'if me | '
 
-		if (can_notify(@recipient, 'comment_notify') &&
-			(@data['type'] == 'comment_on_moment' ||
-			@data['type'] == 'comment_on_moment_private' ||
-			@data['type'] == 'comment_on_strategy' ||
-			@data['type'] == 'comment_on_strategy_private' ||
-			@data['type'] == 'comment_on_meeting'))
+    if (can_notify(@recipient, 'comment_notify') &&
+        (@data['type'] == 'comment_on_moment' ||
+         @data['type'] == 'comment_on_moment_private' ||
+         @data['type'] == 'comment_on_strategy' ||
+         @data['type'] == 'comment_on_strategy_private' ||
+         @data['type'] == 'comment_on_meeting'))
 
-			if (@data['type'] == 'comment_on_moment' || @data['type'] == 'comment_on_moment_private')
-				subject += @data['user'].to_s + ' commented on your moment "' + @data['moment'].to_s + '"'
-			elsif @data['type'] == 'comment_on_strategy' || @data['type'] == 'comment_on_strategy_private'
-				subject += @data['user'].to_s + ' commented on your strategy "' + @data['strategy'].to_s + '"'
-			else
-				groupid = Meeting.where(id: @data['meetingid']).first.groupid
-				group = Group.where(id: groupid).first.name
-				subject += @data['user'].to_s + ' commented on the meeting "' + @data['meeting'].to_s + '" in the group "' + group.to_s + '"'
-			end
+      if (@data['type'] == 'comment_on_moment' || @data['type'] == 'comment_on_moment_private')
+        subject += @data['user'].to_s + ' commented on your moment "' + @data['moment'].to_s + '"'
+      elsif @data['type'] == 'comment_on_strategy' || @data['type'] == 'comment_on_strategy_private'
+        subject += @data['user'].to_s + ' commented on your strategy "' + @data['strategy'].to_s + '"'
+      else
+        groupid = Meeting.where(id: @data['meetingid']).first.groupid
+        group = Group.where(id: groupid).first.name
+        subject += @data['user'].to_s + ' commented on the meeting "' + @data['meeting'].to_s + '" in the group "' + group.to_s + '"'
+      end
 
-			if @data['type'] == 'comment_on_moment_private' || @data['type'] == 'comment_on_strategy_private'
-				@message = '<p>Your ally <strong>' + @data['user'].to_s + '</strong> commented privately:</p>';
-			else
-				@message = '<p>Your ally <strong>' + @data['user'].to_s + '</strong> commented:</p>';
-			end
+      if @data['type'] == 'comment_on_moment_private' || @data['type'] == 'comment_on_strategy_private'
+        @message = '<p>Your ally <strong>' + @data['user'].to_s + '</strong> commented privately:</p>';
+      else
+        @message = '<p>Your ally <strong>' + @data['user'].to_s + '</strong> commented:</p>';
+      end
 
-			if @data['cutoff']
-				@message += '<p><i>' + @data['comment'].to_s + ' [...]</i></p>'
-			else
-				@message += '<p><i>' + @data['comment'].to_s + '</i></p>'
-			end
+      if @data['cutoff']
+        @message += '<p><i>' + @data['comment'].to_s + ' [...]</i></p>'
+      else
+        @message += '<p><i>' + @data['comment'].to_s + '</i></p>'
+      end
 
-			if (@data['type'] == 'comment_on_moment' || @data['type'] == 'comment_on_moment_private')
-				link = link_to("here", moment_url(@data['momentid']))
-				@message += '<p>You can read it all ' + link + '!</p>'
-			elsif @data['type'] == 'comment_on_strategy' || @data['type'] == 'comment_on_strategy_private'
-				link = link_to("here", strategy_url(@data['strategyid']))
-				@message += '<p>You can read it all ' + link + '!</p>'
-			else
-				link = link_to("here", meeting_url(@data['meetingid']))
-				@message += '<p>You can read it all ' + link + '!</p>'
-			end
+      if (@data['type'] == 'comment_on_moment' || @data['type'] == 'comment_on_moment_private')
+        link = link_to("here", moment_url(@data['momentid']))
+        @message += '<p>You can read it all ' + link + '!</p>'
+      elsif @data['type'] == 'comment_on_strategy' || @data['type'] == 'comment_on_strategy_private'
+        link = link_to("here", strategy_url(@data['strategyid']))
+        @message += '<p>You can read it all ' + link + '!</p>'
+      else
+        link = link_to("here", meeting_url(@data['meetingid']))
+        @message += '<p>You can read it all ' + link + '!</p>'
+      end
 
-			mail(to: @recipient.email, subject: subject)
-	    elsif (can_notify(@recipient, 'ally_notify') &&
-	    		(@data['type'] == 'accepted_ally_request' ||
-	    		@data['type'] == 'new_ally_request'))
+      mail(to: @recipient.email, subject: subject)
+    elsif (can_notify(@recipient, 'ally_notify') && ALLY_NOTIFY_TYPES.include?(@data['type']))
 
-	    	if  @data['type'] == 'accepted_ally_request'
-	    		subject += @data['user'].to_s + ' accepted your ally request!'
-	    		@message = '<p>Congrats! You can now share Moments, Strategies, and more with ' + @data['user'].to_s + '.</p>'
-	    	else
-	    		subject += @data['user'].to_s + ' sent an ally request!'
-	    		link = link_to("sign in", allies_url)
-	    		@message = '<p>Please ' + link + ' to accept or reject the request!</p>'
-	    	end
+      if  @data['type'] == 'accepted_ally_request'
+        subject += @data['user'].to_s + ' accepted your ally request!'
+        @message = '<p>Congrats! You can now share Moments, Strategies, and more with ' + @data['user'].to_s + '.</p>'
+      else
+        subject += @data['user'].to_s + ' sent an ally request!'
+        link = link_to("sign in", allies_url)
+        @message = '<p>Please ' + link + ' to accept or reject the request!</p>'
+      end
 
-	    	mail(to: @recipient.email, subject: subject)
-	    elsif ((can_notify(@recipient, 'group_notify') &&
-	    		(@data['type'] == 'new_group' ||
-	      		@data['type'] == 'new_group_member' ||
-	      		@data['type'] == 'add_group_leader' ||
-	      		@data['type'] == 'remove_group_leader')) ||
-	    	(can_notify(@recipient, 'meeting_notify') &&
-	      		(@data['type'] == 'new_meeting' ||
-	      		@data['type'] == 'remove_meeting' ||
-	      		@data['type'] == 'update_meeting' ||
-	      		@data['type'] == 'join_meeting')))
+      mail(to: @recipient.email, subject: subject)
+    elsif ((can_notify(@recipient, 'group_notify') &&
+            (@data['type'] == 'new_group' ||
+             @data['type'] == 'new_group_member' ||
+             @data['type'] == 'add_group_leader' ||
+             @data['type'] == 'remove_group_leader')) ||
+      (can_notify(@recipient, 'meeting_notify') &&
+       (@data['type'] == 'new_meeting' ||
+        @data['type'] == 'remove_meeting' ||
+        @data['type'] == 'update_meeting' ||
+        @data['type'] == 'join_meeting')))
 
-	    	if @data['type'] == 'new_group'
-	    		what = @data['user'].to_s + ' created a group "' + @data['group'].to_s + '"'
-       	 		subject += what
-       	 		group_description = Group.where(id: @data['groupid']).first.description
-       	 		link = link_to("click here", group_url(@data['groupid']))
-       	 		@message = '<p>' + what + ':</p>'
-       	 		@message += '<p><i>' + group_description.to_s + '</i></p>'
-       	 		@message += '<p>To learn more and join, ' + link + '!</p>'
+      if @data['type'] == 'new_group'
+        what = @data['user'].to_s + ' created a group "' + @data['group'].to_s + '"'
+        subject += what
+        group_description = Group.where(id: @data['groupid']).first.description
+        link = link_to("click here", group_url(@data['groupid']))
+        @message = '<p>' + what + ':</p>'
+        @message += '<p><i>' + group_description.to_s + '</i></p>'
+        @message += '<p>To learn more and join, ' + link + '!</p>'
 
-		  	elsif @data['type'] == 'new_group_member'
-		  		subject += @data['user'].to_s + ' joined your group "' + @data['group'].to_s + '"'
-		    	link = link_to("click here", group_url(@data['groupid']))
-       	 		@message = '<p>To see ' + @data['group'].to_s + ', ' + link + '!</p>'
+      elsif @data['type'] == 'new_group_member'
+        subject += @data['user'].to_s + ' joined your group "' + @data['group'].to_s + '"'
+        link = link_to("click here", group_url(@data['groupid']))
+        @message = '<p>To see ' + @data['group'].to_s + ', ' + link + '!</p>'
 
-		  	elsif @data['type'] == 'add_group_leader'
-		  		if @recipient.name == @data['user']
-		  			subject += 'You became a leader of "' + @data['group'].to_s + '"'
-		  		else
-		    		subject += @data['user'].to_s + ' became a leader of "' + @data['group'].to_s + '"'
-		    	end
-		    	link = link_to("click here", group_url(@data['groupid']))
-       	 		@message = '<p>To see ' + @data['group'].to_s + ', ' + link + '! </p>'
+      elsif @data['type'] == 'add_group_leader'
+        if @recipient.name == @data['user']
+          subject += 'You became a leader of "' + @data['group'].to_s + '"'
+        else
+          subject += @data['user'].to_s + ' became a leader of "' + @data['group'].to_s + '"'
+        end
+        link = link_to("click here", group_url(@data['groupid']))
+        @message = '<p>To see ' + @data['group'].to_s + ', ' + link + '! </p>'
 
-		  	elsif @data['type'] == 'remove_group_leader'
-		  		if @recipient.name == @data['user']
-		  			subject += 'You are no longer a leader of "' + @data['group'].to_s + '"'
-		  		else
-		    		subject += @data['user'].to_s + ' is no longer a leader of "' + @data['group'].to_s + '"'
-		    	end
-		    	link = link_to("click here", group_url(@data['groupid']))
-       	 		@message = '<p>To see ' + @data['group'].to_s + ', ' + link + '!</p>'
+      elsif @data['type'] == 'remove_group_leader'
+        if @recipient.name == @data['user']
+          subject += 'You are no longer a leader of "' + @data['group'].to_s + '"'
+        else
+          subject += @data['user'].to_s + ' is no longer a leader of "' + @data['group'].to_s + '"'
+        end
+        link = link_to("click here", group_url(@data['groupid']))
+        @message = '<p>To see ' + @data['group'].to_s + ', ' + link + '!</p>'
 
-		  	elsif @data['type'] == 'new_meeting' || @data['type'] == 'update_meeting'
-		  		if @data['type'] == 'update_meeting'
-		  			what = @data['user'].to_s + ' has updated the meeting "' + @data['meeting'].to_s + '" for "' + @data['group'].to_s + '"'
-		  		else
-		    		what = @data['user'].to_s + ' created a new meeting "' + @data['meeting'].to_s + '" for "' + @data['group'].to_s + '"'
-		    	end
-		    	subject += what
+      elsif @data['type'] == 'new_meeting' || @data['type'] == 'update_meeting'
+        if @data['type'] == 'update_meeting'
+          what = @data['user'].to_s + ' has updated the meeting "' + @data['meeting'].to_s + '" for "' + @data['group'].to_s + '"'
+        else
+          what = @data['user'].to_s + ' created a new meeting "' + @data['meeting'].to_s + '" for "' + @data['group'].to_s + '"'
+        end
+        subject += what
 
-		    	meeting_description = Meeting.where(id: @data['meetingid']).first.description
-		    	meeting_location = Meeting.where(id: @data['meetingid']).first.location
-		    	meeting_date = Meeting.where(id: @data['meetingid']).first.date
-		    	meeting_time = Meeting.where(id: @data['meetingid']).first.time
-       	 		link = link_to("click here", meeting_url(@data['meetingid']))
-       	 		@message = '<p>' + what + ':</p>'
-       	 		@message += '<p><i>' + meeting_description.to_s + '</i></p>'
-       	 		@message += '<p><strong>Location:</strong> ' + meeting_location.to_s + '</p>'
-       	 		@message += '<p><strong>Date:</strong> ' + meeting_date.to_s + '</p>'
-       	 		@message += '<p><strong>Time:</strong> ' + meeting_time.to_s + '</p>'
+        meeting_description = Meeting.where(id: @data['meetingid']).first.description
+        meeting_location = Meeting.where(id: @data['meetingid']).first.location
+        meeting_date = Meeting.where(id: @data['meetingid']).first.date
+        meeting_time = Meeting.where(id: @data['meetingid']).first.time
+        link = link_to("click here", meeting_url(@data['meetingid']))
+        @message = '<p>' + what + ':</p>'
+        @message += '<p><i>' + meeting_description.to_s + '</i></p>'
+        @message += '<p><strong>Location:</strong> ' + meeting_location.to_s + '</p>'
+        @message += '<p><strong>Date:</strong> ' + meeting_date.to_s + '</p>'
+        @message += '<p><strong>Time:</strong> ' + meeting_time.to_s + '</p>'
 
-       	 		if @data['type'] == 'new_meeting'
-       	 			@message += '<p>To learn more and attend, ' + link + '!</p>'
-       	 		else
-       	 			@message += '<p>To learn more, ' + link + '!</p>'
-       	 		end
+        if @data['type'] == 'new_meeting'
+          @message += '<p>To learn more and attend, ' + link + '!</p>'
+        else
+          @message += '<p>To learn more, ' + link + '!</p>'
+        end
 
-		  	elsif @data['type'] == 'remove_meeting'
-		    	subject += @data['user'].to_s + ' has cancelled "' + @data['meeting'].to_s + '" for "' + @data['group'].to_s + '"'
-		    	link = link_to("click here", group_url(@data['groupid']))
-       	 		@message = '<p>To see ' + @data['group'].to_s + ', ' + link + '!</p>'
+      elsif @data['type'] == 'remove_meeting'
+        subject += @data['user'].to_s + ' has cancelled "' + @data['meeting'].to_s + '" for "' + @data['group'].to_s + '"'
+        link = link_to("click here", group_url(@data['groupid']))
+        @message = '<p>To see ' + @data['group'].to_s + ', ' + link + '!</p>'
 
-		  	elsif @data['type'] == 'join_meeting'
-		    	subject += @data['user'].to_s + ' has joined "' + @data['meeting'].to_s + '" for "' + @data['group'].to_s + '"'
-		    	link = link_to("click here", meeting_url(@data['meetingid']))
-       	 		@message = '<p>To see ' + @data['meeting'].to_s + ', ' + link + '!</p>'
+      elsif @data['type'] == 'join_meeting'
+        subject += @data['user'].to_s + ' has joined "' + @data['meeting'].to_s + '" for "' + @data['group'].to_s + '"'
+        link = link_to("click here", meeting_url(@data['meetingid']))
+        @message = '<p>To see ' + @data['meeting'].to_s + ', ' + link + '!</p>'
 
-		  	end
+      end
 
-	    	mail(to: @recipient.email, subject: subject)
-	    end
-  	end
+      mail(to: @recipient.email, subject: subject)
+    end
+  end
 end
--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -1,27 +1,51 @@
 require "spec_helper"
 
 describe "NotificationMailer" do
-  describe "#take_medication" do
-    let(:recipient) { FactoryGirl.create(:user1, email: "some@user.com") }
-    let!(:medication) { FactoryGirl.create(:medication, userid: recipient.id) }
-    let!(:reminder) { FactoryGirl.create(:take_medication_reminder, medication_id: medication.id) }
-    let(:mail) { NotificationMailer.take_medication(reminder) }
+  let(:recipient)  { FactoryGirl.create(:user1, email: "some@user.com") }
+  let(:medication) { FactoryGirl.create(:medication, userid: recipient.id) }
+  let(:reminder)   { FactoryGirl.create(:take_medication_reminder, medication_id: medication.id) }
 
-    it "sends a reminder to a user" do
-      expect(mail.to).to eq(["some@user.com"])
-      expect(mail.subject).to eq("Don't forget to take Fancy Medication Name!")
-    end
+  describe "#take_medication" do
+    subject(:email) { NotificationMailer.take_medication(reminder) }
+
+    it { expect(email.to).to eq(["some@user.com"]) }
+    it { expect(email.subject).to eq("Don't forget to take Fancy Medication Name!") }
   end
 
   describe "#refill_medication" do
-    let(:recipient) { FactoryGirl.create(:user1, email: "some@user.com") }
-    let!(:medication) { FactoryGirl.create(:medication, userid: recipient.id) }
-    let!(:reminder) { FactoryGirl.create(:take_medication_reminder, medication_id: medication.id) }
-    let(:mail) { NotificationMailer.refill_medication(reminder) }
+    subject(:email) { NotificationMailer.refill_medication(reminder) }
 
-    it "sends a reminder to a user" do
-      expect(mail.to).to eq(["some@user.com"])
-      expect(mail.subject).to eq("Your refill for Fancy Medication Name is coming up soon!")
+    it { expect(email.to).to eq(["some@user.com"]) }
+    it { expect(email.subject).to eq("Your refill for Fancy Medication Name is coming up soon!") }
+  end
+
+  describe 'notification' do
+    let(:who_triggered_event) { FactoryGirl.create(:user2) }
+
+    let(:data) do
+      JSON.generate(user: who_triggered_event.name,
+                    userid: who_triggered_event.id,
+                    uid: who_triggered_event.uid,
+                    type: type,
+                    uniqueid: 'some_unique_id')
+    end
+
+    context 'when type is accepted_ally_request' do
+      let(:type) { 'accepted_ally_request' }
+
+      subject(:email) { NotificationMailer.notification_email(recipient, data) }
+
+      it { expect(email.subject).to eq("if me | #{who_triggered_event.name} accepted your ally request!") }
+      it { expect(email.body.encoded).to match("<p>Congrats! You can now share Moments, Strategies, and more with #{who_triggered_event.name}.</p>") }
+    end
+
+    context 'when type is new_ally_request' do
+      let(:type) { 'new_ally_request' }
+
+      subject(:email) { NotificationMailer.notification_email(recipient, data) }
+
+      it { expect(email.subject).to eq("if me | #{who_triggered_event.name} sent an ally request!") }
+      it { expect(email.body.encoded).to match("<p>Please <a href=\"http://localhost:3000/allies\">sign in</a> to accept or reject the request!</p>") }
     end
   end
 end
