5f8a48f57f59acdbf7336fcf2a24e7f29f6a731d
julianguyen <tojulianguyen@gmail.com>
2016-10-02 17:19:30 -0700

    Add contributor photos to home page and create separate partners page

--- config/routes.rb
+++ config/routes.rb
@@ -80,6 +80,7 @@ Rails.application.routes.draw do
   get 'pages/home'
   match 'about', to: 'pages#about', via: :get
   match 'contributors', to: 'pages#contributors', via: :get
+  match 'partners', to: 'pages#partners', via: :get
   match 'blog', to: 'pages#blog', via: :get
   match 'privacy', to: 'pages#privacy', via: :get
   match 'faq', to: 'pages#faq', via: :get
