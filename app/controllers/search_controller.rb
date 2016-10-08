f2657999d6c7ee638bfe473c75b4eab927e0714a
Bruno Casali <brunoocasali@gmail.com>
2016-10-08 17:52:38 -0300

    Green stage to the first part of test

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,60 +1,60 @@
 class SearchController < ApplicationController
-	before_filter :if_not_signed_in
+  before_filter :if_not_signed_in
 
-	def index
-		email = params[:search][:email]
+  def index
+    email = params[:search][:email]
 
-		@matching_users = User
+    @matching_users = User
 
-		if email.present?
-			email.strip!
-			@matching_users = @matching_users.where(email: email)
-		end
+    if !email.nil?
+      email.strip!
+      @matching_users = @matching_users.where(email: email)
+    end
 
-		@matching_users = @matching_users.where.not(id: current_user.id).all
-	end
+    @matching_users = @matching_users.where.not(id: current_user.id).all
+  end
 
-	def posts
-		if !params[:search][:name].blank?
-			if params[:search][:data_type] == 'moment'
-				path = moments_path(search: params[:search][:name])
-			elsif params[:search][:data_type] == 'category'
-				path = categories_path(search: params[:search][:name])
-			elsif params[:search][:data_type] == 'mood'
-				path = moods_path(search: params[:search][:name])
-			elsif params[:search][:data_type] == 'strategy'
-				path = strategies_path(search: params[:search][:name])
-			elsif params[:search][:data_type] == 'medication'
-				path = medications_path(search: params[:search][:name])
-			end
-		else
-			if params[:search][:data_type] == 'moment'
-				path = moments_path
-			elsif params[:search][:data_type] == 'category'
-				path = categories_path
-			elsif params[:search][:data_type] == 'mood'
-				path = moods_path
-			elsif params[:search][:data_type] == 'strategy'
-				path = strategies_path
-			elsif params[:search][:data_type] == 'medication'
-				path = medications_path
-			end
-		end
+  def posts
+    if !params[:search][:name].blank?
+      if params[:search][:data_type] == 'moment'
+        path = moments_path(search: params[:search][:name])
+      elsif params[:search][:data_type] == 'category'
+        path = categories_path(search: params[:search][:name])
+      elsif params[:search][:data_type] == 'mood'
+        path = moods_path(search: params[:search][:name])
+      elsif params[:search][:data_type] == 'strategy'
+        path = strategies_path(search: params[:search][:name])
+      elsif params[:search][:data_type] == 'medication'
+        path = medications_path(search: params[:search][:name])
+      end
+    else
+      if params[:search][:data_type] == 'moment'
+        path = moments_path
+      elsif params[:search][:data_type] == 'category'
+        path = categories_path
+      elsif params[:search][:data_type] == 'mood'
+        path = moods_path
+      elsif params[:search][:data_type] == 'strategy'
+        path = strategies_path
+      elsif params[:search][:data_type] == 'medication'
+        path = medications_path
+      end
+    end
 
-		respond_to do |format|
-			format.html { redirect_to path }
-			format.json { head :no_content }
-		end
-	end
+    respond_to do |format|
+      format.html { redirect_to path }
+      format.json { head :no_content }
+    end
+  end
 
-	private
+  private
 
-	def if_not_signed_in
-		if !user_signed_in?
-			respond_to do |format|
-			  format.html { redirect_to new_user_session_path }
-			  format.json { head :no_content }
-			end
-		end
-	end
+  def if_not_signed_in
+    if !user_signed_in?
+      respond_to do |format|
+        format.html { redirect_to new_user_session_path }
+        format.json { head :no_content }
+      end
+    end
+  end
 end
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,29 +1,45 @@
 RSpec.describe SearchController, type: :controller do
   let(:user) { create(:user) }
 
-  describe '#index' do
-    before { get :index }
+  let!(:user_two) { create(:user, email: 'foo@email.com') }
+  let!(:user_three) { create(:user, email: 'bar@email.com') }
 
+  describe '#index' do
     context 'when user is logged in' do
       include_context :logged_in_user
 
       it 'renders the correct template' do
+        get :index, search: { }
+
         expect(response).to render_template(:index)
       end
 
       context 'when have passed email' do
-        it 'sets the correct instance variables' do
+        it 'strip passed string' do
+          expect_any_instance_of(String).to receive(:strip!)
 
+          get :index, search: { email: '  hi@email.com ' }
+        end
+
+        it 'filter by user email' do
+          get :index, search: { email: 'bar@email.com' }
+
+          expect(assigns(:matching_users)).to eq([user_three])
         end
       end
 
       context 'when have no passed email' do
         it 'sets the correct instance variables' do
+          get :index, search: { email: '' }
+
+          expect(assigns(:matching_users)).to_not include(user)
         end
       end
     end
 
     context 'when user is not logged in' do
+      before { get :index, search: { } }
+
       it_behaves_like :with_no_logged_in_user
     end
   end
