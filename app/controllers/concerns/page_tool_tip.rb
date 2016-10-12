7ec0d334c09dbbb3d6e60ee22d85cf98810409fb
andela-oesho <susan.esho@andela.com>
2016-10-12 15:52:48 +0100

    refactor page tip

--- app/controllers/concerns/page_tool_tip.rb
+++ app/controllers/concerns/page_tool_tip.rb
@@ -8,21 +8,17 @@ module PageToolTip
       current_user.id
     ).all
     user = model.where(userid: current_user.id)
-    check_blank_name(name, search)
+    set_collection(collection, user, search, name)
     @page_tooltip = t("#{model_name.pluralize}.new")
   end
 
-  def check_blank_name(name,search)
-      if !name.blank? && search.exists?
-      instance_variable_set(
-        collection.to_s,
-        search.order('created_at DESC').page(params[:page])
-      )
-    else
-      instance_variable_set(
-        collection.to_s,
-        user.all.order('created_at DESC').page(params[:page])
-      )
-    end
+  def set_collection(collection, user, search, name)
+    search_query = search.exists? && !name.blank? ? search : user.all
+    instance_variable_set(
+      collection.to_s,
+      search_query.order('created_at DESC').page(params[:page])
+    )
   end
 end
+
+
