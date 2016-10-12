1d56b57d6d0554ac6fd985b440c20b4ab4d9d7fd
andela-oesho <susan.esho@andela.com>
2016-10-12 09:27:17 +0100

    modified PageToolTip

--- app/controllers/concerns/page_tool_tip.rb
+++ app/controllers/concerns/page_tool_tip.rb
@@ -1,14 +1,13 @@
 module PageToolTip
-  def set(tips, mod_name)
+  def set(tips, model_name)
     name = params[:search]
-    capt_name = tips.capitalize
-    ClassName = capt_name.constantize
+    ClassName = Object.const_get(model_name.capitalize)
     search = ClassName.where("name ilike ? AND userid = ?", "%#{name}%", current_user.id).all
     if !name.blank? && search.exists?
       tips = search.order("created_at DESC").page(params[:page])
     else
       tips = ClassName.where(:userid => current_user.id).all.order("created_at DESC").page(params[:page])
     end
-    page_tooltip = t("#{mod_name}.new")
+    page_tooltip = t("#{model_name}.new")
   end
 end
