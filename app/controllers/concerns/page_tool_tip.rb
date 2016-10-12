f009281cdd4f24b30d5f34fcb350056a09500864
andela-oesho <susan.esho@andela.com>
2016-10-12 11:11:32 +0100

    fix page_tool_tip file

--- app/controllers/concerns/page_tool_tip.rb
+++ app/controllers/concerns/page_tool_tip.rb
@@ -1,12 +1,12 @@
 module PageToolTip
   def set(collection, model_name)
     name = params[:search]
-    ClassName = Object.const_get(model_name.capitalize)
-    search = ClassName.where("name ilike ? AND userid = ?", "%#{name}%", current_user.id).all
+    Object.const_get(model_name.capitalize)
+    search = Object.const_get(model_name.capitalize).where("name ilike ? AND userid = ?", "%#{name}%", current_user.id).all
     if !name.blank? && search.exists?
       instance_variable_set("#{collection}", search.order("created_at DESC").page(params[:page]))
     else
-      instance_variable_set("#{collection}", ClassName.where(:userid => current_user.id).all.order("created_at DESC").page(params[:page])
+      instance_variable_set("#{collection}", Object.const_get(model_name.capitalize).where(:userid => current_user.id).all.order("created_at DESC").page(params[:page]))
     end
     @page_tooltip = t("#{model_name.pluralize}.new")
   end
