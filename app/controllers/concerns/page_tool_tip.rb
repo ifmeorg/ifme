59a1f47801ae3025ed0a0d8f8901ca121f0f0162
andela-oesho <susan.esho@andela.com>
2016-10-12 12:04:25 +0100

    fix long line issues

--- app/controllers/concerns/page_tool_tip.rb
+++ app/controllers/concerns/page_tool_tip.rb
@@ -1,12 +1,23 @@
 module PageToolTip
   def set(collection, model_name)
     name = params[:search]
-    Object.const_get(model_name.capitalize)
-    search = Object.const_get(model_name.capitalize).where("name ilike ? AND userid = ?", "%#{name}%", current_user.id).all
+    model = Object.const_get(model_name.capitalize)
+    search = model.where(
+      'name ilike ? AND userid = ?',
+      "%#{name}%",
+      current_user.id
+    ).all
+    user = model.where(userid: current_user.id)
     if !name.blank? && search.exists?
-      instance_variable_set("#{collection}", search.order("created_at DESC").page(params[:page]))
+      instance_variable_set(
+        collection.to_s,
+        search.order('created_at DESC').page(params[:page])
+      )
     else
-      instance_variable_set("#{collection}", Object.const_get(model_name.capitalize).where(:userid => current_user.id).all.order("created_at DESC").page(params[:page]))
+      instance_variable_set(
+        collection.to_s,
+        user.all.order('created_at DESC').page(params[:page])
+      )
     end
     @page_tooltip = t("#{model_name.pluralize}.new")
   end
