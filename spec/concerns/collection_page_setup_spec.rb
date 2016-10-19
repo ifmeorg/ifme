3fbb0f90794557502e0a44477bc6fa0e5fb763da
andela-oesho <susan.esho@andela.com>
2016-10-19 09:36:06 +0100

    add test for concern

--- spec/concerns/collection_page_setup_spec.rb
+++ spec/concerns/collection_page_setup_spec.rb
@@ -4,7 +4,13 @@ end
  
 describe CollectionPageSetupTestController do
  
-  # it "should setup a page" do
-  #   subject.page_collection("muyiwa", "osiname")
-  # end
+  it "should setup a page" do
+    user = create(:user)
+    allow(subject).to receive(:current_user).and_return(user)
+    allow(subject).to receive(:params).and_return({search: 'a value'})
+    page_collection = subject.page_collection("@categories", "category")
+
+    expect(subject.params[:search]).to eq 'a value'
+    expect(page_collection).to be_truthy
+  end
 end
