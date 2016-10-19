be93c62061ee2f37e5c2dfd6ca57908fc56166b6
andela-oesho <susan.esho@andela.com>
2016-10-19 09:43:38 +0100

    remove rubocop autocoreect on all files

--- spec/concerns/collection_page_setup_spec.rb
+++ spec/concerns/collection_page_setup_spec.rb
@@ -3,8 +3,14 @@ class CollectionPageSetupTestController < ApplicationController
 end
  
 describe CollectionPageSetupTestController do
+  
+  it "should setup a page" do
+    user = create(:user)
+    allow(subject).to receive(:current_user).and_return(user)
+    allow(subject).to receive(:params).and_return({search: 'a value'})
+    page_collection = subject.page_collection("@categories", "category")
  
-  # it "should mark an item out of stock" do
-  #   subject.page_collection
-  # end
-end
+    expect(subject.params[:search]).to eq 'a value'
+    expect(page_collection).to be_truthy
+  end
+end 
