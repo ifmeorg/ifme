827e9e67c69b47a93c6e5b68fed8031ce70e73fa
Julia Nguyen <julianguyen@users.noreply.github.com>
2016-11-12 23:52:28 -0800

    Clean up mobile tooltip (#369)
    - Added Daniel Chinedu to contributors page
    - Renamed charactercount.js to character_count.js
    - TODO: write test specs for mobile_toolip.js

--- app/assets/javascripts/mobile_tooltip.js
+++ app/assets/javascripts/mobile_tooltip.js
@@ -1,41 +1,35 @@
-(function () {
-  /*
-  **  Tooltip for the profile page on mobile devices
-  **  This script should ideally run on the profile page
+/*
+**  Tooltip for the profile page on mobile devices
+**  This script should ideally run on the profile page
+**  Behaviour
+    ========================
+    - Enables the viewers tooltip to be shown on mobile devices when the
+      parent element is clicked.
+    - Toggle behaviour is implemented so that the tooltip can be both shown
+      and dismissed
+*/
 
-  **  Behaviour
-      ========================
-      - Enables the viewers tooltip to be shown on mobile devices when the
-        parent element is clicked.
-      - Toggle behaviour is implemented so that the tooltip can be both shown
-        and dismissed
+var onReadyMobileTip = function() {
+  var MEDIUM_BREAKPOINT = 640;
 
-  */
+  if ($(window).width() <= MEDIUM_BREAKPOINT) {
+    $('.some_focus_text').click(function () {
+      var child = $(this).children('.yes_title').eq(0);
+      var title = child.prop('title');
 
-  $(document).on("page:load ready", function () {
-    // Allow this script to only be run on the profile page
-
-    if (window.location.pathname !== "/profile") return false;
-
-    var toggleCount = 0;
-
-    // Ensure the script is activated on mobile devices only
-    // Max-width: 768px
-
-    if ($(window).width() <= 768) {
-      $(".some_focus_text").on("click", function () {
-        var child = $(this).children(".yes_title").eq(0),
-            title = child.prop("title");
-
-        child.tooltip({
-          content: title,
-          position: { my: "top-10 left+25", at: "top left", of: $(this) }
-        });
-
-        toggleCount++;
-
-        toggleCount % 2 === 1 ? child.tooltip("open") : child.tooltip("close");
+      child.tooltip({
+        content: title,
+        position: { my: 'left+40 top+25', at: 'left top', of: $(this) }
       });
-    }
-  });
-}());
+
+      var tooltipId = $(child).attr('aria-describedby');
+      if (tooltipId) {
+        child.tooltip('close');
+      } else {
+        child.tooltip('open');
+      }
+    });
+  }
+};
+
+$(document).on("page:load ready", onReadyMobileTip);
