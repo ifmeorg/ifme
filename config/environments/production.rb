b5e233b192bea97cd8f6f1d8833b17afb02807f1
nma <nickma38@gmail.com>
2016-11-27 00:41:03 -0500

    modify server to return the correct domain for SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,4 +104,8 @@ Rails.application.configure do
   }
 
   config.force_ssl = false 
+  # This is required or the host server will send headers telling us its from the *.herokuapp.com domain
+  # subsequently, this will cause the SSL certs registered for our private DNS address to fail.
+  config.action_controller.default_url_options = { host: "www.moodnotes.me" }
+  config.action_controller.asset_host = "www.moodnotes.me"
 end
