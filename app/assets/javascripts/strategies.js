8850e95134df6940826ee5fe643d676da99dd033
Suzan Esho <susan.esho@andela.com>
2017-04-09 03:10:31 +0100

    complete premade for strategies

--- app/assets/javascripts/strategies.js
+++ app/assets/javascripts/strategies.js
@@ -18,3 +18,24 @@ var onReadyStrategies = function() {
 };
 
 $(document).on("page:load ready", onReadyStrategies);
+
+
+var onReadyStrategiesModal = function() {
+
+	var modal = document.getElementById('myModal');
+	var btn = document.getElementById("myBtn");
+	var span = document.getElementsByClassName("close")[0];
+	btn.onclick = function() {
+	    modal.style.display = "block";
+	}
+	span.onclick = function() {
+	    modal.style.display = "none";
+	}
+	window.onclick = function(event) {
+	    if (event.target == modal) {
+	        modal.style.display = "none";
+	    }
+	}
+}
+
+$(document).on("page:load ready", onReadyStrategiesModal);
