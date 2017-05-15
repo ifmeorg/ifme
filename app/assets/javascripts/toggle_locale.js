2e3c5573756a497e42ef3205745d83c03045e8c9
Julia Nguyen <tojulianguyen@gmail.com>
2017-05-15 01:07:22 -0700

    Feature specs for UI toggle

--- app/assets/javascripts/toggle_locale.js
+++ app/assets/javascripts/toggle_locale.js
@@ -2,18 +2,18 @@ function toggleLocale(locale, localStorageLocale) {
 	$.ajax({
 		url: "/toggle_locale",
 		data: {
-      locale: locale,
-      local_storage_locale: localStorageLocale
-    }
+			locale: locale,
+			local_storage_locale: localStorageLocale
+		}
 	}).done(function(data) {
-    if (data.signed_in_reload) {
-      window.localStorage.setItem("locale", locale);
-      window.location.reload();
-    } else if (data.signed_out_reload) {
-      var href = window.location.href.split("?")[0];
-      window.localStorage.setItem("locale", locale);
-      window.location.replace(href + "?locale=" + locale);
-    }
+		if (data.signed_in_reload) {
+			window.localStorage.setItem("locale", locale);
+			window.location.reload();
+		} else if (data.signed_out_reload) {
+			var href = window.location.href.split("?")[0];
+			window.localStorage.setItem("locale", locale);
+			window.location.replace(href + "?locale=" + locale);
+		}
 	});
 }
 
