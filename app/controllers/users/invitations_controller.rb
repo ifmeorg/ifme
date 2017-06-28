9aa1e7188620f03fd8cac547ae01ea5ba15a4d2c
Adam Rice <development@hashnotadam.com>
2017-06-28 12:22:09 +1000

    :cop: Use nested module/class definitions instead of compact style

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,18 +1,20 @@
-class Users::InvitationsController < Devise::InvitationsController
-  # POST /resource/invitation
-  def create
-    self.resource = invite_resource
-    resource_invited = resource.errors.empty?
+module Users
+  class InvitationsController < Devise::InvitationsController
+    # POST /resource/invitation
+    def create
+      self.resource = invite_resource
+      resource_invited = resource.errors.empty?
 
-    yield resource if block_given?
+      yield resource if block_given?
 
-    if resource_invited
-      if is_flashing_format? && self.resource.invitation_sent_at
-        set_flash_message :notice, :send_instructions, email: self.resource.email
+      if resource_invited
+        if is_flashing_format? && self.resource.invitation_sent_at
+          set_flash_message :notice, :send_instructions, email: self.resource.email
+        end
+        respond_with resource, location: new_user_invitation_path
+      else
+        respond_with_navigational(resource) { render :new }
       end
-      respond_with resource, location: new_user_invitation_path
-    else
-      respond_with_navigational(resource) { render :new }
     end
   end
 end
