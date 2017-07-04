e1ed4ab9b725805291b0f9762931ba0be241ff0a
Claire Wild <claire.wild15@gmail.com>
2017-07-04 16:46:38 -0700

    Almost finish tests for modals

--- spec/javascripts/NotificationsSpec.js
+++ spec/javascripts/NotificationsSpec.js
@@ -36,62 +36,62 @@ describe("Notifications", function() {
     expect($('#notifications_none').hasClass("display_block")).toBe(true);
   });
 
-  it("close tips notifications modal", function() { // Doesn't work!!
+  it("close tips notifications modal", function() { // This test is failing!!
     $('.tip_close_notifications').click();
     expect($('.tip_notifications').hasClass("display_none")).toBe(true);
   });
 
-  it("keep tips modal open on modal click", function() { // ADDED
+  it("keep tips modal open on modal click", function() {
     $('.tip_notifications_text').click();
     expect($('.tip_notifications').hasClass("display_none")).toBe(false);
   });
 
-  it("close tips modal on backdrop click", function() { // ADDED
+  it("close tips modal on backdrop click", function() {
     $('.tip_notifications').click();
     expect($('.tip_notifications').hasClass("display_none")).toBe(true);
   });
 
-  it("close tips modal on esc key press", function() { // ADDED
+  it("close tips modal on esc key press", function() {
     keyPress(27);
     expect($('.tip_notifications').hasClass("display_none")).toBe(true);
   });
 
-  it("close categories moods", function() { // UPDATED
+  it("close categories moods", function() {
     $('#close_categories_moods').click();
     expect($('#categories_moods').hasClass("display_none")).toBe(true);
   });
 
-  it("keep categories moods open on modal click", function() { // ADDED
+  it("keep categories moods open on modal click", function() {
     $('#categories_moods_text').click();
     expect($('#categories_moods').hasClass("display_none")).toBe(false);
   });
 
-  it("close categories moods on backdrop click", function() { // ADDED
+  it("close categories moods on backdrop click", function() {
     $('#categories_moods').click();
     expect($('#categories_moods').hasClass("display_none")).toBe(true);
   });
 
-  it("close categories moods on esc key press", function() { // ADDED
+  it("close categories moods on esc key press", function() {
     keyPress(27);
     expect($('#categories_moods').hasClass("display_none")).toBe(true);
   });
 
-  it("close quick create", function() { // ADDED
+  it("close quick create", function() {
     $('.quick_create_close').click();
     expect($('#category_quick_create').hasClass("display_none")).toBe(true);
   });
 
-  it("keep quick create open on modal click", function() { // ADDED
+  it("keep quick create open on modal click", function() {
     $('.quick_create_text').click();
     expect($('#category_quick_create').hasClass("display_none")).toBe(false);
   });
 
-  it("close quick create on backdrop click", function() { // ADDED
+  it("close quick create on backdrop click", function() {
     $('#category_quick_create').click();
     expect($('#category_quick_create').hasClass("display_none")).toBe(true);
   });
 
-  it("close quick create on esc key press", function() { // ADDED
+  it("close quick create on esc key press", function() {
     keyPress(27);
     expect($('#category_quick_create').hasClass("display_none")).toBe(true);
     expect($('#mood_quick_create').hasClass("display_none")).toBe(true);
