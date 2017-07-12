6d9cefbd42c04de86fdc4e2dfb22c83d69a99f0e
Jenny Nam <jennynamster@gmail.com>
2017-07-12 13:59:00 +1000

    [#363] Fix the indentation in character_count.js

--- app/assets/javascripts/character_count.js
+++ app/assets/javascripts/character_count.js
@@ -1,66 +1,66 @@
 function changeEditorCount(editorCount, remaining) {
-	$(editorCount).html(remaining);
-    if (remaining <= 0) {
-    	$(editorCount).css('color', '#990019');
+  $(editorCount).html(remaining);
 
-    	if (remaining === 0) {
-    		$('input[type="submit"]').prop('disabled', false);
-    	} else {
-    		$('input[type="submit"]').prop('disabled', true);
-    	}
+    if (remaining <= 0) {
+      $(editorCount).css('color', '#990019');
+
+      if (remaining === 0) {
+        $('input[type="submit"]').prop('disabled', false);
+      } else {
+        $('input[type="submit"]').prop('disabled', true);
+      }
     } else {
-    	$(editorCount).css('color', '#b5839b');
-    	$('input[type="submit"]').prop('disabled', false);
+      $(editorCount).css('color', '#b5839b');
+      $('input[type="submit"]').prop('disabled', false);
     }
 }
 
 function noCKEditor(editor) {
-	var editorName = editor.attr('id');
-	var editorData = editor.val();
-  	var editorCount = '#' + editorName + '_count';
-	var remaining = 2000 - editorData.length;
+  var editorName = editor.attr('id');
+  var editorData = editor.val();
+  var editorCount = '#' + editorName + '_count';
+  var remaining = 2000 - editorData.length;
 
 	 changeEditorCount(editorCount, remaining);
 }
 
 function yesCKEditor(editor) {
-	var editorCount = '#' + editor.name + '_count';
-    var remaining = 2000 - editor.getData().length;
+  var editorCount = '#' + editor.name + '_count';
+  var remaining = 2000 - editor.getData().length;
 
-	changeEditorCount(editorCount, remaining);
+  changeEditorCount(editorCount, remaining);
 }
 
 var onReadyCharacterCount = function() {
-	CKEDITOR.on("instanceReady", function(event) {
-		var editor;
-	  	editor = event.editor;
+  CKEDITOR.on("instanceReady", function(event) {
+    var editor = event.editor;
 
-	  	yesCKEditor(editor);
+    yesCKEditor(editor);
 
-	  	return editor.on('change', function() {
-	  		yesCKEditor(editor);
-	  	});
-	});
+    return editor.on('change', function() {
+      yesCKEditor(editor);
+    });
+  });
 
-	if ($('body').hasClass('pages home')) {
-		$('#moment_why').bind('keyup', function() {
-			noCKEditor($(this));
-		});
-	}
+  if ($('body').hasClass('pages home')) {
+    $('#moment_why').bind('keyup', function() {
+      noCKEditor($(this));
+    });
+  }
 
-	if (newOrEdit(['moments'])) {
-		noCKEditor($('#strategy_description'));
+  if (newOrEdit(['moments'])) {
+    noCKEditor($('#strategy_description'));
 
-		$('#strategy_description').bind('keyup', function() {
-			noCKEditor($(this));
-		});
-	}
+    $('#strategy_description').bind('keyup', function() {
+      noCKEditor($(this));
+    });
+  }
 
-	if (isShow(['moments', 'strategies', 'meetings'])) {
-		$('#comment_comment').bind('keyup', function() {
-			noCKEditor($(this));
-		});
-	}
+  if (isShow(['moments', 'strategies', 'meetings'])) {
+    $('#comment_comment').bind('keyup', function() {
+      noCKEditor($(this));
+    });
+  }
 };
 
 $(document).on("page:load ready", onReadyCharacterCount);
