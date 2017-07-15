08c561b53db0a8d5edc36399521a0d814fefe2f6
Julia Nguyen <tojulianguyen@gmail.com>
2017-07-15 16:32:05 -0700

    Tests for page_anchor

--- app/assets/javascripts/page_anchor.js
+++ app/assets/javascripts/page_anchor.js
@@ -1,6 +1,10 @@
+function scrollToPageAnchor() {
+  $("html, body").animate({ scrollTop: $(".page_anchor").offset().top - $("#header").height()}, 1000);
+}
+
 var onReadyPageAnchor = function() {
   if ($(".page_anchor").length > 0) {
-    $("html, body").animate({ scrollTop: $(".page_anchor").offset().top - $("#header").height()}, 1000);
+    scrollToPageAnchor();
   }
 };
 
