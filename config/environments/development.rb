00f1b043483420146bd7701161fffd07d0909c6f
Adam Rice <development@hashnotadam.com>
2017-07-16 17:50:13 +1000

    Suppress the writing of asset requests to the log file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,9 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  # Suppress writing of asset requests to the log file
+  config.assets.quiet = true
+
   # Raises error for missing translations
   config.action_view.raise_on_missing_translations = true
 
