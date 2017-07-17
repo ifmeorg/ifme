cf22a9623c1277fd183d5b864f8764400bd2320e
nma <nickma38@gmail.com>
2017-07-16 20:56:59 -0400

    merge in sentimental files to text, licenses and data included

--- app/services/sentiment_analysis.rb
+++ app/services/sentiment_analysis.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require_relative 'sentimental'
 
 class SentimentAnalysis
   attr_reader :language, :id, :text
@@ -9,29 +10,53 @@ class SentimentAnalysis
     @text = text
   end
 
+  # TODO: put this back in place
+  # def get_sentiment
+  #   uri = URI('https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment')
+  #   uri.query = URI.encode_www_form({})
+  #
+  #   request = Net::HTTP::Post.new(uri.request_uri)
+  #   request['Content-Type'] = 'application/json'
+  #   request['Ocp-Apim-Subscription-Key'] = ENV["OCP_APM_SUBSCRIPTION_KEY"]
+  #   request.body = {
+  #     "documents": [
+  #       {
+  #         "language": language,
+  #         "id": id,
+  #         "text": text
+  #       }
+  #     ]
+  #   }.to_json
+  #
+  #   response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+  #     http.request(request)
+  #   end
+  #
+  #   puts '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
+  #   puts response.body
+  #   puts '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
+  # end
+
+  # TODO: figure out how to properly base class in Ruby
   def get_sentiment
-    uri = URI('https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment')
-    uri.query = URI.encode_www_form({})
+    # Create an instance for usage
+    analyzer = Sentimental.new
 
-    request = Net::HTTP::Post.new(uri.request_uri)
-    request['Content-Type'] = 'application/json'
-    request['Ocp-Apim-Subscription-Key'] = ENV["OCP_APM_SUBSCRIPTION_KEY"]
-    request.body = {
-      "documents": [
-        {
-          "language": language,
-          "id": id,
-          "text": text
-        }
-      ]
-    }.to_json
+    # Load the default sentiment dictionaries
+    analyzer.load_defaults
 
-    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-        http.request(request)
-    end
+    # And/or load your own dictionaries
+    # analyzer.load_senti_file('path/to/your/file.txt')
+
+    # Set a global threshold
+    analyzer.threshold = 0.5
 
     puts '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
-    puts response.body
+    puts analyzer.sentiment(text)
+    puts '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
+
+    puts '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
+    puts analyzer.explain(text)
     puts '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
   end
 end
