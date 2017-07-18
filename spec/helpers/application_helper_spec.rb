5de635d3ff1712dbf6269b261e08ef86a60d8e24
Julia Nguyen <tojulianguyen@gmail.com>
2017-07-18 09:31:55 -0700

    Write tests for i18n_set? and get_icon_text

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,46 +1,46 @@
 describe ApplicationHelper do
-  describe "#nav_link_to" do
+  describe '#nav_link_to' do
     let(:is_active) { true }
-    let(:label)     { "foo" }
-    let(:path)      { "bar" }
+    let(:label)     { 'foo' }
+    let(:path)      { 'bar' }
     let(:options)   { {} }
 
     subject { nav_link_to(label, path, options) }
 
     before(:each) do
-      allow(self).to receive("is_active?").and_return(is_active)
+      allow(self).to receive('is_active?').and_return(is_active)
     end
 
-    context "when active" do
+    context 'when active' do
       let(:is_active) { true }
 
-      it { is_expected.to have_selector "li a" }
+      it { is_expected.to have_selector 'li a' }
       it { is_expected.to include label }
-      it { is_expected.to have_selector "li.active" }
+      it { is_expected.to have_selector 'li.active' }
     end
 
-    context "when not active" do
+    context 'when not active' do
       let(:is_active) { false }
 
-      it { is_expected.to have_selector "li a" }
+      it { is_expected.to have_selector 'li a' }
       it { is_expected.to include label }
-      it { is_expected.not_to have_selector "li.active" }
+      it { is_expected.not_to have_selector 'li.active' }
     end
 
-    context "when options include method" do
+    context 'when options include method' do
       let(:options) { { :method => :delete } }
 
-      it "passes method in environment" do
+      it 'passes method in environment' do
         expect(self).to receive(:is_active?).with(path, options)
         nav_link_to(label, path, options)
       end
     end
   end
 
-  describe "#is_active?" do
+  describe '#is_active?' do
     let(:is_current_page)    { false }
-    let(:current_controller) { "" }
-    let(:action_name)        { "" }
+    let(:current_controller) { '' }
+    let(:action_name)        { '' }
     let(:path)               { root_path }
     let(:environment)        { {} }
 
@@ -49,64 +49,94 @@ describe ApplicationHelper do
     before(:each) do
       params[:controller] = current_controller
       allow(controller).to receive(:action_name).and_return(action_name)
-      allow(self).to receive("current_page?").and_return(is_current_page)
+      allow(self).to receive('current_page?').and_return(is_current_page)
     end
 
-    context "current page" do
+    context 'current page' do
       let(:is_current_page) { true }
       it { is_expected.to be true }
     end
 
-    context "current controller" do
-      let(:current_controller) { "moments" }
+    context 'current controller' do
+      let(:current_controller) { 'moments' }
       let(:path)               { new_moment_path }
 
       it { is_expected.to be true }
     end
 
-    context "current controller and profile" do
-      let(:current_controller) { "profile" }
-      let(:path)               { "profile?userid=2" }
+    context 'current controller and profile' do
+      let(:current_controller) { 'profile' }
+      let(:path)               { 'profile?userid=2' }
 
       it { is_expected.to be false }
     end
 
-    context "current controller and about" do
-      let(:current_controller) { "pages" }
-      let(:path)               { "about" }
+    context 'current controller and about' do
+      let(:current_controller) { 'pages' }
+      let(:path)               { 'about' }
 
       it { is_expected.to be false }
     end
 
-    context "parent of active controller" do
-      let(:current_controller) { "categories" }
+    context 'parent of active controller' do
+      let(:current_controller) { 'categories' }
       let(:path)               { moments_path }
 
       it { is_expected.to be true }
     end
 
-    context "new user session with devise" do
-      let(:current_controller) { "devise/sessions" }
-      let(:action_name)        { "new" }
+    context 'new user session with devise' do
+      let(:current_controller) { 'devise/sessions' }
+      let(:action_name)        { 'new' }
       let(:path)               { new_user_session_path }
 
       it { is_expected.to be true }
     end
 
-    context "new user registration with devise" do
-      let(:current_controller) { "devise/registrations" }
-      let(:action_name)        { "create" }
+    context 'new user registration with devise' do
+      let(:current_controller) { 'devise/registrations' }
+      let(:action_name)        { 'create' }
       let(:path)               { new_user_registration_path }
 
       it { is_expected.to be true }
     end
 
-    context "sign out" do
-      let(:current_controller) { "profile" }
+    context 'sign out' do
+      let(:current_controller) { 'profile' }
       let(:path)               { destroy_user_session_path }
       let(:environment)        { { :method => :delete } }
 
       it { is_expected.to be false }
+    end
+  end
+
+  describe '#i18n_set?' do
+    context 'when does not exist' do
+      it 'returns false' do
+        expect(i18n_set?('i_do_not_exist')).to eq(false)
+      end
+    end
+
+    context 'when key exists' do
+      it 'returns i18n value' do
+        expect(i18n_set?('app_name')).to eq('if me')
+      end
+    end
+  end
+
+  describe '#get_icon_text' do
+    context 'when icon and text are nil' do
+      it 'returns empty string' do
+        expect(get_icon_text(nil, nil)).to eq('')
+      end
+    end
+
+    context 'when icon and text are string values' do
+      it 'returns icon text' do
+        expect(get_icon_text('facebook','Facebook')).to eq(
+          '<i class="fa fa-facebook smaller_margin_right"></i>Facebook'
+        )
+      end
     end
   end
 end
