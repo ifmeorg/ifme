df5c16e3a66680d5e6b97f52ba2a177ae3a6e375
nma <nickma38@gmail.com>
2017-07-19 00:49:02 -0400

    Revert "merge in sentimental files to text, licenses and data included"
    This reverts commit cf22a9623c1277fd183d5b864f8764400bd2320e.

--- app/services/sentiment_analysis.rb
+++ app/services/sentiment_analysis.rb
@@ -1,5 +1,4 @@
 require 'net/http'
-require_relative 'sentimental'
 
 class SentimentAnalysis
   attr_reader :language, :id, :text
@@ -10,53 +9,29 @@ class SentimentAnalysis
     @text = text
   end
 
-  # TODO: put this back in place
-  # def get_sentiment
-  #   uri = URI('https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment')
-  #   uri.query = URI.encode_www_form({})
-  #
-  #   request = Net::HTTP::Post.new(uri.request_uri)
-  #   request['Content-Type'] = 'application/json'
-  #   request['Ocp-Apim-Subscription-Key'] = ENV["OCP_APM_SUBSCRIPTION_KEY"]
-  #   request.body = {
-  #     "documents": [
-  #       {
-  #         "language": language,
-  #         "id": id,
-  #         "text": text
-  #       }
-  #     ]
-  #   }.to_json
-  #
-  #   response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-  #     http.request(request)
-  #   end
-  #
-  #   puts '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
-  #   puts response.body
-  #   puts '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
-  # end
-
-  # TODO: figure out how to properly base class in Ruby
   def get_sentiment
-    # Create an instance for usage
-    analyzer = Sentimental.new
+    uri = URI('https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment')
+    uri.query = URI.encode_www_form({})
 
-    # Load the default sentiment dictionaries
-    analyzer.load_defaults
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request['Content-Type'] = 'application/json'
+    request['Ocp-Apim-Subscription-Key'] = ENV["OCP_APM_SUBSCRIPTION_KEY"]
+    request.body = {
+      "documents": [
+        {
+          "language": language,
+          "id": id,
+          "text": text
+        }
+      ]
+    }.to_json
 
-    # And/or load your own dictionaries
-    # analyzer.load_senti_file('path/to/your/file.txt')
-
-    # Set a global threshold
-    analyzer.threshold = 0.5
+    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+        http.request(request)
+    end
 
     puts '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
-    puts analyzer.sentiment(text)
-    puts '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
-
-    puts '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
-    puts analyzer.explain(text)
+    puts response.body
     puts '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
   end
 end
