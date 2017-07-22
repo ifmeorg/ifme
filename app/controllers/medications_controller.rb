823a14dbd240f5a1edb53e73997cef9e1f80d28d
Arthur Brophy <artbrophy@gmail.com>
2017-07-22 10:43:05 -0400

    replaces conditional with guard clause in controller

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -90,16 +90,17 @@ class MedicationsController < ApplicationController
 
   # Save refill date to Google calendar
   def save_refill_to_google_calendar(medication)
-    if current_user.google_oauth2_enabled? && medication.add_to_google_cal == "1"
-      summary = 'Refill for ' + medication.name
-      date = medication.refill
-      CalendarUploader.new(
-        summary: summary,
-        date: date,
-        access_token: current_user.token,
-        email: current_user.email
-        ).upload_event
-    end
+    return unless current_user.google_oauth2_enabled? &&
+     medication.add_to_google_cal == "1"
+
+    summary = 'Refill for ' + medication.name
+    date = medication.refill
+    CalendarUploader.new(
+      summary: summary,
+      date: date,
+      access_token: current_user.token,
+      email: current_user.email
+      ).upload_event
   end
 
   private
