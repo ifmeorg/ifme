60622cd101a97d0982f7e8d7187aed2928c44110
Jenny Nam <jennynamster@gmail.com>
2017-07-24 23:45:02 +0100

    [#455] Amend secret_shares_controller

--- app/controllers/secret_shares_controller.rb
+++ app/controllers/secret_shares_controller.rb
@@ -2,7 +2,7 @@ class SecretSharesController < ApplicationController
 
   def create
     moment = Moment.friendly.find(params[:moments])
-    moment.update :secret_share_identifier => SecureRandom.uuid
+    moment.update!(secret_share_identifier: SecureRandom.uuid)
     redirect_to moment_path(moment)
   end
 
