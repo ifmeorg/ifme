4073b6d4f77de4542cd86ff26f2adc51e4bd1f54
Jenny Nam <jennynamster@gmail.com>
2017-07-24 22:15:13 +0100

    [#455] Add database to Secret Share Moment

--- db/migrate/20170724205314_add_secret_shares_to_moments.rb
+++ db/migrate/20170724205314_add_secret_shares_to_moments.rb
@@ -1,4 +1,6 @@
 class AddSecretSharesToMoments < ActiveRecord::Migration
   def change
+    add_column :moments, :secret_share_identifier, :string
+    add_column :moments, :secret_share_expires_at, :datetime
   end
 end
