f571105339fc9d66ad2f23fdc148a16eefe741ab
nma <nickma38@gmail.com>
2017-07-31 22:33:52 -0400

    first jest test passing

--- client/app/bundles/HelloWorld/startup/registration.jsx
+++ client/app/bundles/HelloWorld/startup/registration.jsx
@@ -1,8 +1,10 @@
 import ReactOnRails from 'react-on-rails';
 
 import HelloWorld from '../components/HelloWorld';
+import Chart from '../components/Chart';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  HelloWorld,
+    HelloWorld,
+    Chart
 });
