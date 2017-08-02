ccfffa236ef36333786716f478a7d95aad0ccaf7
Sophie <sophiermcdonald@gmail.com>
2017-08-02 13:33:28 +1000

    Test for missing keys in locale file

--- spec/compare_locales_spec.rb
+++ spec/compare_locales_spec.rb
@@ -1,111 +1,75 @@
 # frozen_string_literal: true
 
 RSpec.describe CompareLocalesSupport do
-  main_hash = {
-    'en' => {
-      'app_name'=>'if me',
-      'app_description'=>'is a community for mental health experiences',
-      'email'=>'join.ifme@gmail.com',
-      'ellipsis'=>'[...]',
-      'created'=>'<strong>Created:</strong> %{created_at}',
-      'edited'=>'<strong>Created:</strong> %{created_at} <em>(edited)</em>',
-      'salutation'=>'Hi %{name},',
-      'click_here'=>'click here',
-      'less'=>'[Less]',
-      'language'=>'Language'
-    }
-  }
+  describe 'check non english locales include all keys' do
+    context 'when the source is English' do
+      let(:source) { described_class::LOCALES_FILES[:english] }
 
-  second_hash = {
-    'en' => {
-      'app_name'=>'if me',
-      'app_description'=>'is a community for mental health experiences',
-      'ellipsis'=>'[...]',
-      'created'=>'<strong>Created:</strong> %{created_at}',
-      'edited'=>'<strong>Created:</strong> %{created_at} <em>(edited)</em>',
-      'salutation'=>'Hi %{name},',
-      'click_here'=>'click here',
-      'language'=>'Language'
-    }
-  }
+      context 'when comparing to Spanish' do
+        subject { described_class.compare(source, :spanish) }
+        it { is_expected.to be_empty }
+      end
 
-  describe 'compare default locale files' do
-
-    specify 'spanish locale file has nothing missing' do
-      english = './config/locales/en.yml'
-      spanish = './config/locales/es.yml'
-      expect(CompareLocalesSupport.compare(english, spanish)).to match_array([])
-    end
-
-    specify 'portuguese locale file has nothing missing' do
-      english ='./config/locales/en.yml'
-      portuguese ='./config/locales/ptbr.yml'
-      expect(CompareLocalesSupport.compare(english, portuguese)).to match_array([])
+      context 'when comparing to Portuguese' do
+        subject { described_class.compare(source, :portuguese) }
+        it { is_expected.to be_empty }
+      end
     end
   end
 
-  describe 'compare default devise locale files' do
 
-    specify 'spanish  devise locale file has nothing missing' do
-      english = './config/locales/devise.en.yml'
-      spanish = './config/locales/devise.es.yml'
-      expect(CompareLocalesSupport.compare(english, spanish)).to match_array([])
-    end
+  describe 'check non english devise locales include all keys' do
+    context 'when the source is devise_english' do
+      let(:source) { described_class::LOCALES_FILES[:devise_english] }
 
-    specify 'portuguese devise locale file has nothing missing' do
-      english ='./config/locales/devise.en.yml'
-      portuguese ='./config/locales/devise.ptbr.yml'
-      expect(CompareLocalesSupport.compare(english, portuguese)).to match_array([])
-    end
-  end
+      context 'when comparing to devise_spanish' do
+        subject { described_class.compare(source, :devise_spanish) }
+        it { is_expected.to be_empty }
+      end
 
-  describe 'compare default devise invitable locale files' do
-
-    specify 'spanish devise invitable locale file has nothing missing' do
-      english = './config/locales/devise_invitable.en.yml'
-      spanish = './config/locales/devise_invitable.es.yml'
-      expect(CompareLocalesSupport.compare(english, spanish)).to match_array([])
-    end
-
-    specify 'portuguese devise invitable locale file has nothing missing' do
-      english ='./config/locales/devise_invitable.en.yml'
-      portuguese ='./config/locales/devise_invitable.ptbr.yml'
-      expect(CompareLocalesSupport.compare(english, portuguese)).to match_array([])
+      context 'when comparing to devise_portuguese' do
+        subject { described_class.compare(source, :devise_portuguese) }
+        it { is_expected.to be_empty }
+      end
     end
   end
 
-  describe 'compare kaminari locale files' do
 
-    specify 'spanish kaminari locale file has nothing missing' do
-      english = './config/locales/kaminari.en.yml'
-      spanish = './config/locales/kaminari.es.yml'
-      expect(CompareLocalesSupport.compare(english, spanish)).to match_array([])
-    end
+  describe 'check non english devise_invitable locales include all keys' do
+    context 'when the source is devise_invitable_english' do
+      let(:source) { described_class::LOCALES_FILES[:devise_invitable_english] }
 
-    specify 'portuguese kaminari locale file has nothing missing' do
-      english ='./config/locales/kaminari.en.yml'
-      portuguese ='./config/locales/kaminari.ptbr.yml'
-      expect(CompareLocalesSupport.compare(english, portuguese)).to match_array([])
-    end
-  end
+      context 'when comparing to devise_invitable_spanish' do
+        subject { described_class.compare(source, :devise_invitable_spanish) }
+        it { is_expected.to be_empty }
+      end
 
-  describe '#compare_locale_hashes' do
-    let(:missing) { %w[less email] }
-    subject(:subject) { CompareLocalesSupport.compare_locale_hashes(main_hash, second_hash) }
-
-    it 'compares locales' do
-      expect(subject).to match_array(missing)
+      context 'when comparing to devise_invitable_portuguese' do
+        subject { described_class.compare(source, :devise_invitable_portuguese) }
+        it { is_expected.to be_empty }
+      end
     end
   end
 
-  describe '#flatten_keys' do
-    subject(:subject) { CompareLocalesSupport.flatten_keys(main_hash) }
-    let(:result) do
-      ['en.app_name', 'en.app_description', 'en.email', 'en.ellipsis', 'en.created', 'en.edited', 'en.salutation', 'en.click_here', 'en.less', 'en.language']
-    end
+  describe 'check non english kaminari_english locales include all keys' do
+    context 'when the source is kaminari_spanish' do
+      let(:source) { described_class::LOCALES_FILES[:kaminari_english] }
 
-    it 'puts keys of the hash' do
-      expect(subject).to eq(result)
+      context 'when comparing to kaminari_portuguese' do
+        subject { described_class.compare(source, :kaminari_spanish) }
+        it { is_expected.to be_empty }
+      end
+
+      context 'when comparing to devise_invitable_portuguese' do
+        subject { described_class.compare(source, :kaminari_portuguese) }
+        it { is_expected.to be_empty }
+      end
     end
+  end
+
+
+  describe 'check all locale files for missing tests' do
+    subject{Dir.glob("#{described_class::LOCALES_DIR}/*.yml")}
+    it { is_expected.to match_array(described_class::LOCALES_FILES.values) }
   end
 end
