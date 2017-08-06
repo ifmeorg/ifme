d67ceee4b67d7aadb2b1734ad88fa1f4cd079d62
Adam Rice <development@hashnotadam.com>
2017-08-06 17:09:44 +1000

    Update migrations and schema

--- db/migrate/20161127002019_create_strategy_reminders.rb
+++ db/migrate/20161127002019_create_strategy_reminders.rb
@@ -1,4 +1,4 @@
-class CreateStrategyReminders < ActiveRecord::Migration
+class CreateStrategyReminders < ActiveRecord::Migration[4.2]
   def change
     create_table :perform_strategy_reminders do |t|
       t.integer :strategy_id, null: false
--- db/migrate/20170724160338_add_refresh_token_to_user.rb
+++ db/migrate/20170724160338_add_refresh_token_to_user.rb
@@ -1,4 +1,4 @@
-class AddRefreshTokenToUser < ActiveRecord::Migration
+class AddRefreshTokenToUser < ActiveRecord::Migration[4.2]
   def change
     add_column :users, :refresh_token, :string
   end
