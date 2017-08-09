efcf99937cddf594e10680dc9cbf10664f7471f4
Sophie <sophiermcdonald@gmail.com>
2017-08-09 16:56:33 +1000

    revert namstars unlucky change

--- app/controllers/secret_shares_controller.rb
+++ app/controllers/secret_shares_controller.rb
@@ -1,8 +1,16 @@
+# frozen_string_literal: true
+
 class SecretSharesController < ApplicationController
 
+  skip_before_action :if_not_signed_in, except: :create
+
   def create
-    moment = Moment.find(params[:moment])
+    moment = Moment.friendly.find(params[:moment])
     moment.update!(secret_share_identifier: SecureRandom.uuid)
     redirect_to moment_path(moment)
   end
+
+  def show
+  end
+
 end
