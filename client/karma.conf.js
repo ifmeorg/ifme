adcfd761d49f38bf763d386ed85588842b39bf80
nma <nickma38@gmail.com>
2017-08-11 01:53:28 -0400

    more linting and formatting fixes

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -1,38 +1,40 @@
 // karma.conf.js
-var webpackConfig = require('./webpack.config');
+const webpackConfig = require('./webpack.config');
 const webpack = require('webpack');
 
 webpackConfig.plugins = [
-    new webpack.EnvironmentPlugin({
-        NODE_ENV: 'development', // use 'development' unless process.env.NODE_ENV is defined
-        DEBUG: false,
-    }),
+  new webpack.EnvironmentPlugin({
+    NODE_ENV: 'development', // use 'development' unless process.env.NODE_ENV is defined
+    DEBUG: false,
+  }),
 ];
 webpackConfig.devtool = 'eval-source-map';
 
 // https://github.com/airbnb/enzyme/blob/master/docs/guides/webpack.md#react-15-compatibility
 webpackConfig.externals = {
-    'react/addons': true,
-    'react/lib/ExecutionEnvironment': true,
-    'react/lib/ReactContext': true,
-    'react-addons-test-utils': 'react-dom',
+  'react/addons': true,
+  'react/lib/ExecutionEnvironment': true,
+  'react/lib/ReactContext': true,
+  'react-addons-test-utils': 'react-dom',
 };
 
 module.exports = function (config) {
-    config.set({
-        browsers: ['PhantomJS'],
-        singleRun: true,
-        frameworks: ['jasmine'],
-        files: [
-            { pattern: 'app/**/*.spec.jsx', watched: true },
-        ],
-        preprocessors: {
-            'app/**/*.jsx': ['webpack', 'sourcemap']
-        },
-        reporters: ['dots'],
-        webpack: webpackConfig,
-        webpackServer: {
-            noInfo: true
-        }
-    });
+  config.set({
+    browsers: ['PhantomJS'],
+    singleRun: true,
+    frameworks: ['jasmine'],
+    files: [
+      { pattern: 'app/**/*.spec.js', watched: true },
+      { pattern: 'app/**/*.spec.jsx', watched: true },
+    ],
+    preprocessors: {
+      'app/**/*.js': ['webpack', 'sourcemap'],
+      'app/**/*.jsx': ['webpack', 'sourcemap'],
+    },
+    reporters: ['dots'],
+    webpack: webpackConfig,
+    webpackServer: {
+      noInfo: true,
+    },
+  });
 };
