67df5fefd5fa7cf67123847e654d13f8063c3c35
Adam Rice <development@hashnotadam.com>
2017-08-15 11:29:08 +1000

    Setup new CircleCI configuration
    CircleCI has been upgraded to version 2.0 and this update uses a new
    configuration format

--- spec/features/user_creates_strategy_spec.rb
+++ spec/features/user_creates_strategy_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 describe 'UserCreatesAStrategy', js: true do
   let(:user) { create :user2, :with_allies }
   let!(:category) { create :category, userid: user.id }
@@ -8,7 +10,7 @@ describe 'UserCreatesAStrategy', js: true do
   end
 
   feature 'Creating, viewing, and editing a strategy' do
-    it 'is successful' do
+    specify do
       login_as user
       visit strategies_path
 
@@ -16,12 +18,18 @@ describe 'UserCreatesAStrategy', js: true do
         expect(page).to have_content 'Strategies'
       end
 
-      expect(page).to have_content 'Strategize self-care to achieve desired thoughts and attitudes towards your moments.'
-      expect(page).to have_content 'You haven\'t created any custom strategies yet.'
+      expect(page).to have_content(
+        'Strategize self-care to achieve desired thoughts and attitudes ' \
+        'towards your moments.'
+      )
+      expect(page).to have_content(
+        "You haven't created any custom strategies yet."
+      )
       expect(page).to have_content 'Five Minute Meditation'
 
       # CREATING
       page.find('a[title="New Strategy"]').click
+
       within '#page_title_content' do
         expect(page).to have_content 'New Strategy'
       end
@@ -80,7 +88,9 @@ describe 'UserCreatesAStrategy', js: true do
         expect(page).to have_content 'Edit My new strategy'
       end
 
-      fill_in_ckeditor('strategy_description', with: 'I am changing my strategy description')
+      fill_in_ckeditor(
+        'strategy_description', with: 'I am changing my strategy description'
+      )
 
       page.find('input[value="Submit"]').click
 
