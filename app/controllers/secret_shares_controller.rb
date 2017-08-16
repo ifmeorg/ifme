b879945f0ef4d06873ad7d4368eb4f260397f5f0
Sophie <sophiermcdonald@gmail.com>
2017-08-16 10:50:55 +1000

    Add test and remove button and url for not signed in

--- app/controllers/secret_shares_controller.rb
+++ app/controllers/secret_shares_controller.rb
@@ -5,6 +5,7 @@ class SecretSharesController < ApplicationController
   skip_before_action :if_not_signed_in, only: [:show]
 
   def create
+    #need to add an authorisation check here
     moment = Moment.friendly.find(params[:moment])
     moment.update!(secret_share_identifier: SecureRandom.uuid)
     redirect_to moment_path(moment)
@@ -14,6 +15,5 @@ class SecretSharesController < ApplicationController
     @moment = Moment.find_by(secret_share_identifier: params[:id])
     render 'moments/show'
   end
-
 
 end
--- spec/controllers/secret_shares_controller_spec.rb
+++ spec/controllers/secret_shares_controller_spec.rb
@@ -3,13 +3,29 @@
 describe SecretSharesController, type: :controller do
   let(:moment) { create(:moment, :with_user) }
   describe 'POST create' do
-    before do
-      sign_in moment.user
-      post :create, moment: moment
+    context 'signed in as creator of the moment' do
+      before do
+        sign_in moment.user
+        post :create, moment: moment
+      end
+
+      it 'Creates Secret Share Identifier' do
+        expect(moment.reload.secret_share_identifier).not_to be_nil
+      end
     end
 
-    it 'Creates Secret Share Identifier' do
-      expect(moment.reload.secret_share_identifier).not_to be_nil
+    context 'not signed in' do
+      before do
+        post :create, moment: moment
+      end
+
+      it 'does not create Secret Share Identifier' do
+        expect(moment.reload.secret_share_identifier).to be_nil
+      end
+
+      it 'redirects to sign in page' do
+        expect(response).to redirect_to new_user_session_path
+      end
     end
   end
 
