031f31f99bbf502e19aeff049feed00a49c2ef1a
Timothy Ng <hello@timothyng.xyz>
2017-08-19 22:06:00 +0800

    [#534] Moved auth contexts within each describe block

--- spec/controllers/moods_controller_spec.rb
+++ spec/controllers/moods_controller_spec.rb
@@ -1,23 +1,20 @@
 RSpec.describe MoodsController, type: :controller do
   let(:user) { create(:user1) }
 
-  context "user is not signed in" do
-    subject { get :index }
-    it "redirects to sign in page" do
-      expect(subject).to redirect_to new_user_session_url
-    end
-  end
-
-  context "user is signed in" do
-    before do
-      sign_in user
-    end
-
-    describe "GET #index" do
+  describe "GET #index" do
+    context "when the user is logged in" do
+      before do
+        sign_in user
+      end
       it "renders the page" do
         get :index
         expect(response).to render_template(:index)
       end
+    end
+
+    context "when the user is not logged in" do
+      before { get :index }
+      it_behaves_like :with_no_logged_in_user
     end
   end
 end
