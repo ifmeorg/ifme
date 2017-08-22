0da788113bc74ef0d3858f00f6ed912984988f9e
Timothy Ng <hello@timothyng.xyz>
2017-08-22 16:45:47 +0800

    Added spec for CategoriesController#edit

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -64,6 +64,28 @@ RSpec.describe CategoriesController, type: :controller do
     end
   end
 
+  describe 'GET #edit' do
+    context 'when the user is logged in' do
+      include_context :logged_in_user
+      context 'user is trying to edit a category they created' do
+        it 'renders the edit form' do
+          get :edit, params: { id: category.id }
+          expect(response).to render_template(:edit)
+        end
+      end
+      context 'user is trying to edit a category another user created' do
+        it 'redirects to the mood path' do
+          get :edit, params: { id: other_category.id }
+          expect(response).to redirect_to category_path(other_category)
+        end
+      end
+    end
+    context 'when the user is not logged in' do
+      before { get :edit, params: { id: category.id } }
+      it_behaves_like :with_no_logged_in_user
+    end
+  end
+
   describe 'POST #create' do
     context 'when the user is logged in' do
       include_context :logged_in_user
@@ -86,4 +108,6 @@ RSpec.describe CategoriesController, type: :controller do
       it_behaves_like :with_no_logged_in_user
     end
   end
+
+
 end
