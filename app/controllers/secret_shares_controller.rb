6636cec1e2462e889702d8ac7e6bc89e7952381f
Sophie <sophiermcdonald@gmail.com>
2017-08-28 11:22:40 +1000

    Autorisation check and delete secret share with specs

--- app/controllers/secret_shares_controller.rb
+++ app/controllers/secret_shares_controller.rb
@@ -5,8 +5,8 @@ class SecretSharesController < ApplicationController
   skip_before_action :if_not_signed_in, only: [:show]
 
   def create
-    #need to add an authorisation check here
-    moment = Moment.friendly.find(params[:moment])
+    #Authorisation check will automatically raise ActiveRecord::RecordNotFound
+    moment = Moment.where(userid: current_user.id).friendly.find(params[:moment])
     moment.update!(secret_share_identifier: SecureRandom.uuid, secret_share_expires_at: 1.day.from_now)
     redirect_to moment_path(moment)
   end
@@ -21,8 +21,8 @@ class SecretSharesController < ApplicationController
     end
   end
 
-  def delete
-    moment = Moment.friendly.find(params[:moment])
+  def destroy
+    moment = Moment.find_by!(userid: current_user.id, secret_share_identifier: params[:id])
     moment.update!(secret_share_identifier: nil)
     redirect_to moment_path(moment)
   end
--- spec/controllers/secret_shares_controller_spec.rb
+++ spec/controllers/secret_shares_controller_spec.rb
@@ -14,6 +14,14 @@ describe SecretSharesController, type: :controller do
       end
     end
 
+    context 'signed in as another user' do
+      before do
+        new_user = create(:user)
+        sign_in new_user
+      end
+      specify { expect { post :create, params: { moment: moment } }.to raise_error(ActiveRecord::RecordNotFound) }
+    end
+
     context 'not signed in' do
       before do
         post :create, params: { moment: moment }
@@ -30,16 +38,52 @@ describe SecretSharesController, type: :controller do
   end
 
   describe 'GET show' do
-    before { get :show, params: { id: moment.secret_share_identifier } }
 
     context 'secret share has expired' do
       let(:moment) { create(:moment, :with_user, :with_secret_share, secret_share_expires_at: 1.day.ago) }
-      specify { expect(response.status).to eq(404) }
+      specify { expect { get :show, params: { id: moment.secret_share_identifier } }.to raise_error(ActiveRecord::RecordNotFound) }
     end
 
     context 'secret share is valid' do
+      before { get :show, params: { id: moment.secret_share_identifier } }
       let(:moment) { create(:moment, :with_user, :with_secret_share) }
       specify { expect(response).to render_template(:show) }
+    end
+  end
+
+  describe 'DELETE' do
+    let(:moment) { create(:moment, :with_user, :with_secret_share) }
+    context 'signed in as creator of the moment' do
+      before do
+        sign_in moment.user
+        delete :destroy, params: { id: moment.secret_share_identifier }
+      end
+
+      it 'Deletes Secret Share Identifier' do
+        expect(moment.reload.secret_share_identifier).to be_nil
+      end
+    end
+
+    context 'signed in as another user' do
+      before do
+        new_user = create(:user)
+        sign_in new_user
+      end
+      specify { expect { delete :destroy, params: { id: moment.secret_share_identifier } }.to raise_error(ActiveRecord::RecordNotFound) }
+    end
+
+    context 'not signed in' do
+      before do
+        delete :destroy, params: { id: moment.secret_share_identifier }
+      end
+
+      it 'does not delete Secret Share Identifier' do
+        expect(moment.reload.secret_share_identifier).not_to be_nil
+      end
+
+      it 'redirects to sign in page' do
+        expect(response).to redirect_to new_user_session_path
+      end
     end
   end
 end
