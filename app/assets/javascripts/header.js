71fa9fdf21d33ded15f3f8c4985c40539ebc1ff4
Bao Thien Ngo <4228012+baohouse@users.noreply.github.com>
2017-09-06 12:09:13 -0700

    Simplifying HTML and CSS classes
    Moved to using a container for the secondary navigations and control the animation height for the container rather than for each individual secondary navigation list.

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,5 +1,6 @@
 (function() {
   var currentTab;
+  var animationTimespan = 300; // in ms; this must match header.scss 'transition: max-height' value
   var smallNavSpacerHeight;
 
   function setHeight(override) {
@@ -9,7 +10,37 @@
   }
 
   function toggleArrow() {
-    $('.header-logo .expand').toggleClass("fa-sort-desc").toggleClass("fa-sort-asc");
+    $('.header-logo .expand').toggleClass('fa-sort-desc').toggleClass('fa-sort-asc');
+  }
+
+  function toggleVisibility($el) {
+    return $el.toggleClass('display_none').toggleClass('display_block');
+  }
+
+  function hide($el) {
+    return $el.addClass('display_none').removeClass('display_block');
+  }
+
+  function show($el) {
+    return $el.removeClass('display_none').addClass('display_block');
+  }
+
+  function getVisibleSecondaryNavs() {
+    return $('.secondary-nav > ul.display_block');
+  }
+
+  /**
+   * We add a delay so that the 'collapsed' CSS class can properly animate the transition
+   * before we display:none the content (which causes the container to collapse immediately).
+   */
+  function hideVisibleSecondaryNavsWithTransition() {
+    $('.secondary-nav')
+      .addClass('collapsed')
+      .delay(animationTimespan)
+      .promise()
+      .then(function () {
+        toggleVisibility(getVisibleSecondaryNavs());
+      });
   }
 
   function primaryNavMouseenterHandler(e) {
@@ -24,37 +55,37 @@
         toggleArrow();
       }
 
-      var $visibleSecondaryNavs = $('.secondary-nav:not(.transition-hidden)');
+      var $visibleSecondaryNavs = getVisibleSecondaryNavs();
       var $newSecondaryNav = $('ul[data-primary-parent=' + newTab + ']');
       if ($visibleSecondaryNavs.length) {
         if ($newSecondaryNav.length) {
-          // Hacky way of skipping CSS transition animation
-          $visibleSecondaryNavs.css('transition', 'none').toggleClass('transition-hidden');
-          $newSecondaryNav.css('transition', 'none').toggleClass('transition-hidden');
-          setTimeout(function() {
-            $visibleSecondaryNavs.css('transition', '');
-            $newSecondaryNav.css('transition', '');
-          }, 0);
+          // Transition between two secondary navs
+          toggleVisibility($newSecondaryNav);
+          toggleVisibility($visibleSecondaryNavs);
         } else {
-          $visibleSecondaryNavs.toggleClass('transition-hidden');
+          // Transition from visible to no secondary navs
+          hideVisibleSecondaryNavsWithTransition();
         }
       } else {
-        $newSecondaryNav.toggleClass('transition-hidden');
+        // Transition from no secondary navs to potentially visible
+        $('.secondary-nav').removeClass('collapsed');
+        toggleVisibility($newSecondaryNav);
       }
     }
   }
 
   function primaryNavMouseleaveHandler() {
     currentTab = null;
-    var $visibleSecondaryNavs = $('.secondary-nav:not(.transition-hidden)');
-    $visibleSecondaryNavs.toggleClass('transition-hidden');
+    var $visibleSecondaryNavs = getVisibleSecondaryNavs();
     if ($visibleSecondaryNavs.is('[data-primary-parent=me]')) {
       toggleArrow();
     }
+    hideVisibleSecondaryNavsWithTransition();
   }
 
   function resizeHandler() {
-    var smallNavIsVisible = $('#small_nav').is(':visible');
+    var $smallNav = $('#small_nav');
+    var smallNavIsVisible = $smallNav.is(':visible');
     if (smallNavIsVisible) {
       /*
        * If we're resizing while the smallNav is open, we need to force the spacer to retain the height
@@ -62,28 +93,20 @@
        * when available, or determine its value by way of a quick close/measure/open process.
        */
       if (!_.isNumber(smallNavSpacerHeight)) {
-        closeSmallNav();
+        hide($smallNav);
         smallNavSpacerHeight = $('.small-screen').height();
-        openSmallNav();
+        show($smallNav);
       }
       setHeight(smallNavSpacerHeight);
 
     } else {
       /*
-       * Just in case it's open and the user resizes the window and triggers the medium screen size breakpoint
-       * close the smallNav.
+       * Just in case smallNav is open, and the user resizes the window and triggers the
+       * medium screen size breakpoint, we'll close the smallNav.
        */
-      closeSmallNav();
+      hide($smallNav);
       setHeight();
     }
-  }
-
-  function closeSmallNav() {
-    $('#small_nav').addClass('display_none').removeClass('display_block');
-  }
-
-  function openSmallNav() {
-    $('#small_nav').removeClass('display_none').addClass('display_block');
   }
 
   loadPage(function() {
@@ -95,7 +118,7 @@
 
     // Mobile menu toggling
     $('#expand_nav').click(function() {
-      $('#small_nav').toggleClass('display_none').toggleClass('display_block');
+      toggleVisibility($('#small_nav'));
       if ($('#small_nav').is('.display_none')) {
         setHeight();
       }
