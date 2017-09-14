c3ecb9d858871cce371bf7e90ee9440808264b5a
Namster <jennynamster@gmail.com>
2017-09-15 08:32:24 +1000

    Fix foreman bootstrap script (#626)
    * Update local environment to support react
    
    * Ensure all dependencies, including webpack are installed before build.
    
    * Get rid of accidentally committed schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -231,8 +231,8 @@ ActiveRecord::Schema.define(version: 20170830075513) do
     t.datetime "invitation_sent_at"
     t.datetime "invitation_accepted_at"
     t.integer  "invitation_limit"
-    t.string   "invited_by_type"
     t.integer  "invited_by_id"
+    t.string   "invited_by_type"
     t.integer  "invitations_count",      default: 0
     t.boolean  "comment_notify"
     t.boolean  "ally_notify"
