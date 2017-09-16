321eff3047bfe5b1f325fcd00e47687c2f3f059d
Bao Thien Ngo <4228012+baohouse@users.noreply.github.com>
2017-09-15 17:35:17 -0700

    Adding Cloudinary remote-image fetching and scaling
    Changed fallback image technique. Updated tests.
    Streamlined CSS.

--- spec/services/profile_picture_spec.rb
+++ spec/services/profile_picture_spec.rb
@@ -1,62 +1,31 @@
 # frozen_string_literal: true
 
+CLOUDINARY_URL = 'https://res.cloudinary.com/image/upload/ABC123.jpg'
+
 describe ProfilePicture do
   subject { described_class }
 
   describe '.fetch' do
-    before do
-      allow(ProfilePicture).to receive(:avatar_url).and_return('some_avatar_url')
+    it 'returns HTML with class name and path for normal portraits' do
+      expect(subject.fetch('some_image_url', 'some_class')).to \
+      have_tag('img', :with => {
+        :src => 'some_image_url',
+        :class => 'some_class'
+      })
     end
 
-    it 'returns some HTML with the class name and the avatar path' do
-      expect(subject.fetch('some_avatar_url', 'some_class')).to eq(
-        "<div class='some_class' style='background: url(some_avatar_url)'></div>"
-      )
+    it 'returns HTML with class name and path for Cloudinary portraits' do
+      expect(subject.fetch(CLOUDINARY_URL, 'some_class')).to \
+      have_tag('img', :with => {
+        :class => 'some_class'
+      })
     end
   end
 
-  describe '.avatar_url' do
-    context 'when avatar is present' do
-      context 'and is a contributor avatar' do
-        let(:avatar) { '/assets/contributors/yugi-mutoh' }
-
-        it 'returns the avatar as-is' do
-          expect(subject.send(:avatar_url, avatar)).to eq(avatar)
-        end
-      end
-
-      context 'and is not a contributor avatar' do
-        let(:avatar) { 'http://www.if-me.org/totally-valid' }
-
-        before do
-          response_double = double('Net::HTTPResponse', code: response_code)
-          allow(Net::HTTP).to receive(:get_response).and_return(response_double)
-        end
-
-        context 'and URL is valid' do
-          let(:response_code) { 200 }
-
-          it 'returns the avatar as-is' do
-            expect(subject.send(:avatar_url, avatar)).to eq(avatar)
-          end
-        end
-
-        context 'and URL is not valid' do
-          let(:response_code) { 400 }
-
-          it 'returns the default avatar path' do
-            expect(subject.send(:avatar_url, avatar)).to eq(ProfilePicture::DEFAULT_AVATAR)
-          end
-        end
-      end
-    end
-
-    context 'when avatar is not present' do
-      let(:avatar) { nil }
-
-      it 'returns the default avatar path' do
-        expect(subject.send(:avatar_url, avatar)).to eq(ProfilePicture::DEFAULT_AVATAR)
-      end
+  describe '.get_cloudinary_image_id' do
+    it 'returns id for a Cloudinary URL' do
+      expect(subject.send(:get_cloudinary_image_id, CLOUDINARY_URL)).to \
+      eq('ABC123')
     end
   end
 end
