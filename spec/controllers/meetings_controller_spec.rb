5a6e9b0a23238806c6163aee4695907524b70e9a
Ricardo Eihara <ricardo.hejr@gmail.com>
2017-10-04 18:54:20 -0300

    Add delete_comment specs for meetings_controller

--- spec/controllers/meetings_controller_spec.rb
+++ spec/controllers/meetings_controller_spec.rb
@@ -69,4 +69,111 @@ RSpec.describe MeetingsController, :type => :controller do
       end
     end
   end
+
+  describe 'POST #comment' do
+    let(:user) { create(:user) }
+    let(:meeting) { create(:meeting) }
+    let(:comment) { build(:comment, comment_by: user.id, commentable_type: 'meeting') }
+    let(:valid_comment_params) do
+      comment.attributes.merge(commentable_id: meeting.id)
+    end
+    let(:invalid_comment_params) { comment.attributes }
+
+    context 'when the user is logged in' do
+      include_context :logged_in_user
+
+      context 'when the comment is saved' do
+        it 'responds with an OK status' do
+          post :comment, params: valid_comment_params
+          expect(response.status).to eq(200)
+        end
+      end
+
+      context 'when the comment is not saved' do
+        it 'responds with json no_save: true' do
+          post :comment, params: invalid_comment_params
+          expect(response.body).to eq({ no_save: true }.to_json)
+        end
+      end
+    end
+
+    context 'when the user is not logged in' do
+      before { post :comment }
+
+      it_behaves_like :with_no_logged_in_user
+    end
+  end
+
+  describe 'GET #delete_comment' do
+    let(:user) { create(:user, id: 1) }
+    let(:meeting) { create(:meeting, id: 1) }
+    let!(:meeting_member) do
+      create(:meeting_member, meeting: meeting, user: user, leader: true)
+    end
+    let!(:comment) do
+      create(
+        :comment,
+        id: 1,
+        comment_by: 1,
+        commentable_id: 1,
+        commentable_type: 'meeting',
+        visibility: 'all'
+      )
+    end
+
+    context 'when the user is logged in' do
+      include_context :logged_in_user
+
+      context 'when the comment exists and belongs to the current_user' do
+        it 'destroys the comment' do
+          expect { get :delete_comment, params: { commentid: 1 } }.to(
+            change(Comment, :count).by(-1)
+          )
+        end
+
+        it 'renders nothing' do
+          get :delete_comment, params: { commentid: 1 }
+
+          expect(response.body).to eq('')
+        end
+      end
+
+      context 'when the comment exists and the strategy belongs to the current_user' do
+        let!(:comment) do
+          create(
+            :comment, id: 1, comment_by: 1, commentable_id: 1, visibility: 'all'
+          )
+        end
+        let!(:new_moment) { create(:moment, id: 1, userid: 1) }
+
+        it 'destroys the comment' do
+          expect { get :delete_comment, params: { commentid: 1 } }.to(
+            change(Comment, :count).by(-1)
+          )
+        end
+
+        it 'renders nothing' do
+          comment
+          get :delete_comment, params: { commentid: 1 }
+
+          expect(response.body).to eq('')
+        end
+      end
+
+      context 'when the comment does not exist' do
+        it 'renders nothing' do
+          get :delete_comment, params: { commentid: 99 }
+          expect(response.body).to eq('')
+        end
+      end
+    end
+
+    context 'when the user is not logged in' do
+      before do
+        get :delete_comment
+      end
+
+      it_behaves_like :with_no_logged_in_user
+    end
+  end
 end
