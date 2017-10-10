dc178fa26db5b1c04ebab395f515542a889d8c0c
Álvaro Casado Coscollá <alvarocasadoc@gmail.com>
2017-10-10 07:58:50 +0200

    Added a migration for setting every post's updated_at field as default value for its publication date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171006164223) do
+ActiveRecord::Schema.define(version: 20171010054721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
