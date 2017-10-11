ca39ed6e4eb51daf9f565bdaa242de41bc1a4d97
Tsara Fatma L Sudrajat <tsara.tf@gmail.com>
2017-10-11 10:41:37 +0700

    Persisting browser locale after sign in #703

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
-class SessionsController < ApplicationController
+class SessionsController < Devise::SessionsController
+  def create
+    super
+    set_user_locale if user_signed_in?
+  end
+
+  private
+
+  def set_user_locale
+    unless cookies[:locale].eql?(I18n.default_locale)
+      current_user.update(locale: cookies[:locale])
+    end
+  end
 end
