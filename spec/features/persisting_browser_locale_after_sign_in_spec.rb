d019846edf0dce65aaa8113ebed4a645cbb0655b
Sturm-und-Drang <kevinplaut@gmail.com>
2017-10-17 23:34:07 -0700

    Updated expects to add visible: true to wait until subtitle is loaded

--- spec/features/persisting_browser_locale_after_sign_in_spec.rb
+++ spec/features/persisting_browser_locale_after_sign_in_spec.rb
@@ -5,8 +5,9 @@ RSpec.feature 'Persisting browser locale after sign in', type: :feature do
     login_as user
     visit moments_path
 
-    expect(page).to have_content('Delve deep into your moments - events and ' \
-                                 'situations that affect your mental health.')
+    expect(page).to have_selector('.subtitle', visible: true,
+                                  text: 'Delve deep into your moments - events and ' \
+                                  'situations that affect your mental health.')
 
     within('span#title_expand') { find('i.expand').click }
     within('ul#expand_me') { find('a[href="/users/sign_out"]').click }
@@ -14,7 +15,8 @@ RSpec.feature 'Persisting browser locale after sign in', type: :feature do
     login_as user
     visit moments_path
 
-    expect(page).to have_content('Profundiza en tus Momentos - eventos y ' \
-                                 'situaciones que afectan tu salud mental')
+    expect(page).to have_selector('.subtitle', visible: true,
+                                  text: 'Profundiza en tus Momentos - eventos y ' \
+                                  'situaciones que afectan tu salud mental')
   end
 end
