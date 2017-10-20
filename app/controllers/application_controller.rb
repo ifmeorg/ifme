1e847b06b177c0a2f9111dd8b5a8148872d510bb
“Vilde <vilde@vevatne.no>
2017-10-19 20:07:07 -0700

    Add devise_invitable file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,8 @@ class ApplicationController < ActionController::Base
       { name: t('languages.nl'), locale: :nl },
       { name: t('languages.ptbr'), locale: :ptbr },
       { name: t('languages.sv'), locale: :sv },
-      { name: t('languages.it'), locale: :it }
+      { name: t('languages.it'), locale: :it },
+      { name: t('languages.no'), locale: :no}
     ].freeze
     @locale = I18n.locale = locale
   end
