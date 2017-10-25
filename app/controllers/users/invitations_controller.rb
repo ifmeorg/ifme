163969c8931fd78d1e1cdd1cae2c9f8ef3372ed7
haleymnatz <haleymnatz@gmail.com>
2017-10-25 14:37:03 -0400

    Emails sending w/errors

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -3,19 +3,30 @@
 module Users
   class InvitationsController < Devise::InvitationsController
     # POST /resource/invitation
+
+    def invite_resource(&block)
+      resource_class.invite!(invite_params, current_inviter, &block)
+    end
+
+
     def create
-      self.resource = invite_resource
-      resource_invited = resource.errors.empty?
+      invitees = params[:user][:email].split(/,\s*/)
+      invitees.each do |invitee|
+        resource = User.invite!({:email => invitee}, current_user)
+        resource_invited = resource.errors.empty?
 
-      yield resource if block_given?
+        puts "WAZZZZUP #{invite_params}"
+        puts "#{resource_invited}"
+        # yield resource if block_given?
 
-      if resource_invited
-        if is_flashing_format? && resource.invitation_sent_at
-          set_flash_message :notice, :send_instructions, email: resource.email
+        if resource_invited
+          if is_flashing_format? && resource.invitation_sent_at
+            set_flash_message :notice, :send_instructions, email: resource.email
+          end
+          respond_with resource, location: new_user_invitation_path
+        else
+          respond_with_navigational(resource) { render :new }
         end
-        respond_with resource, location: new_user_invitation_path
-      else
-        respond_with_navigational(resource) { render :new }
       end
     end
   end
