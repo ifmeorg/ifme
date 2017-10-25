e5004eb750186cc27d1fa1758a35478e7ad148f4
haleymnatz <haleymnatz@gmail.com>
2017-10-25 16:23:04 -0400

    Remove invite_resource method

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -4,20 +4,14 @@ module Users
   class InvitationsController < Devise::InvitationsController
     # POST /resource/invitation
 
-    def invite_resource(&block)
-      resource_class.invite!(invite_params, current_inviter, &block)
-    end
-
-
     def create
-      successful_invites =[]
+      successful_invites = []
       invitees = params[:user][:email].split(/,\s*/)
       invitees.each do |invitee|
-        resource = User.invite!({:email => invitee}, current_user)
-        resource_invited = resource.errors.empty?
-
-        if resource_invited
-          successful_invites << invitee
+        if invitee =~ Devise.email_regexp
+          resource = User.invite!({:email => invitee}, current_user)
+          resource_invited = resource.errors.empty?
+          successful_invites << invitee if resource_invited
         end
       end
 
