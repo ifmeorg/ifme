473749b0c0c8120c7fc8a31f5834492be30b14ae
haleymnatz <haleymnatz@gmail.com>
2017-10-26 11:26:41 -0400

    Add error message for invalid emails

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -6,17 +6,23 @@ module Users
 
     def create
       successful_invites = []
+      failed_invites = []
       invitees = params[:user][:email].split(/,\s*/)
       invitees.each do |invitee|
-        if invitee =~ Devise.email_regexp
-          resource = User.invite!({:email => invitee}, current_user)
-          resource_invited = resource.errors.empty?
-          successful_invites << invitee if resource_invited
+        resource = User.invite!({ email: invitee }, current_user)
+        resource_invited = resource.errors.empty?
+        if resource_invited
+          successful_invites << invitee
+        else
+          failed_invites << invitee
         end
       end
 
       if is_flashing_format? && !successful_invites.empty?
         set_flash_message :notice, :send_instructions, email: successful_invites.join(', ')
+      end
+      if failed_invites
+        set_flash_message :alert, :failed_send, email: failed_invites.join(', ')
       end
       redirect_to new_user_invitation_path
     end
