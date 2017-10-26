499d50ba4d36e9f9aa9985f7c67fdc9a8b96bf04
jsrn <james.srn@gmail.com>
2017-10-26 23:21:48 +0100

    Use a single quoted string.

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -4,7 +4,7 @@ class LocalesController < ApplicationController
   skip_before_action :if_not_signed_in
 
   def set_initial_locale
-    cookies["locale"] = params[:locale]
+    cookies['locale'] = params[:locale]
     redirect_to root_path
   end
 end
