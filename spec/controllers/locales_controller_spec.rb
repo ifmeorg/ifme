9d743a2b364508b114ce2235d6fb7a2e3f3f58d4
jsrn <james.srn@gmail.com>
2017-10-26 23:21:48 +0100

    Use a single quoted string.

--- spec/controllers/locales_controller_spec.rb
+++ spec/controllers/locales_controller_spec.rb
@@ -3,7 +3,7 @@
 RSpec.describe LocalesController, type: :controller do
   %w(en es nl ptbr sv).each do |locale|
     describe "GET ##{locale}" do
-      it "sets a locale cookie when prompted" do
+      it 'sets a locale cookie when prompted' do
         get :set_initial_locale, params: { locale: locale }
         expect(response).to be_redirect
         expect(response.cookies['locale']).to eq(locale)
