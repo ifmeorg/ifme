21e9209ceed62277451816ca149c576307a07532
haleymnatz <haleymnatz@gmail.com>
2017-10-26 14:54:30 -0400

    Fix rubocop errors

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -10,7 +10,8 @@ module Users
       invitees = params[:user][:email].split(/,\s*/)
       invitees.each do |invitee|
         resource = User.invite!({ email: invitee }, current_user)
-        (resource.errors.empty? ? successful_invites : failed_invites) << invitee
+        invited = resource.errors.empty?
+        (invited ? successful_invites : failed_invites) << invitee
       end
       invitation_flash_messages(successful_invites, failed_invites)
       redirect_to new_user_invitation_path
@@ -18,13 +19,11 @@ module Users
 
     private
 
-    def invitation_flash_messages(invites, failures)
+    def invitation_flash_messages(invites, fails)
       if invites && is_flashing_format?
         set_flash_message :notice, :send_instructions, email: invites.join(', ')
       end
-      if failures
-        set_flash_message :alert, :failed_send, email: failures.join(', ')
-      end
+      set_flash_message :alert, :failed_send, email: fails.join(', ') if fails
     end
   end
 end
