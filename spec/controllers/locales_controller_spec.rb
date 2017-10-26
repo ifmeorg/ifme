3a138140bb7f3634dc9a154636ded8dced0dd53b
jsrn <james.srn@gmail.com>
2017-10-26 22:44:24 +0100

    Test the locale redirects.

---
+++ spec/controllers/locales_controller_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+RSpec.describe LocalesController, type: :controller do
+  %w(en es nl ptbr sv gr).each do |locale|
+    describe "GET ##{locale}" do
+      it "sets a locale cookie when prompted" do
+        get :set_initial_locale, params: { locale: locale }
+        expect(response).to be_redirect
+        expect(response.cookies['locale']).to eq(locale)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
+end
