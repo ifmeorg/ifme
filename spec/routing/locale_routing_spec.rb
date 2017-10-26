9ff31b3b4becc61225f93e8f26b8bd85d6a2c80c
jsrn <james.srn@gmail.com>
2017-10-26 23:35:12 +0100

    It occurs to me that we can just pluck the list of available locales from the config.

--- spec/routing/locale_routing_spec.rb
+++ spec/routing/locale_routing_spec.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 RSpec.describe 'locale routing', type: :routing do
-  %w(en es nl ptbr sv).each do |locale|
+  Rails.configuration.i18n.available_locales.each do |locale|
     it 'should route all of our locale shortcuts' do
-      expect(get: locale).to be_routable
-      expect(get: locale).to route_to(
+      expect(get: locale.to_s).to be_routable
+      expect(get: locale.to_s).to route_to(
         controller: 'locales',
         action: 'set_initial_locale',
-        locale: locale
+        locale: locale.to_s
       )
     end
   end
