4958f7fabca15ce5a8ca84a2bccee0c054112d4a
haleymnatz <haleymnatz@gmail.com>
2017-10-26 13:46:08 -0400

    Fix rubocop errors and add form placeholder

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -10,21 +10,21 @@ module Users
       invitees = params[:user][:email].split(/,\s*/)
       invitees.each do |invitee|
         resource = User.invite!({ email: invitee }, current_user)
-        resource_invited = resource.errors.empty?
-        if resource_invited
-          successful_invites << invitee
-        else
-          failed_invites << invitee
-        end
+        (resource.errors.empty? ? successful_invites : failed_invites) << invitee
       end
-
-      if is_flashing_format? && !successful_invites.empty?
-        set_flash_message :notice, :send_instructions, email: successful_invites.join(', ')
-      end
-      if failed_invites
-        set_flash_message :alert, :failed_send, email: failed_invites.join(', ')
-      end
+      invitation_flash_messages(successful_invites, failed_invites)
       redirect_to new_user_invitation_path
+    end
+
+    private
+
+    def invitation_flash_messages(invites, failures)
+      if invites && is_flashing_format?
+        set_flash_message :notice, :send_instructions, email: invites.join(', ')
+      end
+      if failures
+        set_flash_message :alert, :failed_send, email: failures.join(', ')
+      end
     end
   end
 end
