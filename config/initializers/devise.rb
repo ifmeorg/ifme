b24119ab3cc9b07c30936d4b1ae7a3041c59787a
haleymnatz <haleymnatz@gmail.com>
2017-11-02 09:52:31 -0400

    Subject in invite email displays inviters name

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,7 @@ Devise.setup do |config|
   config.mailer_sender = ENV['SMTP_ADDRESS']
 
   # Configure the class responsible to send e-mails.
-  config.mailer = 'Devise::Mailer'
+  config.mailer = 'CustomDeviseMailer'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
