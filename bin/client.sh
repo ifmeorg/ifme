3bc1bf591a226cae75cb7f00d0cf9715ab36081f
Andy Alt <andy5995@users.noreply.github.com>
2017-11-07 17:41:09 -0600

    create client.sh: move shell commands in Procfile.dev (#763)
    * create client.sh: move shell commands in Procfile.dev
    
    * this shortens the client: line in Procfile.dev
    * gives a little more flexibility in the future for dealing with errors
    
    * moving and condensing script

---
+++ bin/client.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# client.sh
+# This script is run from Procfile.dev (which is run from bin/start_app
+
+# Make sure we're in the source directory by checking for the existence
+# of ./public (note that we're checking the relative path, not the absolute).
+#
+# bin/start_app should change to the source directory, so
+# this should never happen unless something very unusual is wrong
+if [ ! -d public ]; then
+{
+  echo "Error in setup"
+  exit $?
+}
+fi
+
+rm -rf public/webpack/development/* || true && \
+cd client && \
+bundle exec rake react_on_rails:locale && \
+yarn install && \
+yarn run build:development && \
+
+exit 0;
