a3e8740db1543c7e1593ae8052283b9af8f585b5
haleymnatz <haleymnatz@gmail.com>
2017-11-08 13:36:20 -0500

    Add tests for valid/invalid params mix

--- spec/controllers/users/invitations_controller_spec.rb
+++ spec/controllers/users/invitations_controller_spec.rb
@@ -37,12 +37,11 @@ RSpec.describe ::Users::InvitationsController, type: :controller do
 
         context 'when the user has invited multiple friends' do
           let(:friends_array) { [invitee1, invitee2] }
-          let(:invite_two_friends) do
+          before(:each) do
             post :create, params: { user: { email: "#{invitee1}, #{invitee2}" } }
           end
 
           it 'creates a new user for each supplied email address' do
-            invite_two_friends
             newest_users = User.order(invitation_sent_at: :desc).last(2)
             emails = []
             newest_users.each { |u| emails << u.email }
@@ -50,7 +49,6 @@ RSpec.describe ::Users::InvitationsController, type: :controller do
           end
 
           it 'sends an invitation email to each valid address' do
-            invite_two_friends
             emails = []
             Devise.mailer.deliveries.each { |d| emails << d.to[0] }
             expect(Devise.mailer.deliveries.count).to eq(2)
@@ -58,7 +56,6 @@ RSpec.describe ::Users::InvitationsController, type: :controller do
           end
 
           it 'creates a unique token for each email' do
-            invite_two_friends
             friend1 = User.find_by(email: invitee1)
             friend2 = User.find_by(email: invitee2)
             expect(friend1.invitation_token).not_to eq(friend2.invitation_token)
@@ -78,6 +75,28 @@ RSpec.describe ::Users::InvitationsController, type: :controller do
           user_count = User.count
           invalid_invite
           expect(User.count).to eq(user_count)
+        end
+      end
+
+      context 'when both valid and invalid params are given' do
+        before(:each) do
+          post :create, params: { user: { email: "#{invitee1}, #{invalid_email}" } }
+        end
+
+        it 'only creates a new User for the valid email' do
+          newest_users = User.order(invitation_sent_at: :desc).last(2)
+          emails = []
+          newest_users.each { |u| emails << u.email }
+          expect(emails.sort).to include(invitee1)
+          expect(emails.sort).not_to include(invalid_email)
+        end
+
+        it 'only sends an invitation email to the valid email address' do
+          emails = []
+          Devise.mailer.deliveries.each { |d| emails << d.to[0] }
+          expect(Devise.mailer.deliveries.count).to eq(1)
+          expect(emails.sort).to include(invitee1)
+          expect(emails.sort).not_to include(invalid_email)
         end
       end
     end
