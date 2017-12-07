02c936308cf646b8cdcdc5ac51d3cfdecb3f29c2
Tim Moore <tmoore@incrementalism.net>
2017-12-07 22:57:17 +1030

    Increase the wait time for the locale selector
    This reduces flaky test failures.

--- spec/support/page_transition_support.rb
+++ spec/support/page_transition_support.rb
@@ -2,11 +2,11 @@ module PageTransitionSupport
   def change_language(languageCode)
     within 'select[name=locale]' do
       find("option[value=#{languageCode}]").click
-      expect(find("option[value=#{languageCode}][selected]")).to be_present
+      expect(find("option[value=#{languageCode}][selected]", wait: 5)).to be_present
     end
   end
 end
-    
+
 RSpec.configure do |config|
   config.include PageTransitionSupport, :type => :feature
 end
