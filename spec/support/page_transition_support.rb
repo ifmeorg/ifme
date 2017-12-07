7211d9c6f6984339ed22495f44c3adc8323d49e8
Tim Moore <tmoore@incrementalism.net>
2017-12-07 23:08:08 +1030

    Fix Rubocop offenses

--- spec/support/page_transition_support.rb
+++ spec/support/page_transition_support.rb
@@ -1,12 +1,16 @@
+# frozen_string_literal: true
+
 module PageTransitionSupport
-  def change_language(languageCode)
+  def change_language(language_code)
     within 'select[name=locale]' do
-      find("option[value=#{languageCode}]").click
-      expect(find("option[value=#{languageCode}][selected]", wait: 5)).to be_present
+      find("option[value=#{language_code}]").click
+      expect(
+        find("option[value=#{language_code}][selected]", wait: 5)
+      ).to be_present
     end
   end
 end
 
 RSpec.configure do |config|
-  config.include PageTransitionSupport, :type => :feature
+  config.include PageTransitionSupport, type: :feature
 end
