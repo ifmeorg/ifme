660343eb34170df30a87d02f6ff130e78dcc9aea
trishla08 <trishla.verma@gmail.com>
2018-01-27 01:32:29 +0530

    Update webpack.config.js

--- client/.storybook/webpack.config.js
+++ client/.storybook/webpack.config.js
@@ -1,18 +1,12 @@
-// you can use this file to add your custom webpack plugins, loaders and anything you like.
-// This is just the basic way to add additional webpack configurations.
-// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
-
-// IMPORTANT
-// When you add this file, we won't add the default configurations which is similar
-// to "React Create App". This only has babel loader to load JavaScript.
+const path = require('path');
 
 module.exports = {
-  plugins: [
-    // your custom plugins
-  ],
   module: {
     rules: [
-      // add your custom rules.
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader?modules&camelCase&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass-loader',
+      },
     ],
   },
 };
