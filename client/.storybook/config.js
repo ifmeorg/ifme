283f72b5a263f9346ea80da55996de94738282e1
trishla08 <trishla.verma@gmail.com>
2018-01-27 01:32:07 +0530

    Update config.js

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,9 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
+
 import { configure } from '@storybook/react';
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/);
 function loadStories() {
-  req.keys().forEach((filename) => req(filename));
+  require('../app/stories/index');
 }
 
 configure(loadStories, module);
