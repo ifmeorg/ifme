e91fdaecfe871bdf9a134fa277ba59ba60fdf913
Andrew Schutt <schutt.andrew@gmail.com>
2018-02-12 21:39:09 -0600

    Extracted out multiple methods of add method
    Detangled some of the logic that was tied up in the if statement by
    extracting the logic determining the pusher_type. Created
    `setup_allyship`, `remove_allyship_request`, and
    `send_allyship_notifications` to help clarify what all is a part of the
    add method within the allies_controller.

--- app/controllers/allies_controller.rb
+++ app/controllers/allies_controller.rb
@@ -19,48 +19,13 @@ class AlliesController < ApplicationController
       user_id: current_user.id, ally_id: ally_id
     )
 
-    if allyship
-      allyship.update(status: User::ALLY_STATUS[:accepted])
+    pusher_type = allyship ? 'accepted_ally_request' : 'new_ally_request'
 
-      # Notify the user who made the request
-      pusher_type = 'accepted_ally_request'
+    setup_allyship(allyship, ally_id)
 
-      # Get rid of original new_ally_request notification
-      Notification.where(
-        userid: current_user.id,
-        uniqueid: "new_ally_request_#{ally_id}"
-      ).destroy_all
-    else
-      Allyship.create(
-        user_id: current_user.id,
-        ally_id: ally_id,
-        status: User::ALLY_STATUS[:pending_from_ally]
-      )
+    remove_allyship_request(ally_id)
 
-      # Notify the user the request has been made to
-      pusher_type = 'new_ally_request'
-    end
-
-    uniqueid = "#{pusher_type}_#{current_user.id}"
-    data = JSON.generate(
-      user: current_user.name,
-      userid: current_user.id,
-      uid: current_user.uid,
-      type: pusher_type,
-      uniqueid: uniqueid
-    )
-
-    Notification.create(
-      userid: ally_id,
-      uniqueid: uniqueid,
-      data: data
-    )
-
-    notifications = Notification.where(userid: ally_id).order(:created_at)
-    Pusher["private-#{ally_id}"]
-      .trigger('new_notification', notifications: notifications)
-
-    NotificationMailer.notification_email(ally_id, data).deliver_now
+    send_allyship_notifications(pusher_type, ally_id)
 
     respond_to do |format|
       format.html { redirect_to :back }
@@ -79,5 +44,50 @@ class AlliesController < ApplicationController
       format.html { redirect_to :back }
       format.json { head :no_content }
     end
+  end
+
+  private
+
+  def setup_allyship(allyship, ally_id)
+    if allyship
+      allyship.update(status: User::ALLY_STATUS[:accepted])
+    else
+      Allyship.create(
+        user_id: current_user.id,
+        ally_id: ally_id,
+        status: User::ALLY_STATUS[:pending_from_ally]
+      )
+    end
+  end
+
+  def remove_allyship_request(ally_id)
+    Notification.where(
+      userid: current_user.id,
+      uniqueid: "new_ally_request_#{ally_id}"
+    ).destroy_all
+  end
+
+  def send_allyship_notifications(pusher_type, ally_id)
+    uniqueid = "#{pusher_type}_#{current_user.id}"
+    data = JSON.generate(
+      user: current_user.name,
+      userid: current_user.id,
+      uid: current_user.uid,
+      type: pusher_type,
+      uniqueid: uniqueid
+    )
+
+    Notification.create(
+      userid: ally_id,
+      uniqueid: uniqueid,
+      data: data
+    )
+
+    notifications = Notification.where(userid: ally_id).order(:created_at)
+
+    Pusher["private-#{ally_id}"]
+      .trigger('new_notification', notifications: notifications)
+
+    NotificationMailer.notification_email(ally_id, data).deliver_now
   end
 end
