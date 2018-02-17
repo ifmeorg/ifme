da14c6233b1b9f7628a120017501ade3917c69e8
seunadex <adekunleseun001@gmail.com>
2018-02-17 04:20:38 +0100

    add language dropdown and my blurb

--- client/app/bundles/shared/components/Footer/Connect.jsx
+++ client/app/bundles/shared/components/Footer/Connect.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import css from './Footer.scss';
 
 const Connect = props => (
--- client/app/bundles/shared/components/Footer/Footer.jsx
+++ client/app/bundles/shared/components/Footer/Footer.jsx
@@ -3,34 +3,41 @@ import css from './Footer.scss';
 import Resources from './Resources';
 import Connect from './Connect';
 import Ifme from './Ifme';
+import DropdownGhostSmall from '../Dropdown/DropdownGhostSmall';
 
 import enYML from '../../../../../../config/locales/en.yml';
 
-const Footer = () => {
-  const navProp = enYML.en.navigation;
-  return (
-    <div className={css.footer}>
-      <div id={css.footer_contest}>
-        <div className={css.table}>
-          <div className={css.row}>
-            <div className={`${css.table_cell} ${css.resources}`}>
-              <Resources pages={enYML.en.pages.resources} navigation={navProp} />
-            </div>
-            <div className={`${css.table_cell} ${css.connect}`}>
-              <Connect navigation={navProp} common={enYML.en.common} />
-            </div>
-            <div className={`${css.table_cell} ${css.if_me}`}>
-              <Ifme app_name={enYML.en.app_name} navigation={navProp} />
-            </div>
+const navProp = enYML.en.navigation;
+const we = enYML.en.shared.footer.license_we;
+const foss = enYML.en.shared.footer.license_foss;
+
+const Footer = () => (
+  <div className={css.footer}>
+    <div id={css.footer_contest}>
+      <div className={css.table}>
+        <div className={css.row}>
+          <div className={`${css.table_cell} ${css.if_me}`}>
+            <Ifme app_name={enYML.en.app_name} navigation={navProp} />
+          </div>
+          <div className={`${css.table_cell} ${css.connect}`}>
+            <Connect navigation={navProp} common={enYML.en.common} />
+          </div>
+          <div className={`${css.table_cell} ${css.resources}`}>
+            <Resources pages={enYML.en.pages.resources} navigation={navProp} />
+          </div>
+          <div className={`${css.table_cell} ${css.dropdown}`}>
+            <DropdownGhostSmall />
           </div>
           <div className={`${css.table_cell} ${css.love_foss}`}>
-            <h4>We &hearts; FOSS</h4>
-            <a className={css.license} href="https://github.com/ifmeorg/ifme/blob/master/LICENSE.txt" target="blank" >Licensed under AGPL-3.0</a>
+            <h4>{we} &hearts; {foss}</h4>
+            <a className={css.license} href="https://github.com/ifmeorg/ifme/blob/master/LICENSE.txt" target="blank" >
+              {enYML.en.shared.footer.license_subtitle} {enYML.en.shared.footer.licence_name}
+            </a>
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Footer;
