841629de4e6b5b64d1adfa00ec6ce89ce72b41ff
Nishiki Liu <nshki@users.noreply.github.com>
2018-03-04 11:46:12 -0500

    Resolve Rubocop errors for uploaders

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,4 +1,3 @@
-# encoding: utf-8
 # frozen_string_literal: true
 
 class AvatarUploader < CarrierWave::Uploader::Base
@@ -21,7 +20,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #   # ActionController::Base.helpers.asset_path(
+  #   #   "fallback/" + [version_name, "default.png"].compact.join('_')
+  #   # )
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
@@ -49,7 +50,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
+  # Avoid using model.id or version_name here, see uploader/store.rb for
+  # details.
   # def filename
   #   "something.jpg" if original_filename
   # end
