ccc96746b4f72969aa05a7bdd51b3b411be7cee8
Nishiki Liu <nshki@users.noreply.github.com>
2018-03-04 18:16:04 -0500

    Reindent JS

--- app/assets/javascripts/strategies.js
+++ app/assets/javascripts/strategies.js
@@ -1,20 +1,20 @@
 var showTaggedMoments = function() {
-	$('#moment_tag_usage').addClass('display_block').removeClass('display_none');
-	$('#showTaggedMoments').addClass('display_none').removeClass('display_inline_block');
-	$('#hideTaggedMoments').addClass('display_inline_block').removeClass('display_none');
+  $('#moment_tag_usage').addClass('display_block').removeClass('display_none');
+  $('#showTaggedMoments').addClass('display_none').removeClass('display_inline_block');
+  $('#hideTaggedMoments').addClass('display_inline_block').removeClass('display_none');
 };
 
 var hideTaggedMoments = function() {
-	$('#moment_tag_usage').removeClass('display_block').addClass('display_none');
-	$('#showTaggedMoments').removeClass('display_none').addClass('display_inline_block');
-	$('#hideTaggedMoments').removeClass('display_inline_block').addClass('display_none');
+  $('#moment_tag_usage').removeClass('display_block').addClass('display_none');
+  $('#showTaggedMoments').removeClass('display_none').addClass('display_inline_block');
+  $('#hideTaggedMoments').removeClass('display_inline_block').addClass('display_none');
 };
 
 var onReadyStrategies = function() {
-	if (isShow(['strategies'])) {
-		$('#showTaggedMoments').click(showTaggedMoments);
-		$('#hideTaggedMoments').click(hideTaggedMoments);
-	}
+  if (isShow(['strategies'])) {
+    $('#showTaggedMoments').click(showTaggedMoments);
+    $('#hideTaggedMoments').click(hideTaggedMoments);
+  }
 };
 
 loadPage(onReadyStrategies);
