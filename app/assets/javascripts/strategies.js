fc4157ac4e1b9c6ea6955d39e328eb3c8b3a1d5b
Julia Nguyen <tojulianguyen@gmail.com>
2018-03-04 19:50:49 -0800

    Even more clean up

--- app/assets/javascripts/strategies.js
+++ app/assets/javascripts/strategies.js
@@ -1,19 +1,25 @@
-var showTaggedMoments = function() {
-  $('#moment_tag_usage').addClass('display_block').removeClass('display_none');
-  $('#showTaggedMoments').addClass('display_none').removeClass('display_inline_block');
-  $('#hideTaggedMoments').addClass('display_inline_block').removeClass('display_none');
-};
+function momentTagUsageClass(remove) {
+  return remove ? 'display_none' : 'display_block';
+}
 
-var hideTaggedMoments = function() {
-  $('#moment_tag_usage').removeClass('display_block').addClass('display_none');
-  $('#showTaggedMoments').removeClass('display_none').addClass('display_inline_block');
-  $('#hideTaggedMoments').removeClass('display_inline_block').addClass('display_none');
-};
+function showTaggedMomentsClass(remove) {
+  return remove ? 'display_inline_block' : 'display_none';
+}
+
+function showTaggedMoments(show) {
+  $('#moment_tag_usage').removeClass(momentTagUsageClass(show)).addClass(momentTagUsageClass(!show));
+  $('#showTaggedMoments').removeClass(showTaggedMomentsClass(show)).addClass(showTaggedMomentsClass(!show));
+  $('#hideTaggedMoments').removeClass(showTaggedMomentsClass(!show)).addClass(showTaggedMomentsClass(show));
+}
 
 var onReadyStrategies = function() {
   if (isShow(['strategies'])) {
-    $('#showTaggedMoments').click(showTaggedMoments);
-    $('#hideTaggedMoments').click(hideTaggedMoments);
+    $('#showTaggedMoments').click(function() {
+      showTaggedMoments(true);
+    });
+    $('#hideTaggedMoments').click(function() {
+      showTaggedMoments(false);
+    });
   }
 };
 
