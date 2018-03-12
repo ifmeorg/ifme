9e35512c854758a3b33ae3bcf155e38755cb7943
seunadex <adekunleseun001@gmail.com>
2018-03-12 11:28:02 +0100

    add resource component

---
+++ client/app/bundles/shared/components/Resource/Resources.jsx
@@ -0,0 +1,31 @@
+// @flow
+import React from 'react';
+import css from './Resources.scss';
+import Tag from '../Tag';
+
+type Props = {
+  resourcesObj?: Object,
+  tagged?: boolean,
+};
+
+export default class Resources extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+  }
+
+  render() {
+    const { resourcesObj, tagged } = this.props;
+    const resources = tagged ? resourcesObj.tags.map((tag, index) =>
+      <Tag normal label={tag} key={index} />,
+    ) : <div className={css.author}>{resourcesObj.author}</div>;
+
+    return (
+      <div className={css.container}>
+        <div className={css.dash} />
+        <div className={css.link}><a href={resourcesObj.link} target="blank" >{resourcesObj.name}</a></div>
+        { resources }
+      </div>
+    );
+  }
+}
+
