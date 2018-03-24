539b80344f3d25c504325367646c967ff4f0374e
Marcel Kalveram <marcel@hanno.co>
2018-03-24 21:26:48 +0100

    add German to i18nSetup

---
+++ client/app/libs/i18n/I18nSetup.js
@@ -0,0 +1,27 @@
+import first from 'lodash/first';
+import { addLocaleData } from 'react-intl';
+// every locale is an array of various sub locales, that follow the type defined below
+// https://github.com/yahoo/react-intl/wiki/API#addlocaledata
+/*
+  type LocaleData = {
+      locale: string,
+      [key: string]: any,
+  }
+ */
+import en from 'react-intl/locale-data/en';
+import es from 'react-intl/locale-data/es';
+import it from 'react-intl/locale-data/it';
+import nb from 'react-intl/locale-data/nb';
+import sv from 'react-intl/locale-data/sv';
+import nl from 'react-intl/locale-data/nl';
+import br from 'react-intl/locale-data/br';
+import de from 'react-intl/locale-data/de';
+
+const ptbr = first(br);
+ptbr.locale = 'ptbr';
+
+// Initizalize all locales for react-intl.
+// eslint-disable-next-line import/prefer-default-export
+export const loadLocales = () => {
+  addLocaleData([...en, ...es, ...it, ...nb, ...sv, ...nl, ptbr, ...de]);
+};
