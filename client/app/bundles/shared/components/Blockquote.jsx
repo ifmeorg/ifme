0014a182d57f0f3e273122c6d030756c7f2339fa
Varshika Choudhary <varshika15@gmail.com>
2018-03-30 12:27:39 +0530

    Update Blockquote.jsx

--- client/app/bundles/shared/components/Blockquote.jsx
+++ client/app/bundles/shared/components/Blockquote.jsx
@@ -5,12 +5,9 @@ export default class Blockquote extends React.Component {
   render() {
     return (
       <div className="Blockquote">      
-        
         <blockquote>"It's not just all in your head, it's all around you. We 
-          can heal together."</blockquote>
-          <blockquote>{"\u2764\uFE0F"}</blockquote>
-        
-      </div>
+          can heal together."{"\u2764\uFE0F"}</blockquote>
+       </div>
     );
   }
 }
