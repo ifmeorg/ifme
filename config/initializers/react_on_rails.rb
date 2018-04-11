530f00f94e4d47f46d3fd28a04b2a84f57d98953
nma <nickma38@gmail.com>
2018-04-11 01:28:09 -0400

    chore(react_on_rails): remove broken config line that confuses new versions of react_on_rails gem.

--- config/initializers/react_on_rails.rb
+++ config/initializers/react_on_rails.rb
@@ -7,7 +7,8 @@ ReactOnRails.configure do |config|
   config.generated_assets_dir = File.join(%w[public webpack], Rails.env)
 
   # Define the files we need to check for webpack compilation when running tests.
-  config.webpack_generated_files = %w( webpack-bundle.js )
+  # Removed https://github.com/shakacode/react_on_rails/issues/949
+  # config.webpack_generated_files = %w( webpack-bundle.js )
 
   # This is the file used for server rendering of React when using `(prerender: true)`
   # If you are never using server rendering, you may set this to "".
