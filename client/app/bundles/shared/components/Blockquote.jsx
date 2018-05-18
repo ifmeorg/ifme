9279b552ecac8de1c9df736df8941d5f6cb5b577
Atibhi Agrawal <atibhi.a@gmail.com>
2018-05-18 23:12:03 +0530

    Update Blockquote.jsx

--- client/app/bundles/shared/components/Blockquote.jsx
+++ client/app/bundles/shared/components/Blockquote.jsx
@@ -4,14 +4,12 @@ import css from './Blockquote.scss';
 
 type Props = {
   label?: string,
-
 };
 
 export default class Blockquote extends React.Component<Props> {
   render() {
     const { label} = this.props;
     const labelClassNames = `${css.label}`;
-
     return (
       <div className={labelClassNames}>
         {label}
