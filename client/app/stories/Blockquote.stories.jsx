7d0df8b39e0249555f1a75866709f49be5ee4773
prateksha_u <32712346+prateksha@users.noreply.github.com>
2018-05-22 13:21:50 +0530

    Update Blockquote.stories.jsx

--- client/app/stories/Blockquote.stories.jsx
+++ client/app/stories/Blockquote.stories.jsx
@@ -8,4 +8,4 @@ storiesOf('Blockquotes', module)
   .add('Blockquote', withSource(
     <SingleColumnLayout>
       <Blockquote text="It's not just all in your head, it's all around you. We can heal together." author="❤️" />    </SingleColumnLayout>,
-));
+  ));
