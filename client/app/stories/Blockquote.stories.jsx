22a0a68c9e719efa26a965c8bca6cf98eb3c871b
prateksha.u <prateksha.u@iiitb.org>
2018-05-22 15:36:38 +0530

    Final edit

--- client/app/stories/Blockquote.stories.jsx
+++ client/app/stories/Blockquote.stories.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
@@ -7,5 +8,6 @@ import { SingleColumnLayout, withSource } from './Stories.helper';
 storiesOf('Blockquotes', module)
   .add('Blockquote', withSource(
     <SingleColumnLayout>
-      <Blockquote text="It's not just all in your head, it's all around you. We can heal together." author="❤️" />    </SingleColumnLayout>,
-));
+      <Blockquote text="It's not just all in your head, it's all around you. We can heal together." author="❤️" />
+    </SingleColumnLayout>,
+  ));
