145a00d187a19d3b7d2a23f21520b1cdcec8514d
Nicholas  Vorraso <nvorraso2020@gmail.com>
2018-06-16 16:43:21 -0400

    attempt at removing eslint errors

--- client/app/bundles/shared/components/Pagination/Pagination.jsx
+++ client/app/bundles/shared/components/Pagination/Pagination.jsx
@@ -2,20 +2,17 @@ import React from 'react';
 import css from './Pagination.scss';
 
 export default class Pagination extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      active: 1,  // which page we are on
-      pages: 3    // the amount of pages we have
+      active: 1,
+      pages: 3,
     };
     this.togglePages = this.togglePages.bind(this);
   }
 
   togglePages(event) {
-    this.setState({ active: parseInt(event.target.value) }, () => {
-      console.log(`We are on page ${this.state.active}`);
-    })
+    this.setState({ active: parseInt(event.target.value, 10) }});
   }
 
   render() {
@@ -24,10 +21,11 @@ export default class Pagination extends React.Component {
 
     while (val > 0) {
       elements.unshift(
-        <button className={css.circle} onClick={this.togglePages} value={val} key={val - 1}
-          style={{ 'background': this.state.active === val ? '#ffffff' : '#B2B2B2' }}>
+        <button className={css.circle} 
+          onClick={this.togglePages} value={val} key={val - 1}
+          style={{ background: this.state.active === val ? '#ffffff' : '#B2B2B2' }}>
           <span className={css.number}
-            style={{ 'color': this.state.active === val ? '#59002B' : '#ffffff' }}>{val}</span>
+            style={{ color: this.state.active === val ? '#59002B' : '#ffffff' }}>{val}</span>
         </button>
       )
       val--;
@@ -40,4 +38,3 @@ export default class Pagination extends React.Component {
     )
   }
 }
-
