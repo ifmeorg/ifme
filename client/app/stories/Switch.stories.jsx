1bc7014aac4af3347615392e3738e12c0a679fcb
prateksha.u <prateksha.u@iiitb.org>
2018-07-03 14:40:24 +0530

    Switch component added

---
+++ client/app/stories/Switch.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { withInfo } from '@storybook/addon-info';
+import { storiesOf } from '@storybook/react';
+
+import Switch from 'bundles/shared/components/Switch';
+
+storiesOf('Switch', module)
+  .add('Switch ON/OFF', withInfo({})(() =>
+    <Switch />
+  ));
