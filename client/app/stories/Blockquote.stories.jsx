4f3999790877b930ef009d66d008b323eeba6952
Bao Thien Ngo <bingo@productfy.io>
2018-07-06 17:39:01 -0700

    Removing SingleColumnLayout, since it's not needed.

--- client/app/stories/Blockquote.stories.jsx
+++ client/app/stories/Blockquote.stories.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import Blockquote from 'bundles/shared/components/Blockquote';
-import { SingleColumnLayout } from './Stories.helper';
 
 storiesOf('Blockquotes', module)
   .add('Blockquote', () => (
-    <SingleColumnLayout>
-      <Blockquote text="It's not just all in your head, it's all around you. We can heal together." author="❤️" />
-    </SingleColumnLayout>
+    <Blockquote
+      text="It's not just all in your head, it's all around you. We can heal together."
+      author="❤️"
+    />
   ));
