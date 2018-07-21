baa2b9bf811c1156e8aa7d26e7701a69d6fa5078
Jasper Martin <jaspervanbrianmartin@gmail.com>
2018-07-22 05:43:03 +0800

    Fix membership controller test.

--- spec/controllers/groups/memberships_controller_spec.rb
+++ spec/controllers/groups/memberships_controller_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe Groups::MembershipsController, type: :controller do
 
   describe '#create' do
     context 'new group member for existing group' do
-      let(:leader_user) { create :user2 }
-      let(:group_leader) { create :group_member, user_id: leader_user.id, leader: true }
-      let(:group) { group_leader.group }
+      let!(:leader_user) { create :user2 }
+      let!(:group_leader) { create :group_member, user_id: leader_user.id, leader: true }
+      let!(:group) { group_leader.group }
 
       it 'adds new member' do
         post :create, params: {
@@ -37,28 +37,28 @@ RSpec.describe Groups::MembershipsController, type: :controller do
 
   describe '#kick' do
     context 'when current_user is a leader' do
-      let(:group_member) { create :group_member, user_id: controller.current_user.id, leader: true }
-      let(:group) { group_member.group }
-      let(:another_user) { create :user2 }
-      let(:another_member) { create :group_member, user_id: another_user.id, leader: false, group: group }
+      let!(:group_leader) { create :group_member, user_id: controller.current_user.id, leader: true }
+      let!(:group) { group_leader.group }
+      let!(:another_user) { create :user2 }
+      let!(:another_member) { create :group_member, user_id: another_user.id, leader: false, group: group }
 
-      # it 'can kick another group member out' do
-      #   delete :kick, params: {
-      #     group_id: group.id,
-      #     member_id: another_user.id
-      #   }
-      #   expect(response).to redirect_to(groups_path)
-      #   expect(flash[:notice]).to eq(
-      #     "You have removed #{another_user.name} from #{group.name}"
-      #   )
-      # end
+      it 'can kick another group member out' do
+        delete :kick, params: {
+          group_id: group.id,
+          member_id: another_user.id
+        }
+        expect(response).to redirect_to(groups_path)
+        expect(flash[:notice]).to eq(
+          "You have removed #{another_user.name} from #{group.name}"
+        )
+      end
     end
 
     context 'when current_user is not a leader' do
-      let(:group_member) { create :group_member, user_id: controller.current_user.id, leader: false }
-      let(:group) { group_member.group }
-      let(:another_user) { create :user2 }
-      let(:another_member) { create :group_member, user_id: another_user.id, leader: false, group: group }
+      let!(:group_member) { create :group_member, user_id: controller.current_user.id, leader: false }
+      let!(:group) { group_member.group }
+      let!(:another_user) { create :user2 }
+      let!(:another_member) { create :group_member, user_id: another_user.id, leader: false, group: group }
 
       it 'cannot kick another group member out' do
         delete :kick, params: {
@@ -75,10 +75,10 @@ RSpec.describe Groups::MembershipsController, type: :controller do
 
   describe '#leave' do
     context 'when current_user is not a leader' do
-      let(:leader_user) { create :user2 }
-      let(:leader_member) { create :group_member, user_id: another_user.id, leader: true }
-      let(:group_member) { create :group_member, user_id: controller.current_user.id, leader: false }
-      let(:group) { group_member.group }
+      let!(:leader_user) { create :user2 }
+      let!(:leader_member) { create :group_member, user_id: leader_user.id, leader: true }
+      let!(:another_member) { create :group_member, user_id: controller.current_user.id, leader: false }
+      let!(:group) { another_member.group }
 
       it 'can leave group' do
         delete :destroy, params: { group_id: group.id }
@@ -90,7 +90,7 @@ RSpec.describe Groups::MembershipsController, type: :controller do
     end
 
     context 'when current_user is a leader' do
-      let(:group_member) { create :group_member, user_id: controller.current_user.id, leader: true }
+      let!(:group_member) { create :group_member, user_id: controller.current_user.id, leader: true }
 
       it 'can leave the group when user is not the only leader' do
         group = group_member.group
