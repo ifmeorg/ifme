3496608c64dafb5411c35a440dea91bb9f51c579
Julia Nguyen <tojulianguyen@gmail.com>
2018-07-21 18:55:09 -0700

    Fix broken tests, add React tests to Circle build

--- client/app/components/Chart/__tests__/Chart.spec.jsx
+++ client/app/components/Chart/__tests__/Chart.spec.jsx
@@ -1,44 +1,30 @@
 // @flow
-import { render } from 'enzyme';
+import { shallow } from 'enzyme';
 import React from 'react';
 import { Chart } from '../index';
 
+function getComponent(options) {
+  return shallow(
+    <Chart
+      xtitle="foo"
+      ytitle="bar"
+      data={{
+        '2013-02-10 00:00:00 -0800': 11,
+        '2013-02-11 00:00:00 -0800': 6,
+      }}
+      chartType={options.chartType}
+    />,
+  );
+}
+
 describe('Chart', () => {
   it('renders an AreaChart', () => {
-    let wrapper = null;
-    expect(() => {
-      wrapper = render(
-        <Chart
-          xtitle="foo"
-          ytitle="bar"
-          data={{
-            '2013-02-10 00:00:00 -0800': 11,
-            '2013-02-11 00:00:00 -0800': 6,
-          }}
-          chartType={'Area'}
-        />,
-      );
-    }).not.toThrow();
-
-    expect(wrapper).not.toBeNull();
+    const wrapper = getComponent({ chartType: 'Area' });
+    expect(wrapper.length).toEqual(1);
   });
 
   it('renders a LineChart', () => {
-    let wrapper = null;
-    expect(() => {
-      wrapper = render(
-        <Chart
-          xtitle="foo"
-          ytitle="bar"
-          data={{
-            '2013-02-10 00:00:00 -0800': 11,
-            '2013-02-11 00:00:00 -0800': 6,
-          }}
-          chartType={'Line'}
-        />,
-      );
-    }).not.toThrow();
-
-    expect(wrapper).not.toBeNull();
+    const wrapper = getComponent({ chartType: 'Line'});
+    expect(wrapper.length).toEqual(1);
   });
 });
