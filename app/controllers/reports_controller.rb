a73c8a33e6d840e3fa8040a7a2875ec1d3d63a89
prateksha.u <prateksha.u@iiitb.org>
2018-07-25 21:14:25 +0530

    Report feature in user profile

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,10 @@
+class ReportsController < ApplicationController
+  def new
+  end
+
+  def create
+    user_id = current_user.id
+    ally_id = params[:ally_id].to_i
+    @report = Report.create(reporter_id: user_id, reportee_id: ally_id, comments: params[:report][:comments] )
+  end
+end
