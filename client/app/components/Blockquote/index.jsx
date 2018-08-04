b1f81638ec38fd3b6a94bf2ea0c002cc49ea9073
Julia Nguyen <tojulianguyen@gmail.com>
2018-08-04 13:20:41 -0700

    Setup new header

--- client/app/components/Blockquote/index.jsx
+++ client/app/components/Blockquote/index.jsx
@@ -2,23 +2,21 @@
 import React from 'react';
 import css from './Blockquote.scss';
 
-type Props = {
-  text?: string,
-  author?: string,
-};
-
-export class Blockquote extends React.Component<Props> {
-  render() {
-    const { text, author } = this.props;
-    const textClassNames = `${css.text}`;
-    const authorClassNames = `${css.author}`;
-    return (
-      <div className={textClassNames}>
-        <p>
-          <q>{text}</q>
-        </p>
-        <div className={authorClassNames}>{author}</div>
-      </div>
-    );
-  }
+export interface Props {
+  text?: string;
+  author?: string;
 }
+
+export const Blockquote = (props: Props) => {
+  const { text, author } = props;
+  const textClassNames = `${css.text}`;
+  const authorClassNames = `${css.author}`;
+  return (
+    <div className={textClassNames}>
+      <p>
+        <q>{text}</q>
+      </p>
+      <div className={authorClassNames}>{author}</div>
+    </div>
+  );
+};
