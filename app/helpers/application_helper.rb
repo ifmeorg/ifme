86da4ea7c7861b16fdca9ea6186ce73bfb71bf8f
Julia Nguyen <tojulianguyen@gmail.com>
2018-08-05 13:44:09 -0700

    More cleanup for page_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,12 +3,90 @@
 module ApplicationHelper
   include ViewersHelper
 
-  def active_class(path)
-    active?(path) ? 'contentNavLinksActive' : ''
+  # rubocop:disable RescueStandardError
+  def i18n_set?(key)
+    I18n.t key, raise: true
+  rescue
+    false
+  end
+  # rubocop:enable RescueStandardError
+
+  def active?(link_path, environment = {})
+    current_controller = params[:controller]
+    link_controller = Rails.application.routes
+                           .recognize_path(link_path, environment)[:controller]
+    # Current page.
+    current_page?(link_path) ||
+      # Current controller.
+      (
+        current_controller != 'profile' &&
+        current_controller != 'pages' &&
+        current_controller == link_controller
+      ) ||
+      # New user session with devise.
+      (link_path == new_user_session_path &&
+       current_controller == 'devise/sessions' && action_name == 'new') ||
+      # New user registration with devise.
+      (link_path == new_user_registration_path &&
+       current_controller == 'devise/registrations' && action_name == 'create')
+  end
+  # rubocop:enable MethodLength
+
+  def sign_in_path?
+    current_page?(new_user_session_path) ||
+      (params[:controller] == 'devise/sessions' && action_name == 'new')
+  end
+
+  def join_path?
+    current_page?(new_user_registration_path) ||
+      (params[:controller] == 'devise/registrations' && action_name == 'create')
+  end
+
+  def forgot_password_path?
+    current_page?(new_user_password_path) ||
+      (params[:controller] == 'devise/passwords' && action_name == 'new')
+  end
+
+  def update_account_path?
+    current_page?(edit_user_registration_path) ||
+      (params[:controller] == 'registrations' && action_name == 'update')
+  end
+
+  def not_signed_in_root_path?
+    current_page?(root_path) && !user_signed_in?
+  end
+
+  def send_ally_invitation_path?
+    current_page?(new_user_invitation_path) ||
+      (params[:controller] == 'devise/invitations' && action_name == 'new') ||
+      (params[:controller] == 'users/invitations' && action_name == 'create')
+  end
+
+  def ally_accept_invitation_path?
+    current_page?(accept_user_invitation_path) ||
+      (params[:controller] == 'devise/invitations' && action_name == 'accept')
+  end
+
+  def reset_password_path?
+    current_page?(edit_user_password_path) ||
+      (params[:controller] == 'devise/passwords' && action_name == 'edit')
   end
 
   def static_page?
-    active?(about_path) || active?(blog_path) || active?(resources_path) || active?(new_user_registration_path) || active?(new_user_session_path) || active?(edit_user_password_path) || (params[:controller]  == 'devise/invitations' && action_name == 'accept')
+    non_devise_paths = [
+      about_path, blog_path, resources_path, faq_path,
+      contribute_path, partners_path, press_path, privacy_path
+    ]
+    non_devise_paths = non_devise_paths.select { |path| active?(path) }
+    devise = ally_accept_invitation_path? || reset_password_path?
+    devise || non_devise_paths.count == 1
+  end
+
+  def content_nav_link_to(label, url, html_options = {})
+    environment = html_options[:method] ? { method: html_options[:method] } : {}
+    active_class = active?(url, environment) ? 'contentNavLinksActive' : ''
+    html_options[:class] = active_class
+    link_to(label, url, html_options)
   end
 
   # rubocop:disable MethodLength
@@ -54,27 +132,6 @@ module ApplicationHelper
     }
   end
 
-  def active?(link_path, environment = {})
-    current_controller = params[:controller]
-    link_controller = Rails.application.routes
-                           .recognize_path(link_path, environment)[:controller]
-    # Current page.
-    current_page?(link_path) ||
-      # Current controller.
-      (
-        current_controller != 'profile' &&
-        current_controller != 'pages' &&
-        current_controller == link_controller
-      ) ||
-      # New user session with devise.
-      (link_path == new_user_session_path &&
-       current_controller == 'devise/sessions' && action_name == 'new') ||
-      # New user registration with devise.
-      (link_path == new_user_registration_path &&
-       current_controller == 'devise/registrations' && action_name == 'create')
-  end
-  # rubocop:enable MethodLength
-
   def title(page_title)
     content_for(:title) { page_title }
   end
@@ -82,14 +139,6 @@ module ApplicationHelper
   def page_new(page_new_path)
     content_for(:page_new) { page_new_path }
   end
-
-  # rubocop:disable RescueStandardError
-  def i18n_set?(key)
-    I18n.t key, raise: true
-  rescue
-    false
-  end
-  # rubocop:enable RescueStandardError
 
   def get_icon_class(icon)
     if %w[envelope gift rss].include?(icon)
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,57 +1,14 @@
 describe ApplicationHelper do
-  describe '#header_props' do
-    subject { header_props }
-
-    before(:each) do
-      allow(self).to receive('active?').and_return(false)
-      allow(self).to receive('active?').with(active_path).and_return(active)
-      allow(self).to receive('user_signed_in?').and_return(user_signed_in)
-      allow(self).to receive('current_user').and_return(current_user)
-    end
-
-    context 'when user is signed in' do
-      let(:current_user) { create :user2 }
-      let(:user_signed_in) { true }
-      let(:active_path) { resources_path }
-
-      context 'has no active link' do
-        let(:active) { false }
-        it 'returns props with no active link ' do
-          active_links = subject[:links].select { |link| link[:active] }
-          expect(active_links.count).to eq(0)
-        end
-      end
-
-      context 'has an active link' do
-        let(:active) { true }
-        it 'returns props with an active link ' do
-          active_links = subject[:links].select { |link| link[:active] }
-          expect(active_links.count).to eq(1)
-          expect(active_links.first[:url]).to eq(active_path)
-        end
+  describe '#i18n_set?' do
+    context 'when does not exist' do
+      it 'returns false' do
+        expect(i18n_set?('i_do_not_exist')).to eq(false)
       end
     end
 
-    context 'when user is not signed in' do
-      let(:current_user) { nil }
-      let(:user_signed_in) { false }
-      let(:active_path) { new_user_session_path }
-
-      context 'has no active link' do
-        let(:active) { false }
-        it 'returns props with no active link ' do
-          active_links = subject[:links].select { |link| link[:active] }
-          expect(active_links.count).to eq(0)
-        end
-      end
-
-      context 'has an active link' do
-        let(:active) { true }
-        it 'returns props with an active link ' do
-          active_links = subject[:links].select { |link| link[:active] }
-          expect(active_links.count).to eq(1)
-          expect(active_links.first[:url]).to eq(active_path)
-        end
+    context 'when key exists' do
+      it 'returns i18n value' do
+        expect(i18n_set?('app_name')).to eq('if me')
       end
     end
   end
@@ -122,16 +79,372 @@ describe ApplicationHelper do
     end
   end
 
-  describe '#i18n_set?' do
-    context 'when does not exist' do
-      it 'returns false' do
-        expect(i18n_set?('i_do_not_exist')).to eq(false)
+  describe 'sign_in_path?' do
+    let(:page) { '' }
+    let(:current_controller) { '' }
+    let(:action_name) { '' }
+    subject { sign_in_path? }
+
+    before(:each) do
+      params[:controller] = current_controller
+      allow(self).to receive('action_name').and_return(action_name)
+      allow(self).to receive('current_page?').and_return(false)
+      allow(self).to receive('current_page?').with(page).and_return(true)
+    end
+
+    context 'when the path matches' do
+      let(:page) { new_user_session_path }
+      it { is_expected.to be true }
+    end
+
+    context 'when the controller and action match' do
+      let(:current_controller) { 'devise/sessions' }
+      let(:action_name) { 'new' }
+      it { is_expected.to be true }
+    end
+
+    context 'when the path does not match' do
+      let(:page) { about_path }
+      it { is_expected.to be false }
+    end
+  end
+
+  describe 'join_path?' do
+    let(:page) { '' }
+    let(:current_controller) { '' }
+    let(:action_name) { '' }
+    subject { join_path? }
+
+    before(:each) do
+      params[:controller] = current_controller
+      allow(self).to receive('action_name').and_return(action_name)
+      allow(self).to receive('current_page?').and_return(false)
+      allow(self).to receive('current_page?').with(page).and_return(true)
+    end
+
+    context 'when the path matches' do
+      let(:page) { new_user_registration_path }
+      it { is_expected.to be true }
+    end
+
+    context 'when the controller and action match the path' do
+      let(:current_controller) { 'devise/registrations' }
+      let(:action_name) { 'create' }
+      it { is_expected.to be true }
+    end
+
+    context 'when the path does not match' do
+      let(:page) { about_path }
+      it { is_expected.to be false }
+    end
+  end
+
+  describe 'forgot_password_path?' do
+    let(:page) { '' }
+    let(:current_controller) { '' }
+    let(:action_name) { '' }
+    subject { forgot_password_path? }
+
+    before(:each) do
+      params[:controller] = current_controller
+      allow(self).to receive('action_name').and_return(action_name)
+      allow(self).to receive('current_page?').and_return(false)
+      allow(self).to receive('current_page?').with(page).and_return(true)
+    end
+
+    context 'when the path matches' do
+      let(:page) { new_user_password_path }
+      it { is_expected.to be true }
+    end
+
+    context 'when the controller and action match the path' do
+      let(:current_controller) { 'devise/passwords' }
+      let(:action_name) { 'new' }
+      it { is_expected.to be true }
+    end
+
+    context 'when the path does not match' do
+      let(:page) { about_path }
+      it { is_expected.to be false }
+    end
+  end
+
+  describe 'update_account_path?' do
+    let(:page) { '' }
+    let(:current_controller) { '' }
+    let(:action_name) { '' }
+    subject { update_account_path? }
+
+    before(:each) do
+      params[:controller] = current_controller
+      allow(self).to receive('action_name').and_return(action_name)
+      allow(self).to receive('current_page?').and_return(false)
+      allow(self).to receive('current_page?').with(page).and_return(true)
+    end
+
+    context 'when the path matches' do
+      let(:page) { edit_user_registration_path }
+      it { is_expected.to be true }
+    end
+
+    context 'when the controller and action match the path' do
+      let(:current_controller) { 'registrations' }
+      let(:action_name) { 'update' }
+      it { is_expected.to be true }
+    end
+
+    context 'when the path does not match' do
+      let(:page) { about_path }
+      it { is_expected.to be false }
+    end
+  end
+
+  describe 'not_signed_in_root_path?' do
+    let(:page) { '' }
+    let (:user_signed_in) { true }
+    subject { not_signed_in_root_path? }
+
+    before(:each) do
+      allow(self).to receive('user_signed_in?').and_return(user_signed_in)
+      allow(self).to receive('current_page?').and_return(false)
+      allow(self).to receive('current_page?').with(page).and_return(true)
+    end
+
+    context 'when the path matches and user is not signed in' do
+      let(:page) { root_path }
+      let(:user_signed_in) { false }
+      it { is_expected.to be true }
+    end
+
+    context 'when the path matches and user is signed in' do
+      let(:page) { root_path }
+      it { is_expected.to be false }
+    end
+
+    context 'when the path does not match' do
+      let(:page) { about_path }
+      it { is_expected.to be false }
+    end
+  end
+
+  describe 'send_ally_invitation_path?' do
+    let(:page) { '' }
+    let(:current_controller) { '' }
+    let(:action_name) { '' }
+    subject { send_ally_invitation_path? }
+
+    before(:each) do
+      params[:controller] = current_controller
+      allow(self).to receive('action_name').and_return(action_name)
+      allow(self).to receive('current_page?').and_return(false)
+      allow(self).to receive('current_page?').with(page).and_return(true)
+    end
+
+    context 'when the path matches' do
+      let(:page) { new_user_invitation_path }
+      it { is_expected.to be true }
+    end
+
+    context 'when the controller and action match the path' do
+      let(:current_controller) { 'devise/invitations' }
+      let(:action_name) { 'new' }
+      it { is_expected.to be true }
+    end
+
+    context 'when another controller and action match the path' do
+      let(:current_controller) { 'users/invitations' }
+      let(:action_name) { 'create' }
+      it { is_expected.to be true }
+    end
+
+    context 'when the path does not match' do
+      let(:page) { about_path }
+      it { is_expected.to be false }
+    end
+  end
+
+  describe 'ally_accept_invitation_path?' do
+    let(:page) { '' }
+    let(:current_controller) { '' }
+    let(:action_name) { '' }
+    subject { ally_accept_invitation_path? }
+
+    before(:each) do
+      params[:controller] = current_controller
+      allow(self).to receive('action_name').and_return(action_name)
+      allow(self).to receive('current_page?').and_return(false)
+      allow(self).to receive('current_page?').with(page).and_return(true)
+    end
+
+    context 'when the path matches' do
+      let(:page) { accept_user_invitation_path }
+      it { is_expected.to be true }
+    end
+
+    context 'when the controller and action match the path' do
+      let(:current_controller) { 'devise/invitations' }
+      let(:action_name) { 'accept' }
+      it { is_expected.to be true }
+    end
+
+    context 'when the path does not match' do
+      let(:page) { about_path }
+      it { is_expected.to be false }
+    end
+  end
+
+  describe 'reset_password_path?' do
+    let(:page) { '' }
+    let(:current_controller) { '' }
+    let(:action_name) { '' }
+    subject { reset_password_path? }
+
+    before(:each) do
+      params[:controller] = current_controller
+      allow(self).to receive('action_name').and_return(action_name)
+      allow(self).to receive('current_page?').and_return(false)
+      allow(self).to receive('current_page?').with(page).and_return(true)
+    end
+
+    context 'when the path matches' do
+      let(:page) { edit_user_password_path }
+      it { is_expected.to be true }
+    end
+
+    context 'when the controller and action match the path' do
+      let(:current_controller) { 'devise/passwords' }
+      let(:action_name) { 'edit' }
+      it { is_expected.to be true }
+    end
+
+    context 'when the path does not match' do
+      let(:page) { about_path }
+      it { is_expected.to be false }
+    end
+  end
+
+  describe '#static_page?' do
+    subject { static_page? }
+
+    before(:each) do
+      allow(self).to receive('current_page?').and_return(false)
+      allow(self).to receive('current_page?').with(page).and_return(true)
+    end
+
+    context 'non-devise is the active page' do
+      context 'when it is a static page' do
+        let(:page) { about_path }
+        it { is_expected.to be true }
+      end
+
+      context 'when it is not a static page' do
+        let(:page) { moments_path }
+        it { is_expected.to be false }
       end
     end
 
-    context 'when key exists' do
-      it 'returns i18n value' do
-        expect(i18n_set?('app_name')).to eq('if me')
+    context 'devise path is the active page' do
+      context 'when it is a static page' do
+        let(:page) { accept_user_invitation_path }
+        it { is_expected.to be true }
+      end
+
+      context 'when it is not a static page' do
+        let(:page) { new_user_password_path }
+        it { is_expected.to be false }
+      end
+    end
+  end
+
+  describe '#content_nav_link_to' do
+    let(:active)    { true }
+    let(:label)     { 'foo' }
+    let(:path)      { 'bar' }
+    let(:options)   { {} }
+
+    subject { content_nav_link_to(label, path, options) }
+
+    before(:each) do
+      allow(self).to receive('active?').and_return(active)
+    end
+
+    context 'when active' do
+      let(:active) { true }
+      it { is_expected.to have_selector 'a' }
+      it { is_expected.to include label }
+      it { is_expected.to have_selector 'a.contentNavLinksActive' }
+    end
+
+    context 'when not active' do
+      let(:active) { false }
+      it { is_expected.to have_selector 'a' }
+      it { is_expected.to include label }
+      it { is_expected.not_to have_selector 'a.contentNavLinksActive' }
+    end
+
+    context 'when options include method' do
+      let(:options) { { :method => :delete } }
+
+      it 'passes method in environment' do
+        expect(self).to receive(:active?).with(path, options)
+        content_nav_link_to(label, path, options)
+      end
+    end
+  end
+
+  describe '#header_props' do
+    subject { header_props }
+
+    before(:each) do
+      allow(self).to receive('active?').and_return(false)
+      allow(self).to receive('active?').with(active_path).and_return(active)
+      allow(self).to receive('user_signed_in?').and_return(user_signed_in)
+      allow(self).to receive('current_user').and_return(current_user)
+    end
+
+    context 'when user is signed in' do
+      let(:current_user) { create :user2 }
+      let(:user_signed_in) { true }
+      let(:active_path) { resources_path }
+
+      context 'has no active link' do
+        let(:active) { false }
+        it 'returns props with no active link ' do
+          active_links = subject[:links].select { |link| link[:active] }
+          expect(active_links.count).to eq(0)
+        end
+      end
+
+      context 'has an active link' do
+        let(:active) { true }
+        it 'returns props with an active link ' do
+          active_links = subject[:links].select { |link| link[:active] }
+          expect(active_links.count).to eq(1)
+          expect(active_links.first[:url]).to eq(active_path)
+        end
+      end
+    end
+
+    context 'when user is not signed in' do
+      let(:current_user) { nil }
+      let(:user_signed_in) { false }
+      let(:active_path) { new_user_session_path }
+
+      context 'has no active link' do
+        let(:active) { false }
+        it 'returns props with no active link ' do
+          active_links = subject[:links].select { |link| link[:active] }
+          expect(active_links.count).to eq(0)
+        end
+      end
+
+      context 'has an active link' do
+        let(:active) { true }
+        it 'returns props with an active link ' do
+          active_links = subject[:links].select { |link| link[:active] }
+          expect(active_links.count).to eq(1)
+          expect(active_links.first[:url]).to eq(active_path)
+        end
       end
     end
   end
