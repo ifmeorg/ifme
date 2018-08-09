69e0327293543b78f40963a520d9b56e5b9961ea
prateksha.u <prateksha.u@iiitb.org>
2018-08-09 21:02:25 +0530

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180730034853) do
-
+ActiveRecord::Schema.define(version: 20180809070052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +28,6 @@ ActiveRecord::Schema.define(version: 20180730034853) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.text     "reports"
     t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
   end
@@ -200,11 +198,9 @@ ActiveRecord::Schema.define(version: 20180730034853) do
   end
 
   create_table "reports", force: :cascade do |t|
-    t.string   "reporter_id"
-    t.string   "reportee_id"
-    t.text     "comments"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string "reporter_id"
+    t.string "reportee_id"
+    t.text   "comments"
   end
 
   create_table "strategies", force: :cascade do |t|
@@ -273,7 +269,6 @@ ActiveRecord::Schema.define(version: 20180730034853) do
     t.string   "locale"
     t.datetime "access_expires_at"
     t.string   "refresh_token"
-    t.boolean  "admin"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true, using: :btree
     t.index ["invitations_count"], name: "index_users_on_invitations_count", using: :btree
