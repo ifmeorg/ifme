9fc321fba4599e9c5927c7e620823af0a1acc862
Atibhi Agrawal <Atibhi.Agrawal@iiitb.org>
2018-08-30 20:32:20 +0530

    show->destroy in admin fixed

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,6 +4,12 @@ class Report < ApplicationRecord
   validates :reportee_id, presence: true
   validates :reporter_id, presence: true
   validates :reasons, presence: true
+  #after_create :send_instructions
+
+  # def send_instructions
+  #    @reporter_mail = User.find(r_mail: current)
+  #    ReportMailer.report_email(reporter_id,'example@a.com').deliver_now
+  # end
 
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180811055552) do
+ActiveRecord::Schema.define(version: 20180823193939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
