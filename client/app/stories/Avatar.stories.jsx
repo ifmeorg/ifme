6035a129a71a2a42f73f2922ea09d17856883785
Julia Nguyen <tojulianguyen@gmail.com>
2018-09-09 23:38:36 -0700

    Remove antd

--- client/app/stories/Avatar.stories.jsx
+++ client/app/stories/Avatar.stories.jsx
@@ -1,38 +1,22 @@
-import { Col, Row } from 'antd';
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import photoJulia from '../../../app/assets/images/contributors/julia_nguyen.jpg';
 import photoTara from '../../../app/assets/images/contributors/tara_swenson.jpg';
 import { Avatar } from '../components/Avatar';
 
 storiesOf('Avatar', module)
-  .add('With name', () => (
-    <Row>
-      <Col span={8}>
-        <Avatar src={photoJulia} name="Julia Nguyen" displayName />
-      </Col>
-      <Col span={8}>
-        <Avatar src={photoTara} name="Tara Swenson" displayName />
-      </Col>
-      <Col span={8}>
-        <Avatar
-          src="NonExistentFile.jpg"
-          name="Default On Loading Error"
-          displayName
-        />
-      </Col>
-    </Row>
-  ))
   .add('Without name', () => (
-    <Row>
-      <Col span={8}>
-        <Avatar src={photoJulia} name="Julia Nguyen" />
-      </Col>
-      <Col span={8}>
-        <Avatar src={photoTara} name="Tara Swenson" />
-      </Col>
-      <Col span={8}>
-        <Avatar src="NonExistentFile.jpg" name="Default On Loading Error" />
-      </Col>
-    </Row>
+    <Avatar src={photoTara} name="Tara Swenson" />
+  ))
+  .add('Without name and invalid image', () => (
+    <Avatar src="NonExistentFile.jpg" name="Default On Loading Error" />
+  ))
+  .add('With name', () => (
+    <Avatar src={photoTara} name="Tara Swenson" displayName />
+  ))
+  .add('With name and invalid image', () => (
+    <Avatar
+      src="NonExistentFile.jpg"
+      name="Default On Loading Error"
+      displayName
+    />
   ));
