e6025667b8ef6d015f96eb99284764cde7ea7629
Julia Nguyen <tojulianguyen@gmail.com>
2018-09-09 23:38:36 -0700

    Remove antd

--- client/app/stories/Resource.stories.jsx
+++ client/app/stories/Resource.stories.jsx
@@ -1,4 +1,3 @@
-import { Col, Row } from 'antd';
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Resource } from '../components/Resource';
@@ -13,100 +12,35 @@ const tags = [
 ];
 
 storiesOf('Resource', module)
-  .add('Tagged Resource', () => (
-    <Row gutter={24}>
-      <Col span={8}>
-        <Resource
-          tagged
-          tags={[]}
-          title={'LifeSIGNS: Self Injury Guidance & Network Support (UK)'}
-          link={'http://www.lifesigns.org.uk/'}
-        />
-      </Col>
-      <Col span={8}>
-        <Resource
-          tagged
-          tags={tags}
-          title={'LifeSIGNS: Self Injury Guidance & Network Support (UK)'}
-          link={'http://www.lifesigns.org.uk/'}
-        />
-      </Col>
-      <Col span={8}>
-        <Resource
-          tagged
-          tags={tags.concat(tags)}
-          title={'LifeSIGNS: Self Injury Guidance & Network Support (UK)'}
-          link={'http://www.lifesigns.org.uk/'}
-        />
-      </Col>
-    </Row>
+  .add('With tags', () => (
+    <Resource
+      tagged
+      tags={tags.concat(tags)}
+      title={'LifeSIGNS: Self Injury Guidance & Network Support (UK)'}
+      link={'http://www.lifesigns.org.uk/'}
+    />
   ))
-
-  .add('External Resource', () => (
-    <Row gutter={24}>
-      <Col span={8}>
-        <Resource
-          external
-          title={'Invisible Illnesses: depression is an ocean'}
-          link={'www.if-me.org'}
-        />
-      </Col>
-      <Col span={8}>
-        <Resource
-          external
-          title={'Invisible Illnesses: depression is an ocean'}
-          link={'www.if-me.org'}
-          author={'Desi Rottman'}
-        />
-      </Col>
-      <Col span={8}>
-        <Resource
-          external
-          title={
-            'A very long title for a resource that should wrap to two lines and then some or not'
-          }
-          link={'www.if-me.org'}
-          author={
-            'Author with a very very long name that is usually an edge case'
-          }
-        />
-      </Col>
-    </Row>
+  .add('Without tags', () => (
+    <Resource
+      external
+      title={
+        'A very long title for a resource that should wrap to two lines and then some or not'
+      }
+      link={'www.if-me.org'}
+      author={
+        'Author with a very very long name that is usually an edge case'
+      }
+    />
   ))
-
-  .add('Combination Resource', () => (
-    <Row gutter={24}>
-      <Col span={8}>
-        <Resource
-          tagged
-          external
-          tags={[]}
-          title={'Invisible Illnesses: depression is an ocean'}
-          link={'www.if-me.org'}
-          author={'Desi Rottman'}
-        />
-      </Col>
-      <Col span={8}>
-        <Resource
-          tagged
-          external
-          tags={tags}
-          title={'Invisible Illnesses: depression is an ocean'}
-          link={'www.if-me.org'}
-          author={'Desi Rottman'}
-        />
-      </Col>
-      <Col span={8}>
-        <Resource
-          tagged
-          external
-          tags={tags.concat(tags)}
-          title={
-            'Invisible Illnesses: depression is an ocean, and another measure to consider'
-          }
-          link={'www.if-me.org'}
-          author={'Desi Rottman'}
-        />
-      </Col>
-    </Row>
+  .add('With all options', () => (
+    <Resource
+      tagged
+      external
+      tags={tags.concat(tags)}
+      title={
+        'Invisible Illnesses: depression is an ocean, and another measure to consider'
+      }
+      link={'www.if-me.org'}
+      author={'Desi Rottman'}
+    />
   ));
