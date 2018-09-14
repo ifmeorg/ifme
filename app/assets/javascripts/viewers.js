98d6a9e5d0d9d1d19e2abde527d0f6f2c3ddfb36
Julia Nguyen <tojulianguyen@gmail.com>
2018-09-14 00:54:24 -0700

    Form in storybook

--- app/assets/javascripts/viewers.js
+++
@@ -1,9 +0,0 @@
-var onReadyViewers = function() {
-  if (newOrEdit(["moments", "strategies"])) {
-    $("#viewers_all").click(function() {
-      $("#viewers_list :checkbox").prop("checked", $(this).prop("checked"));
-    });
-  }
-};
-
-loadPage(onReadyViewers);
--- client/app/components/Input/InputDefault.jsx
+++ client/app/components/Input/InputDefault.jsx
@@ -20,67 +20,53 @@ export type Props = {
   hasError?: Function,
 };
 
-export type State = {
-  value: any,
+const onChangeBlur = (
+  e: SyntheticEvent<HTMLInputElement>,
+  required: ?boolean,
+  hasError: ?Function,
+) => {
+  const { value } = e.currentTarget;
+  if (required && hasError) {
+    hasError(!value);
+  }
 };
 
-export class InputDefault extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { value: props.value || '' };
-  }
-
-  changeValue = (e: SyntheticEvent<HTMLInputElement>) => {
-    const { required, hasError } = this.props;
-    const { value } = e.currentTarget;
-    if (required && hasError) {
-      hasError(!value);
-    }
-    this.setState({ value });
-  };
-
-  onBlur = () => {
-    const { required, hasError } = this.props;
-    const { value } = this.state;
-    if (required && hasError) {
-      hasError(!value);
-    }
-  };
-
-  render() {
-    const {
-      id,
-      type,
-      name,
-      placeholder,
-      readOnly,
-      disabled,
-      required,
-      minLength,
-      maxLength,
-      min,
-      max,
-    } = this.props;
-    const { value } = this.state;
-    if (!REQUIRES_DEFAULT.includes(type)) return null;
-    return (
-      <input
-        className={css.default}
-        id={id}
-        type={type}
-        name={name}
-        value={value}
-        placeholder={placeholder}
-        readOnly={readOnly}
-        disabled={disabled}
-        required={required}
-        minLength={minLength}
-        maxLength={maxLength}
-        min={min}
-        max={max}
-        onChange={this.changeValue}
-        onBlur={this.onBlur}
-      />
-    );
-  }
-}
+export const InputDefault = (props: Props) => {
+  const {
+    id,
+    type,
+    name,
+    placeholder,
+    readOnly,
+    disabled,
+    required,
+    minLength,
+    maxLength,
+    min,
+    max,
+    value,
+    hasError,
+  } = props;
+  if (!REQUIRES_DEFAULT.includes(type)) return null;
+  return (
+    <input
+      className={css.default}
+      id={id}
+      type={type}
+      name={name}
+      defaultValue={value}
+      placeholder={placeholder}
+      readOnly={readOnly}
+      disabled={disabled}
+      required={required}
+      minLength={minLength}
+      maxLength={maxLength}
+      min={min}
+      max={max}
+      onChange={(e: SyntheticEvent<HTMLInputElement>) => onChangeBlur(e, required, hasError)
+      }
+      onBlur={(e: SyntheticEvent<HTMLInputElement>) => onChangeBlur(e, required, hasError)
+      }
+    />
+  );
+};
