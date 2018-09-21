a113552bd9fa10c519eec8a8fbb9e2056770f905
prateksha.u <prateksha.u@iiitb.org>
2018-09-21 10:50:48 +0530

    Tests for reports_controller

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -1,2 +1,24 @@
-RSpec.describe ReportsController, type: :controller do
+# == Schema Information
+#
+# Table name: reports
+#  reporter_id             :string        
+#  reportee_id             :string
+#  comments                :text
+#  commentable_type        :string
+#  commentable_id          :integer
+
+describe ReportsController, type: :controller do
+  describe "#create" do
+    context "as an authenticated user" do
+      before do
+        @report = FactoryBot.create(:report)
+      end
+      it "adds a report" do
+        report_params = FactoryBot.attributes_for(:report)
+        expect {
+          report :create, params: { report: report_params }
+        }
+      end
+    end
+  end
 end
