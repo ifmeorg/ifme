6ca5c721ca7de55018285d450be5f7babcfd4c20
Mia <43491270+mialahmoka@users.noreply.github.com>
2018-09-24 00:52:13 -0700

    Update application.rb (#1000)
    added security headers, split line 51 to make rubocop happy
    Closes #976

--- config/application.rb
+++ config/application.rb
@@ -37,8 +37,19 @@ module Ifme
       end
     }
 
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'SAMEORIGIN',
+      'X-XSS-Protection' => '1; mode=block',
+      'X-Content-Type-Options' => 'nosniff',
+      'X-Download-Options' => 'noopen',
+      'X-Permitted-Cross-Domain-Policies' => 'none',
+      'Referrer-Policy' => 'strict-origin-when-cross-origin',
+      'Strict-Transport-Security' => 'max-age=31536000'
+    }
+
     # gzip the html/json responses
-    config.middleware.use Rack::Deflater, include: %w[text/html application/json image/svg+xml]
+    config.middleware.use Rack::Deflater,
+      include: %w[text/html application/json image/svg+xml]
     # export translations for use in javascript
     config.middleware.use I18n::JS::Middleware
 
