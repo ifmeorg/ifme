2fb86d08f53efa0d9d361a255c67b61f8eed2e0e
Julia Nguyen <tojulianguyen@gmail.com>
2018-09-23 23:40:05 -0700

    Fix Babel weirdness

--- client/app/startup/registration.jsx
+++ client/app/startup/registration.jsx
@@ -3,7 +3,7 @@
  * Add components only if needed in current application, because
  * this bundle will be loaded in production.
  */
-
+import '@babel/polyfill';
 import ReactOnRails from 'react-on-rails';
 import { loadLocales } from '../libs/i18n/I18nSetup';
 import '../styles/_global.scss';
