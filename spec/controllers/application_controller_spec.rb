4f11bed2a529d3fc7caba51bb45fc13d1db747c4
Julia Nguyen <tojulianguyen@gmail.com>
2018-09-26 11:27:47 -0700

    PR Feedback

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -148,18 +148,18 @@ describe ApplicationController do
     it 'has more than one moment created this month' do
       new_moment1 = create(:moment, user_id: user1.id)
       new_moment2 = create(:moment, user_id: user1.id)
-      expect(controller.moments_stats).to eq('<div class="center" class="stats">You have written a <strong>total</strong> of <strong>2</strong> moments.</div>')
+      expect(controller.moments_stats).to eq('<div class="center stats">You have written a <strong>total</strong> of <strong>2</strong> moments.</div>')
     end
 
     it 'has more than one moment created on different months' do
       new_moment1 = create(:moment, user_id: user1.id, created_at: '2014-01-01 00:00:00')
       new_moment2 = create(:moment, user_id: user1.id)
 
-      expect(controller.moments_stats).to eq('<div class="center" class="stats">You have written a <strong>total</strong> of <strong>2</strong> moments. This <strong>month</strong> you wrote <strong>1</strong> moment.</div>')
+      expect(controller.moments_stats).to eq('<div class="center stats">You have written a <strong>total</strong> of <strong>2</strong> moments. This <strong>month</strong> you wrote <strong>1</strong> moment.</div>')
 
       new_moment3 = create(:moment, user_id: user1.id)
 
-      expect(controller.moments_stats).to eq('<div class="center" class="stats">You have written a <strong>total</strong> of <strong>3</strong> moments. This <strong>month</strong> you wrote <strong>2</strong> moments.</div>')
+      expect(controller.moments_stats).to eq('<div class="center stats">You have written a <strong>total</strong> of <strong>3</strong> moments. This <strong>month</strong> you wrote <strong>2</strong> moments.</div>')
     end
   end
 end
