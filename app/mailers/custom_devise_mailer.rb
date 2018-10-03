c4051d17d8523065891ff3e28fa036df81279d40
Imran Ahmad <imran@milaap.org>
2018-10-03 14:00:21 +0530

    Refactoring invitations instructions

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -1,17 +1,14 @@
 # frozen_string_literal: true
-# inviter_name method from InvitationsHelper
-include InvitationsHelper
-
 class CustomDeviseMailer < Devise::Mailer
   protected
 
   def subject_for(key)
     return super unless key.to_s == 'invitation_instructions'
 
-    if inviter_name(@resource)
+    if @resource.invitee&.name
       I18n.t(
         'devise.mailer.invitation_instructions.subject',
-        name: inviter_name(@resource)
+        name: @resource.invitee&.name
       )
     else
       I18n.t('devise.mailer.invitation_instructions.subject_nameless')
