7b7cf121013763af0f5f0dfbd0903f8268e04128
Julia Nguyen <tojulianguyen@gmail.com>
2018-10-05 12:36:01 -0700

    PR Feedback #1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -126,5 +126,6 @@ Rails.application.configure do
 
   Raven.configure do |config|
     config.dsn = ENV['SENTRY_DSN']
+    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   end
 end
--- config/initializers/sentry.rb
+++
@@ -1,5 +0,0 @@
-if Rails.env.production?
-  Raven.configure do |config|
-    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-  end
-end
