f0115a533fa87ef5af86fbef28536c0b0fcc26c3
Julia Nguyen <tojulianguyen@gmail.com>
2018-10-05 13:14:49 -0700

    Fix Sentry bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -233,7 +233,9 @@ class ApplicationController < ActionController::Base
   end
 
   def set_raven_context
-    Raven.user_context(id: current_user.id)
+    if user_signed_in?
+      Raven.user_context(id: current_user.id)
+    end
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
 end
