81c6455e9438c54c77bfd73b93ea08bcb99d381d
Jiradeto <jiradet.jd@gmail.com>
2018-10-08 20:06:52 +0200

    Move tag_usage method in application_controller.rb into its own helper (#1012)
        * extract method get_moments and get_moods

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -5,18 +5,9 @@ module TagsHelper
     result = []
     moments = Moment.where(user_id: user_id).order('created_at DESC')
     if data_type == 'category'
-      strategies = Strategy.where(user_id: user_id).order('created_at DESC')
-      [moments, strategies].each do |records|
-        objs = []
-        records.find_each do |r|
-          objs.push(r) if data_included?(data_type, data_id, r)
-        end
-        result << objs
-      end
+      result = get_moments(data_id, data_type, user_id, moments)
     elsif data_type.in?(%w[mood strategy])
-      moments.find_each do |m|
-        result << m if data_included?(data_type, data_id, m)
-      end
+      result = get_moods(data_id, data_type, moments)
     end
     result
   end
@@ -24,9 +15,28 @@ module TagsHelper
 
   private
 
-  def data_included?(data_type, data_id, data)
-    return false unless data_type.in?(%w[category mood strategy])
+  def get_moods(data_id, data_type, moments)
+    result = []
+    moments.find_each do |m|
+      result << m if data_included?(data_type, data_id, m)
+    end
+    result
+  end
 
-    data_id.in?(data[data_type])
+  def get_moments(data_id, data_type, user_id, moments)
+    result = []
+    strategies = Strategy.where(user_id: user_id).order('created_at DESC')
+    [moments, strategies].each do |records|
+      objs = []
+      records.find_each do |r|
+        objs.push(r) if data_included?(data_type, data_id, r)
+      end
+      result << objs
+    end
+    result
+  end
+
+  def data_included?(data_type, data_id, data)
+    data_type.in?(%w[category mood strategy]) && data_id.in?(data[data_type])
   end
 end
