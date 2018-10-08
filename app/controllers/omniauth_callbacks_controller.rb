603013afb30ce0748109ae0f99e13bb9fd73c312
Hannah Masila <hannahmasila@gmail.com>
2018-10-08 15:50:58 +0300

    extract user to it's own method

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,14 +1,8 @@
 # frozen_string_literal: true
 
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # rubocop:disable MethodLength
   def google_oauth2
-    @user = User.find_for_google_oauth2(
-      request.env['omniauth.auth'],
-      current_user
-    )
-
-    if @user
+    if user
       flash[:notice] = I18n.t('devise.omniauth_callbacks.success',
                               kind: t('omniauth.google'))
       sign_in_and_redirect @user, event: :authentication
@@ -16,5 +10,11 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_session_path, notice: t('omniauth.access_denied')
     end
   end
-  # rubocop:enable MethodLength
+  
+  def user
+    @user ||= User.find_for_google_oauth2(
+      request.env['omniauth.auth'],
+      current_user
+    )
+  end
 end
