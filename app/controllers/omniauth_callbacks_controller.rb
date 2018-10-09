2e2f16c52cbede51204d268b1e219c5df9ba489d
Dipil Singh Saud <dipil.saud@gmail.com>
2018-10-10 02:05:59 +0545

    #507 Accept invites through google oauth.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,6 +3,8 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     if user.present?
+      user.accept_invitation! if invitation_token
+
       flash[:notice] = I18n.t('devise.omniauth_callbacks.success',
                               kind: t('omniauth.google'))
       sign_in_and_redirect @user, event: :authentication
@@ -11,10 +13,17 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
+  private
+
   def user
     @user ||= User.find_for_google_oauth2(
       request.env['omniauth.auth'],
       current_user
     )
+  end
+
+  # returns the invitation token passed in with the callback url
+  def invitation_token
+    request.env.dig('omniauth.params', 'invitation_token')
   end
 end
--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -1,49 +1,44 @@
 # frozen_string_literal: true
 
 RSpec.describe OmniauthCallbacksController, type: :controller do
+  shared_examples 'successful sign in with oauth details' do
+    it 'should sign in and redirect to root' do
+      expect(subject.current_user).to eq user
+      expect(response).to redirect_to('/')
+    end
+
+    it 'should create authentication with google_oauth2' do
+      expect(user.reload.google_oauth2_enabled?).to eq true
+    end
+  end
+
   describe 'GET #google_oauth2' do
+    before { stub_env_for_omniauth }
+    let(:oauth_email) { request.env['omniauth.auth']['info']['email'] }
+    let(:oauth_user) { User.find_by(email: oauth_email) }
+
     context 'when google_oauth2 email doesnt exist in the system' do
-      let(:user) { User.find_by(email: 'example@xyze.it') }
-      
-      before do
-        stub_env_for_omniauth
-        get :google_oauth2
-      end
+      let(:user) { oauth_user }
+
+      before { get :google_oauth2 }
 
       it 'creates user with info in google_oauth2' do
         expect(user.name).to eq 'Test User'
       end
 
-      it 'should create authentication with google_oauth2' do
-        expect(user.google_oauth2_enabled?).to eq true
-      end
-
-      it 'should sign in the user and redirect_to root' do
-        expect(subject.current_user).to eq user
-        expect(response).to redirect_to('/')
-      end
+      include_examples 'successful sign in with oauth details'
     end
-    
+
     context 'when google_oauth2 email already exist in the system' do
       let!(:user) { create(:user, email: 'example@xyze.it') }
-      
-      before do
-        stub_env_for_omniauth
-        get :google_oauth2
-      end
+
+      before { get :google_oauth2 }
 
       it 'updates the user with google_oauth2 credentials' do
         expect(user.reload.token).to eq 'abcdefg12345'
       end
 
-      it 'should create authentication with google_oauth2' do
-        expect(user.reload.google_oauth2_enabled?).to eq true
-      end
-
-      it 'should sign in the user and redirect_to root' do
-        expect(subject.current_user).to eq user
-        expect(response).to redirect_to('/')
-      end  
+      include_examples 'successful sign in with oauth details'
     end
 
     context 'when google_oauth2 enabling fails' do
@@ -57,6 +52,33 @@ RSpec.describe OmniauthCallbacksController, type: :controller do
       it 'redirects to sign in path' do
         expect(response).to redirect_to(new_user_session_path)
       end
-    end 
+    end
+
+    context 'when an invitation_token is passed in' do
+      let(:inviter) { create(:user) }
+      let(:invitee_email) { oauth_email }
+      let!(:invitee) { User.invite!({ email: invitee_email }, inviter) }
+
+      before do
+        request.env['omniauth.params'] = { 'invitation_token' => invitee.invitation_token }
+        get :google_oauth2
+      end
+
+      context 'when the user logs in with the same email as the invitation' do
+        let(:user) { invitee }
+
+        include_examples 'successful sign in with oauth details'
+        it { expect(user.reload.invitation_accepted_at).to be_present }
+      end
+
+      context 'when the user logs in with a different email from the invitation' do
+        let(:invitee_email) { 'no-invite@xyze.it' }
+        let(:user) { oauth_user }
+
+        include_examples 'successful sign in with oauth details'
+
+        it { expect(user.reload.invitation_accepted_at).to be_blank }
+      end
+    end
   end
 end
