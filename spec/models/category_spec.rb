6f39035ffd2693d6ef33de221d7298a5193196e8
kensupermen <votantho93@gmail.com>
2018-10-13 21:50:36 +0700

    fix typo

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,9 +1,3 @@
 RSpec.describe Category, type: :model do
   it { is_expected.to validate_presence_of(:user_id) }
-
-  describe '#link' do
-    it 'category path' do
-      # binding.pry
-    end
-  end
 end
