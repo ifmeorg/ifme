d811888bfe75ee73710951205d6c17d71e36cd34
Lee Mulvey <lmulvey@me.com>
2018-10-12 22:48:39 -0600

    Add contributor avatar blurb to new German locale.

--- client/app/components/Modal/index.jsx
+++ client/app/components/Modal/index.jsx
@@ -1,8 +1,6 @@
 // @flow
 import React from 'react';
 import renderHTML from 'react-render-html';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import css from './Modal.scss';
 import { I18n } from '../../libs/i18n';
 import { Avatar } from '../Avatar';
@@ -54,11 +52,22 @@ export class Modal extends React.Component<Props, State> {
           tabIndex={0}
           aria-label={I18n.t('close')}
         >
-          <FontAwesomeIcon icon={faTimes} />
+          {this.displayCloseSvg()}
         </div>
       </div>
     );
   };
+
+  displayCloseSvg = () => (
+    <svg width="36" height="35" viewBox="0 0 36 35" fill="#D0839" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M18.4141 17.7071L34.707 34L34 34.7071L17.707 18.4141L1.41406 34.7071L0.707031 34L17 17.7071L0 0.707108L0.707031 0L17.707 17L34.707 0L35.4141 0.707108L18.4141 17.7071Z"
+        fill="#D0839"
+      />
+    </svg>
+  );
 
   displayModalBody = () => {
     const { body } = this.props;
