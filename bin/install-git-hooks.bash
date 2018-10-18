5d1b335384315ebe540d1a4eec97269929ca0517
Michael Jalkio <mjalkio@gmail.com>
2018-10-17 20:22:33 -0700

    Override git hooks if they exist

--- bin/install-git-hooks.bash
+++ bin/install-git-hooks.bash
@@ -4,5 +4,5 @@ FILE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 for f in ${FILE_DIR}/../git-hooks/*; do
     filename=$(basename ${f})
-    ln -s ${f} ${FILE_DIR}/../.git/hooks/${filename}
+    ln -sf ${f} ${FILE_DIR}/../.git/hooks/${filename}
 done
