b0fd73007bcebe7b055a2ce04dac0556d93a035d
Carl Summers <31711543+CSumm@users.noreply.github.com>
2018-10-17 23:06:46 -0400

    Delete SideNav.stories.jsx

--- client/app/stories/SideNav.stories.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { SideNav } from '../components/SideNav/index';
-
-storiesOf('SideNav', module)
-  .add('SideNav-Silver', () => <SideNav color="silver" />);
