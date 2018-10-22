e91041df57f498cebc55982ea70091db3d858573
Lee Mulvey <lmulvey@me.com>
2018-10-21 18:18:09 -0600

    Change responsive behaviour for modal. Replace SVG close icon with FAIcon.

--- client/app/stories/Modal.stories.jsx
+++ client/app/stories/Modal.stories.jsx
@@ -7,6 +7,8 @@ const title = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit';
 
 const bodyText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis commodo erat quis ipsum sodales condimentum. Vestibulum nec posuere lorem. Nulla condimentum, dui et sagittis hendrerit, enim sapien luctus orci, non vehicula nibh massa vel risus. Nunc vitae aliquam lacus. Donec dolor velit, blandit eu erat luctus, aliquam congue augue. Curabitur interdum leo id orci porttitor, eget dictum nibh gravida. Praesent facilisis, justo non convallis consectetur, tellus est egestas erat, quis commodo risus tellus consequat est. Curabitur quis massa non est pharetra mollis. Proin finibus ipsum massa, et semper ipsum ultricies vel. Mauris dignissim auctor egestas. Aenean elit ante, egestas eu ligula a, tincidunt suscipit diam. Nulla ultrices tempus turpis ac cursus. Suspendisse congue sem nec ex vehicula, in vestibulum leo ultricies. Morbi ac faucibus lorem. Donec vitae tellus id quam aliquet iaculis. Nam aliquet quis ante faucibus convallis.';
 
+const longBodyText = `${bodyText} ${bodyText} ${bodyText}`;
+
 const bodyHTML = (
   <div>
     <button type="button">Here is a button</button>
@@ -21,6 +23,9 @@ const openListener = () => {
 storiesOf('Modal', module)
   .add('Text values for element and body', () => (
     <Modal element="Hello" body={bodyText} title={title} />
+  ))
+  .add('Long text value forcing body to scroll', () => (
+    <Modal element="Hello" body={longBodyText} title={title} />
   ))
   .add('Text values for element and body with openListener', () => (
     <Modal
