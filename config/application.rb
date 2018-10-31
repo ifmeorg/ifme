10c38c80187096f4fbf0786a2cd1667e4dbcb27a
Jason Alsip <13713268+matcha440@users.noreply.github.com>
2018-10-30 20:02:14 -0400

    Add ja to available locales

--- config/application.rb
+++ config/application.rb
@@ -51,7 +51,7 @@ module Ifme
     config.middleware.use Rack::Deflater,
       include: %w[text/html application/json image/svg+xml]
 
-    config.i18n.available_locales = ['en', 'es', 'de', 'it', 'nb', 'nl', 'pt-BR', 'sv', 'vi'].map &:to_sym
+    config.i18n.available_locales = ['en', 'es', 'de', 'it', 'nb', 'nl', 'pt-BR', 'sv', 'vi', 'ja'].map &:to_sym
     config.i18n.default_locale = :en
   end
 end
