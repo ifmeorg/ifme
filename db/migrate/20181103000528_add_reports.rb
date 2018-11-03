94712310cf7268ce29896da19a72b92cfbf1a722
Julia Nguyen <tojulianguyen@gmail.com>
2018-11-02 20:09:43 -0400

    Fix migration bugs

---
+++ db/migrate/20181103000528_add_reports.rb
@@ -0,0 +1,13 @@
+class AddReports < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reports do |t|
+      t.integer "reporter_id"
+      t.integer "reportee_id"
+      t.text "reasons"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.integer "comment_id"
+      t.integer "user_id"
+    end
+  end
+end
