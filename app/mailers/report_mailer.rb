e3f220f2ac48c71edd821e06d752b4db77fac320
Julia Nguyen <tojulianguyen@gmail.com>
2018-11-04 00:51:05 -0400

    Refactor ReportsHelper

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,22 +1,20 @@
 # frozen_string_literal: true
 class ReportMailer < ApplicationMailer
   default from: ENV['SMTP_ADDRESS']
-  def reported_email(recipient, reportee, email_id)
-    @recipient = User.find_by(id: recipient)
-    @reportee = User.find_by(id: reportee)
-    @email_id = email_id
+
+  def reported_email(recipient, reportee)
+    @recipient = recipient
+    @reportee = reportee
     mail(
-      to: email_id,
+      to: @recipient.email,
       subject: t('notifications.mailer.reported_user_subject')
     )
   end
 
-  def reportee_email(recipient, reporter, email_id)
-    @recipient = User.find_by(id: recipient)
-    @reporter = User.find_by(id: reporter)
-    @email_id = email_id
+  def reportee_email(recipient)
+    @recipient = recipient
     mail(
-      to: email_id,
+      to: @recipient.email,
       subject: t('notifications.mailer.reportee_user_subject')
     )
   end
--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -1,28 +1,61 @@
-describe Report, type: :model do
-  it 'is valid with reporter_id, reportee_id, reasons' do
-    report = Report.new(
-    reporter_id: '1',
-    reportee_id: '2',
-    reasons: 'You have been reported.'
-    )
-    expect(report).to be_valid
+# frozen_string_literal: true
+# == Schema Information
+#
+# Table name: reports
+#
+#  id          :bigint(8)        not null, primary key
+#  reporter_id :integer
+#  reportee_id :integer
+#  reasons     :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  comment_id  :integer
+#
+
+describe Report do
+  let(:user1) { create(:user1) }
+  let(:user2) { create(:user2) }
+  let(:report_params) do
+    {
+      reporter_id: user1.id,
+      reportee_id: user2.id,
+      reasons: 'You have been reported.'
+    }
   end
 
-  it 'is invalid without reporter_id' do
-    report = Report.new(reporter_id: nil)
-    report.valid?
-    expect(report.errors[:reporter_id]).to include("can't be blank")
+  describe '#new' do
+    let(:error) { "can't be blank" }
+
+    it 'is valid with reporter_id, reportee_id, reasons' do
+      report = Report.new(report_params)
+      expect(report).to be_valid
+    end
+
+    it 'is invalid without reporter_id' do
+      report = Report.new(reporter_id: nil)
+      report.valid?
+      expect(report.errors[:reporter_id]).to include(error)
+    end
+
+    it 'is invalid without reportee_id' do
+      report = Report.new(reportee_id: nil)
+      report.valid?
+      expect(report.errors[:reportee_id]).to include(error)
+    end
+
+    it 'is invalid without comments' do
+      report = Report.new(reasons: nil)
+      report.valid?
+      expect(report.errors[:reasons]).to include(error)
+    end
   end
 
-  it 'is invalid without reportee_id' do
-    report = Report.new(reportee_id: nil)
-    report.valid?
-    expect(report.errors[:reportee_id]).to include("can't be blank")
-  end
+  describe '#send_emails' do
+    before(:all) { Devise.mailer.deliveries.clear }
 
-  it 'is invalid without comments' do
-    report = Report.new(reasons: nil)
-    report.valid?
-    expect(report.errors[:reasons]).to include("can't be blank")
+    it 'sends emails successfully' do
+      Report.create(report_params)
+      expect(Devise.mailer.deliveries.count).to eq(2)
+    end
   end
 end
