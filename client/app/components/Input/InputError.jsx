cb97f6e957fa54036e440cfd653ebe1db2dba694
Mera Gangapersaud <mera.gangapersaud@gmail.com>
2018-11-22 10:11:15 -0800

    Refactored InputError component

--- client/app/components/Input/InputError.jsx
+++ client/app/components/Input/InputError.jsx
@@ -1,39 +1,26 @@
 // @flow
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faAsterisk,
-  faQuestion,
-  faExclamation,
-} from '@fortawesome/free-solid-svg-icons';
-import { Tooltip } from '../Tooltip';
+import { faExclamation } from '@fortawesome/free-solid-svg-icons';
 import css from './Input.scss';
 import globalCss from '../../styles/_global.scss';
 import { I18n } from '../../libs/i18n';
 
 export type Props = {
-  label: string,
-  required?: boolean,
-  info?: string,
   error?: boolean,
 };
 
-
 export const InputError = (props: Props) => {
-  const {
-    error, label, required, info,
-  } = props;
+  const { error } = props;
   return (
-    <div className={`${globalCss.gridRowSpaceBetween} ${css.label}`}>
     <div className={css.labelInfo}>
-    {error ? (
-        <div className="labelError">
+      {error ? (
+        <div className={`labelError ${globalCss.smallMarginTop} ${css.label}`}>
           <FontAwesomeIcon icon={faExclamation} />
           &emsp;
           {I18n.t('common.form.error_explanation')}
         </div>
       ) : null}
     </div>
-  </div>
   );
 };
