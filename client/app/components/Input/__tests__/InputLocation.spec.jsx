66e394b43e61dee22b014c355b3e2b49f3a98b5e
Julia Nguyen <tojulianguyen@gmail.com>
2018-12-02 20:19:12 -0800

    Add missing test for InputLocation

--- client/app/components/Input/__tests__/InputLocation.spec.jsx
+++ client/app/components/Input/__tests__/InputLocation.spec.jsx
@@ -4,17 +4,37 @@ import React from 'react';
 import { InputLocation } from '../InputLocation';
 
 describe('InputLocation', () => {
-  describe('handle location input', () => {
+  describe('has no initialized value', () => {
     it('updates the value of the input', () => {
       const wrapper = shallow(
-        <InputLocation placeholder="Location" apiKey="fakeKey" />,
+        <InputLocation placeholder="Location" apiKey="fakeKey" id="fakeId" />,
       );
       const value = 'Test Location';
-
       wrapper
         .find('LocationAutocomplete')
         .simulate('change', { target: { value } });
+      expect(wrapper.find('LocationAutocomplete').props().value).toEqual(value);
+    });
+  });
 
+  describe('has an initialized value', () => {
+    it('updates the value of the input', () => {
+      const initializedValue = 'Hey';
+      const wrapper = shallow(
+        <InputLocation
+          placeholder="Location"
+          apiKey="fakeKey"
+          id="fakeId"
+          value={initializedValue}
+        />,
+      );
+      expect(wrapper.find('LocationAutocomplete').props().value).toEqual(
+        initializedValue,
+      );
+      const value = 'Test Location';
+      wrapper
+        .find('LocationAutocomplete')
+        .simulate('change', { target: { value } });
       expect(wrapper.find('LocationAutocomplete').props().value).toEqual(value);
     });
   });
