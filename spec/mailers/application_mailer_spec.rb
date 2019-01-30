27117b02617e934884886bff9ab7bb0143ce4081
Tsara Fatma L <tsara.tf@gmail.com>
2019-01-30 15:57:41 +0700

    Add spec to test load_logo_inline

---
+++ spec/mailers/application_mailer_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+describe 'ApplicationMailer' do
+  describe '#load_logo_inline' do
+    let(:user1) { create(:user1) }
+    let(:user2) { create(:user2) }
+
+    subject(:email) { ReportMailer.reported_email(user1, user2) }
+
+    it 'adds logo to email attachments' do
+      email = ReportMailer.reported_email(user1, user2)
+
+      email.deliver
+
+      expect(email.attachments[0].filename).to eq('logo@2x.png')
+    end
+  end
+end
