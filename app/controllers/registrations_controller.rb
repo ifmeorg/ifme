53306b3183a306c3c99be99e389ef0377e7ccf1d
Julia Nguyen <tojulianguyen@gmail.com>
2019-04-18 22:13:34 -0700

    Make methods in RegistrationsController protected

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
-
 class RegistrationsController < Devise::RegistrationsController
+  protected
+
   def after_update_path_for(_resource)
     edit_user_registration_path
   end
