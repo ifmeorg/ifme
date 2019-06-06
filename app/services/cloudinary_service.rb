73d7b686b2354c3ea1b9ca9c32954bde1fa4c0f3
Benjamin Wacha <bmwachajr@gmail.com>
2019-06-06 15:44:50 +0300

    complete debugging failing tets

--- app/services/cloudinary_service.rb
+++ app/services/cloudinary_service.rb
@@ -2,18 +2,19 @@
 include CloudinaryHelper
 include ReactOnRailsHelper
 
+# TODO: better exception handling. and error reporting
 class CloudinaryService
   class << self
     def upload(file, options = {})
       Cloudinary::Uploader.upload(file, options)
-    rescue StandardError => error
-      puts error
+    rescue StandardError
+      nil
     end
 
     def delete(public_id, options = {})
       Cloudinary::Uploader.destroy(public_id, options)
-    rescue StandardError => error
-      puts error
+    rescue StandardError
+      nil
     end
   end
 end
