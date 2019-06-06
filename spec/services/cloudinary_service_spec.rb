ddb4291a5910265a1fa0c07ec0ddc520504e1886
Benjamin Wacha <bmwachajr@gmail.com>
2019-06-06 15:44:50 +0300

    complete debugging failing tets

--- spec/services/cloudinary_service_spec.rb
+++ spec/services/cloudinary_service_spec.rb
@@ -1,25 +1,12 @@
 # frozen_string_literal: true
 FILENAME = "moment.jpeg"
 FILE_PATH = "/spec/uploads/"
-# OPTIONS = {}
 
 describe CloudinaryService do
   subject { described_class }
 
   describe '#upload' do
-    context 'has no options' do
-      it 'returns json response from cloudinary' do
-        file = File.new(File.join(::Rails.root.to_s, FILE_PATH, FILENAME))
-        response = subject.upload(file)
-
-        expect(response).to have_key("public_id")
-        expect(response).to have_key("secure_url")
-        expect(response).to have_key("signature")
-        expect(response["original_filename"]).to eq("moment")
-      end
-    end
-
-    context 'has options' do
+    context 'successfully' do
       it 'returns json response from cloudinary' do
         file = File.new(File.join(::Rails.root.to_s, FILE_PATH, FILENAME))
         response = subject.upload(file)
@@ -41,7 +28,7 @@ describe CloudinaryService do
   end
 
   describe '#delete' do
-    context 'has no options' do
+    context 'successfully' do
       it 'returns success json response from cloudinary' do
         # upload image
         file = File.new(File.join(::Rails.root.to_s, FILE_PATH, FILENAME))
@@ -50,19 +37,6 @@ describe CloudinaryService do
         expect(uploaded_file).to have_key("secure_url")
 
         # delete image
-        response = subject.delete(uploaded_file["public_id"])
-        expect(response).to eq({"result"=>"ok"})
-      end
-    end
-
-    context 'has options' do
-      it 'returns json response from cloudinary' do
-        # upload image
-        file = File.new(File.join(::Rails.root.to_s, FILE_PATH, FILENAME))
-        uploaded_file = subject.upload(file)
-        expect(uploaded_file).to have_key("public_id")
-        expect(uploaded_file).to have_key("secure_url")
-
         response = subject.delete(uploaded_file["public_id"])
         expect(response).to eq({"result"=>"ok"})
       end
