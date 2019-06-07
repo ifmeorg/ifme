c7a0cf0008cbe17e3fcdd64f9a4254c9512d3a7a
Janessa Tran <tranjanessa@gmail.com>
2019-06-06 19:54:43 -0500

    Add tests for PusherController

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -4,6 +4,10 @@ class PusherController < ApplicationController
   # stop rails CSRF protection for this action
   protect_from_forgery except: :auth
 
+  # POST /pusher/auth
+  #
+  # Returns an auth token from Pusher that looks like this
+  # {:auth=>":7944d5b3dc2915f6a9c55dbedcbe91037799bb3627fc868890c2a7fd6f5fde4b"}
   def auth
     if current_user
       response = Pusher[params[:channel_name]].authenticate(params[:socket_id])
--- spec/controllers/pusher_controller_spec.rb
+++ spec/controllers/pusher_controller_spec.rb
@@ -1,16 +1,27 @@
 describe PusherController do
 
   let(:user) { create(:user) }
-  let(:pusher) { create(:pusher) }
 
   describe 'auth' do
     context 'when the user is logged in' do
       before { sign_in user }
 
-    it 'renders pusher response notification' do
-        get :auth
-        expect(response).to render_template("auth")
+      it 'returns the pusher auth token in json' do
+        post :auth, params: { channel_name: 'janessa', socket_id: '123.456' }
+        json = JSON.parse(response.body)
+
+        expect(json['auth']).to_not be_nil
       end
     end
+
+    context 'when the user is not logged in' do
+
+      it 'returns a login redirect' do
+        post :auth, params: { channel_name: 'janessa', socket_id: '123.456' }
+
+        expect(response.status).to eq(302)
+      end
+    end
+
   end
 end
