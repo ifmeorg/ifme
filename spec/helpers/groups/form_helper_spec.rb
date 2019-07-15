b57fad7abdf83f74d58e69701d59a814b7ccac3a
stephen wright <scw248@gmail.com>
2019-07-15 14:30:48 -0500

    corrected spec back to groups_form_helper_spec.rb

--- spec/helpers/groups/form_helper_spec.rb
+++ spec/helpers/groups/form_helper_spec.rb
@@ -1,170 +1,88 @@
 describe GroupsFormHelper do
   let(:group) { create(:group) }
-  let(:inputs) { nil }
-  let(:action) { nil }
   
   before do
     @group = group 
   end
   
-  let(:valid_inputs) do
-    [
-      {
-        id: 'group_name',
-        type: 'text',
-        name: 'group[name]',
-        label: t('common.name'),
-        value: 'Test Group',
-        required: true,
-        dark: true
-      },
-      {
-        id: 'group_description',
-        type: 'textarea',
-        name: 'group[description]',
-        label: t('common.form.description'),
-        value: 'Group description',
-        required: true,
-        dark: true
-      }
-    ]
-  end
-
-  let(:valid_action) { '/groups' }
-  
   describe '#new_group_props' do
     subject { new_group_props }
 
-    context 'has no existing group' do
-      before do
-        @group = nil 
-      end
+    before do
+      @group = nil 
+    end
   
-      it 'returns correct props' do
-        expect(subject).to eq(
-          inputs: [
-            {
-              id: 'group_name',
-              type: 'text',
-              name: 'group[name]',
-              label: t('common.name'),
-              value: nil,
-              required: true,
-              dark: true
-            },
-            {
-              id: 'group_description',
-              type: 'textarea',
-              name: 'group[description]',
-              label: t('common.form.description'),
-              value: nil,
-              required: true,
-              dark: true
-            },
-            { id: 'submit', type: 'submit', value: 'Submit', dark: true }
-          ],
-          action: '/groups'
-        )
-        
-      end
+    it 'returns correct props' do
+      expect(subject).to eq(
+        inputs: [
+          {
+            id: 'group_name',
+            type: 'text',
+            name: 'group[name]',
+            label: t('common.name'),
+            value: nil,
+            required: true,
+            dark: true
+          },
+          {
+            id: 'group_description',
+            type: 'textarea',
+            name: 'group[description]',
+            label: t('common.form.description'),
+            value: nil,
+            required: true,
+            dark: true
+          },
+          { id: 'submit', type: 'submit', value: 'Submit', dark: true }
+        ],
+        action: '/groups'
+      )
+    end
+  end
+
+  describe '#edit_group_props' do
+    subject { edit_group_props }
+
+    before do
+      params[:controller] = 'application'
+      allow(controller).to receive(:action_name).and_return('edit')
     end
 
-    context 'has valid arguments' do
-      let(:inputs) { valid_inputs }
-      let(:action) { valid_action }
-      it 'returns correct props' do
-        expect(subject).to eq(
-          inputs: [
-            {
-              id: 'group_name',
-              type: 'text',
-              name: 'group[name]',
-              label: t('common.name'),
-              value: 'Test Group',
-              required: true,
-              dark: true
-            },
-            {
-              id: 'group_description',
-              type: 'textarea',
-              name: 'group[description]',
-              label: t('common.form.description'),
-              value: 'Group description',
-              required: true,
-              dark: true
-            },
-            { id: 'submit', type: 'submit', value: 'Submit', dark: true }
-          ],
-          action: '/groups'
-        )
-      end
-    end
-
-    describe '#edit_group_props' do
-      subject { edit_group_props }
-  
-      context 'has invalid arguments' do
-      
-          it 'returns correct props' do
-            expect(subject).to eq(
-              inputs: [
-                {
-                  id: 'group_name',
-                  type: 'text',
-                  name: 'group[name]',
-                  label: t('common.name'),
-                  value: nil,
-                  required: true,
-                  dark: true
-                },
-                {
-                  id: 'group_description',
-                  type: 'textarea',
-                  name: 'group[description]',
-                  label: t('common.form.description'),
-                  value: nil,
-                  required: true,
-                  dark: true
-                },
-                { id: 'submit', type: 'submit', value: 'Submit', dark: true }
-              ],
-              action: '/groups'
-            )
-            
-          end
-        end
-  
-      context 'has valid arguments' do
-        let(:inputs) { valid_inputs }
-        let(:action) { valid_action }
-        it 'returns correct props' do
-          expect(subject).to eq(
-            inputs: [
-              {
-                id: 'group_name',
-                type: 'text',
-                name: 'group[name]',
-                label: t('common.name'),
-                value: 'Test Group',
-                required: true,
-                dark: true
-              },
-              {
-                id: 'group_description',
-                type: 'textarea',
-                name: 'group[description]',
-                label: t('common.form.description'),
-                value: 'Group description',
-                required: true,
-                dark: true
-              },
-              { id: '_method', name: '_method', type: 'hidden', value: 'patch' },
-              { id: 'submit', type: 'submit', value: 'Submit', dark: true }
-            ],
-            action: '/groups/test-group'
-          )
-        end
-      end
+    it 'returns the correct props' do
+      expect(subject).to eq(
+        inputs: [
+          {
+            id: 'group_name',
+            type: 'text',
+            name: 'group[name]',
+            label: t('common.name'),
+            value: group.name,
+            required: true,
+            dark: true
+          },
+          {
+            id: 'group_description',
+            type: 'textarea',
+            name: 'group[description]',
+            label: t('common.form.description'),
+            value: group.description,
+            required: true,
+            dark: true
+          },
+          {
+            checkboxes: [],
+            dark: true,
+            id: 'group_leader',
+            label: 'Leaders',
+            name: 'group[leader]',
+            required: true,
+            type: 'checkboxGroup'
+          },
+          { id: '_method', name: '_method', type: 'hidden', value: 'patch' },
+          { id: 'submit', type: 'submit', value: 'Submit', dark: true }
+        ],
+        action: '/groups/test-group'
+      )
     end
   end
 end
