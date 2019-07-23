7202af1e25677ed94d5f0ce67b70a05c7cc8052a
Janessa Tran <tranjanessa@gmail.com>
2019-07-22 19:06:58 -0500

    Add specs for AvatarUploader

---
+++ spec/uploaders/avatar_uploader_spec.rb
@@ -0,0 +1,18 @@
+describe AvatarUploader do
+  let(:user) { create(:user) }
+
+  it 'wont upload if there is already an avatar' do
+    allow(user.avatar).to receive(:file).and_return('moment.jpeg')
+    allow(user.avatar.file).to receive(:exists?).and_return(true)
+
+    expect(user).not_to receive(:remote_avatar_url=)
+
+    AvatarUploader.fetch_avatar_from_url(user, 'http://example.com/images/profile.jpeg')
+  end
+
+  it 'uploads when avatar is blank' do
+    expect(user).to receive(:remote_avatar_url=)
+
+    AvatarUploader.fetch_avatar_from_url(user, 'http://example.com/images/profile.jpeg')
+  end
+end
