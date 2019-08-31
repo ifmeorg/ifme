f3f61af6ffe327c994d9bd7c7a3e083ef775b775
Julia Nguyen <tojulianguyen@gmail.com>
2019-08-31 11:34:59 -0700

    [#1541] Getting Storybook to work with updates

--- client/.storybook/addons.js
+++ client/.storybook/addons.js
@@ -1,6 +1,4 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
-
 import '@storybook/addon-actions/register';
-import '@storybook/addon-backgrounds/register';
 import '@storybook/addon-links/register';
 import '@storybook/addon-notes/register';
