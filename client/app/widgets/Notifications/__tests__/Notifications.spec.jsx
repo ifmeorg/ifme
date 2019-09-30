386d08082bf55a34336be69b527629cc05bb9145
briceyokoyama <briceyokoyama@gmail.com>
2019-09-30 02:47:33 -0700

    update notification test file to meet code quality

--- client/app/widgets/Notifications/__tests__/Notifications.spec.jsx
+++ client/app/widgets/Notifications/__tests__/Notifications.spec.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import axios from 'axios';
 import { mount } from 'enzyme';
-import { Notifications } from '../index'
+import { Notifications } from '../index';
 
 let axiosGetSpy;
 let axiosDeleteSpy;
@@ -11,24 +11,28 @@ let componentDidMountSpy;
 const data = {
   signed_in: 1,
   fetch_notifications: ['Notification 1'],
-}
+};
 
-const component = <Notifications
-                    element={<button>"Notifications"</button>}
-                    plural="Notifications"
-                    none="There are none"
-                    clear="Clear" 
-                  />;
+const component = (
+  <Notifications
+    element={<button type="button">Notifications</button>}
+    plural="Notifications"
+    none="There are none"
+    clear="Clear"
+  />
+);
 
 describe('Notifications', () => {
   beforeEach(() => {
     axiosGetSpy = jest.spyOn(axios, 'get').mockImplementation(() => Promise.resolve({
-      data
+      data,
     }));
     axiosDeleteSpy = jest.spyOn(axios, 'delete').mockImplementation(() => Promise.resolve({
-      data: {ok: true}
+      data: { ok: true },
     }));
-    componentDidMountSpy = jest.spyOn(Notifications.prototype, 'componentDidMount').mockImplementation(() => {})
+    componentDidMountSpy = jest
+      .spyOn(Notifications.prototype, 'componentDidMount')
+      .mockImplementation(() => {});
   });
 
   it('renders correctly', () => {
@@ -41,25 +45,32 @@ describe('Notifications', () => {
 
   it('gets notifications and clears them', async (done) => {
     const wrapper = mount(component);
+    componentDidMountSpy();
     const instance = wrapper.instance();
     instance.setState({ signedInKey: 1 });
-    instance.fetchNotifications()
+    instance
+      .fetchNotifications()
       .then(() => {
+        axiosGetSpy();
         wrapper.update();
-        expect(wrapper.state('notifications')).toBe('<div>Notification 1</div>');
+        expect(wrapper.state('notifications')).toBe(
+          '<div>Notification 1</div>',
+        );
       })
       .then(async () => {
         await wrapper.find('.buttonDarkS').simulate('click');
+        axiosDeleteSpy();
         wrapper.update();
         expect(wrapper.find('.modal').exists()).toEqual(false);
       })
       .then(async () => {
         data.fetch_notifications = [];
         await instance.fetchNotifications();
+        axiosGetSpy();
         wrapper.update();
         expect(wrapper.state('notifications')).toBe('');
         done();
       })
       .catch(done);
-  })
-})
+  });
+});
