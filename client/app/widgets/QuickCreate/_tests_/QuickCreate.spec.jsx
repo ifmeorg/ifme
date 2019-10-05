4a7c70d4534b139858041a18eaf354ad807e190f
Erica Pramer <epramer@godaddy.com>
2019-10-04 20:55:00 -0700

    test more functions within one `it` block

--- client/app/widgets/QuickCreate/_tests_/QuickCreate.spec.jsx
+++ client/app/widgets/QuickCreate/_tests_/QuickCreate.spec.jsx
@@ -3,47 +3,68 @@ import { mount } from 'enzyme';
 import { QuickCreate } from '../index';
 
 const checkboxes = [
-{ id: 'second', value: 'value', label: 'second_label' },
-{ id: 'first', value: 'value', label: 'first_label' }
+{ id: 'second', value: 'second_value', label: 'second_label' },
+{ id: 'first', value: 'first_value', label: 'first_label' }
 ];
 
+const formProps = {
+  inputs: [ {} ]
+};
+
 describe('QuickCreate', () => {
-  let tree;
+  let wrapper;
   beforeEach(() => {
-    tree = mount(<QuickCreate name='name' id='123' label='label' checkboxes={ checkboxes } />);
+    wrapper = mount(<QuickCreate 
+      name='name' 
+      id='123' 
+      label='label' 
+      checkboxes={ checkboxes } 
+      formProps={ formProps } />
+    );
   });
 
   afterEach(() => {
-    tree && tree.unmount();
+    jest.restoreAllMocks();
+    wrapper && wrapper.unmount();
   });
 
   it('sets correct initial values in state', () => {
-    const state = tree.state();
+    const state = wrapper.state();
     expect(state).toHaveProperty('checkboxes');
     expect(state).toHaveProperty('open', false);
     expect(state).toHaveProperty('accordionOpen', false);
   });
 
-  it('sorts checkboxes prop when setting state', () => {
-    const checkboxes = tree.state('checkboxes');
+  it('sorts checkboxes', () => {
+    const checkboxes = wrapper.state('checkboxes');
     expect(checkboxes[0].id).toBe('first');
     expect(checkboxes[1].id).toBe('second');
   });
   
   it('renders Input', () => {
-    expect(tree.find('Input')).toHaveLength(1);
+    expect(wrapper.find('Input')).toHaveLength(1);
   });
   
   it('renders Modal', () => {
-    expect(tree.find('Modal')).toHaveLength(1);
+    expect(wrapper.find('Modal')).toHaveLength(1);
   });
   // describe('#getCheckboxes', () => {
   //   it('return checkbox array from state');
   // });
-  // describe('#onChange', () => {
-  //   it('sets state if label does not exist');
-  //   it('does not change state if label exists');
-  // });
+  // 
+  describe('#onChange', () => {
+    it('opens modal and displays quick create form if label does not exist', function () {
+      expect(wrapper.find('DynamicForm')).toHaveLength(0);
+      expect(wrapper.find('Modal').prop('open')).toBeFalsy();
+      wrapper.find('Input').prop('onChange')({ checkboxes, label: 'new_label' });
+      wrapper.update();
+      expect(wrapper.find('Modal').prop('open')).toBeTruthy();
+      expect(wrapper.find('DynamicForm')).toHaveLength(1);
+    });
+
+    // it('does nothing if label exists');
+  });
+
   // describe('#labelExists', () => {
   //   it('return checkbox array from state');
   // });
@@ -51,22 +72,41 @@ describe('QuickCreate', () => {
   //   it('return checkbox array from state');
   // });
   describe('#onCreate', () => {
-    it('calls addToCheckboxes', () => {
-      const spy = jest.spyOn(tree.instance(), 'addToCheckboxes');
-      tree.instance().onCreate({ data: { success: true }});
-      expect(spy).toHaveBeenCalled();
-      spy.mockRestore();
+    beforeEach(() => {
+      // render DynamicForm
+      wrapper.find('Input').prop('onChange')({ checkboxes, label: 'new_label' });
+      wrapper.update();
     });
 
-    it('sets state and renders Modal/Input with correct key', () => {
-      expect(tree.state('accordionOpen')).toBeFalsy();
-      tree.instance().onCreate({ data: { success: true }});
-      tree.update();
-      const modalKey = tree.find('Modal').key();
-      const inputKey = tree.find('Input').key();
-      expect(tree.state('accordionOpen')).toBeTruthy();
-      expect(tree.state('modalKey')).toBe(modalKey);
-      expect(tree.state('tagKey')).toBe(inputKey);
+    it('adds checkboxes from data', () => {
+      const addCheckboxesSpy = jest.spyOn(wrapper.instance(), 'addToCheckboxes');
+      expect(wrapper.find('Input').prop('checkboxes')).toHaveLength(2);
+      wrapper.find('DynamicForm').prop('onCreate')({ data: { 
+        success: true,
+        name: 'third_label',
+        id: 'third_value',
+        slug: 'third'
+      }});
+      wrapper.update();
+      expect(wrapper.find('Input').prop('checkboxes')).toHaveLength(3);
+      expect(addCheckboxesSpy).toHaveBeenCalled();
+    });
+
+    it('opens accordion and renders Modal/Input with correct key', () => {
+      expect(wrapper.state('accordionOpen')).toBeFalsy();
+      wrapper.find('DynamicForm').prop('onCreate')({ data: { 
+        success: true,
+        name: 'third_label',
+        id: 'third_value',
+        slug: 'third'
+      }});
+      wrapper.update();
+      const input = wrapper.find('Input');
+      const modalKey = wrapper.find('Modal').key();
+      expect(wrapper.state('accordionOpen')).toBeTruthy();
+      expect(input.prop('accordionOpen')).toBeTruthy();
+      expect(modalKey).toBe(wrapper.state('modalKey'));
+      expect(input.key()).toBe(wrapper.state('tagKey'));
     });
   });
 });
