6e6a3ac6772a03c1a79882d21b783b2ad31a9950
juls <julianmacmang@gmail.com>
2019-10-20 20:56:40 +0800

    initial controller action for github omniauth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -14,6 +14,19 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
+  def github
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.valid?
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success',
+                              kind: t('omniauth.google'))
+
+      sign_in_and_redirect @user, event: :authentication
+    else
+      redirect_to new_user_session_path, notice: t('omniauth.access_denied')
+    end
+  end
+
   private
 
   def user
