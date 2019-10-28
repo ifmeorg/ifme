ac943feb9eb8b5fa44c4b3be30ca8a4adc4a1beb
SonuToor <savrooptoor92@gmail.com>
2019-10-28 16:35:04 -0400

    addressed problems from previous revision to tests

--- client/app/components/Input/__tests__/InputTag.spec.jsx
+++ client/app/components/Input/__tests__/InputTag.spec.jsx
@@ -18,7 +18,7 @@ describe('InputTag', () => {
     expect(wrapper.find('.tagMenu').exists()).toEqual(true);
     expect(wrapper.find(id).exists()).toEqual(false);
     wrapper
-      .find(li[role="option"])
+      .find(`li[role='option']`)
       .simulate('click');
     expect(wrapper.find(id).exists()).toEqual(true);
   });
@@ -40,7 +40,7 @@ describe('InputTag', () => {
     expect(wrapper.find('.tagMenu').exists()).toEqual(true);
     expect(wrapper.find(id).exists()).toEqual(false);
     wrapper
-      .find([role="option"])
+      .find(`[role='option']`)
       .at(0)
       .simulate('click');
     expect(wrapper.find(id).exists()).toEqual(true);
