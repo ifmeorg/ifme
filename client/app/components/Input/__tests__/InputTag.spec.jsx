eeea37405717308738bac6303821ebbbd42d1944
SonuToor <savrooptoor92@gmail.com>
2019-10-29 17:09:04 -0400

    made changes to the InputTag test.

--- client/app/components/Input/__tests__/InputTag.spec.jsx
+++ client/app/components/Input/__tests__/InputTag.spec.jsx
@@ -8,37 +8,40 @@ describe('InputTag', () => {
   it('type in value and select it', () => {
     const wrapper = mount(component);
     const id = `#${InputMocks.inputTagProps.checkboxes[1].id}`;
-    const value = { value: 'Two' };
+    // const value = { value: 'Two' };
     // Hack: Looks like react-autocomplete is looking for target, but we should use currentTarget
-    wrapper.find('.tagAutocomplete').prop('onChange')({
-      target: value,
-      currentTarget: value,
-    });
+    // wrapper.find('.tagAutocomplete').prop('onChange')({
+    //   target: value,
+    //   currentTarget: value,
+    // });
     wrapper.find('.tagAutocomplete').simulate('focus');
     expect(wrapper.find('.tagMenu').exists()).toEqual(true);
     expect(wrapper.find(id).exists()).toEqual(false);
-    wrapper.find('li[role=\'option\']').simulate('click');
+    wrapper
+      .find('.tagLabel')
+      .at(1)
+      .simulate('click');
     expect(wrapper.find(id).exists()).toEqual(true);
   });
 
   it('un-check existing value and retype and select it', () => {
     const wrapper = mount(component);
     const id = `input#${InputMocks.inputTagProps.checkboxes[0].id}`;
-    const value = { value: 'One' };
+    // const value = { value: 'One' };
     expect(wrapper.find(id).exists()).toEqual(true);
     wrapper.find(id).prop('onChange')({ currentTarget: { checked: false } });
     wrapper.update();
     expect(wrapper.find(id).exists()).toEqual(false);
     // Hack: Looks like react-autocomplete is looking for target, but we should use currentTarget
-    wrapper.find('.tagAutocomplete').prop('onChange')({
-      target: value,
-      currentTarget: value,
-    });
+    // wrapper.find('.tagAutocomplete').prop('onChange')({
+    //   target: value,
+    //   currentTarget: value,
+    // });
     wrapper.find('.tagAutocomplete').simulate('focus');
     expect(wrapper.find('.tagMenu').exists()).toEqual(true);
     expect(wrapper.find(id).exists()).toEqual(false);
     wrapper
-      .find('[role=\'option\']')
+      .find('.tagLabel')
       .at(0)
       .simulate('click');
     expect(wrapper.find(id).exists()).toEqual(true);
