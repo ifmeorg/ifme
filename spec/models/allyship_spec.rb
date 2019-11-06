f108f50ab2b4e231a419777990d2adb919175383
Marcela Kopko <marcelakopko@gmail.com>
2019-11-06 11:47:44 -0300

    more improves

--- spec/models/allyship_spec.rb
+++ spec/models/allyship_spec.rb
@@ -22,61 +22,50 @@ describe Allyship do
   end
 
   context 'when creating allies relationship' do
-    let(:new_user1) { create(:user1) }
-    let(:new_user2) { create(:user2) }
+    let(:user_id) { create(:user1).id }
+    let(:ally_id) { create(:user2).id }
 
-    context 'with accepted status' do
-      it 'creates a valid ally relationship' do
-        create(:allyships_accepted, user_id: new_user1.id, ally_id: new_user2.id)
+    context 'with valid ally relationship' do
+      before { create(allyship_status, user_id: user_id, ally_id: ally_id) }
 
-        expect(Allyship.count).to eq 2
+      context 'with accepted status' do
+        let(:allyship_status) { :allyships_accepted }
+        it { expect(Allyship.count).to eq 2 }
       end
-    end
 
-    context 'with pending_from_user_id1 status' do
-      it 'creates a valid ally relationship' do
-        create(:allyships_pending_from_user_id1, user_id: new_user1.id, ally_id: new_user2.id)
-
-        expect(Allyship.count).to eq 2
+      context 'with pending_from_user_id1 status' do
+        let(:allyship_status) { :allyships_pending_from_user_id1 }
+        it { expect(Allyship.count).to eq 2 }
       end
-    end
 
-    context 'with pending_from_user_id2 status' do
-      it 'creates a valid ally relationship' do
-        create(:allyships_pending_from_user_id2, user_id: new_user1.id, ally_id: new_user2.id)
-
-        expect(Allyship.count).to eq 2
+      context 'with pending_from_user_id2 status' do
+        let(:allyship_status) { :allyships_pending_from_user_id2 }
+        it { expect(Allyship.count).to eq 2 }
       end
     end
 
     context 'with invalid ally relationship' do
-      context 'when users are identical' do
-        it 'creates an invalid ally relationship' do
-          new_allies = build(:allyships_accepted, user_id: new_user1.id, ally_id: new_user1.id)
+      let(:new_allies) { build(:allyships_accepted, user_id: user_id, ally_id: ally_id) }
 
-          expect(new_allies).to have(1).error_on(:user_id)
-        end
+      context 'when users are identical' do
+        let(:ally_id) { user_id }
+        it { expect(new_allies).to have(1).error_on(:user_id) }
       end
 
       context 'when user1 is nil' do
-        it 'creates an invalid ally relationship' do
-          new_allies = build(:allyships_accepted, user_id: nil, ally_id: new_user2.id)
-
-          expect(new_allies).to have(1).error_on(:user_id)
-        end
+        let(:user_id) { nil }
+        it { expect(new_allies).to have(1).error_on(:user_id) }
       end
 
       context 'when user2 is nil' do
-        it 'creates an invalid ally relationship' do
-          new_allies = build(:allyships_accepted, user_id: new_user1.id, ally_id: nil)
-
-          expect(new_allies).to have(1).error_on(:ally_id)
-        end
+        let(:ally_id) { nil }
+        it { expect(new_allies).to have(1).error_on(:ally_id) }
       end
     end
   end
 
   context 'when destroying' do
+    let(:subject) { allyship.destroy }
     let!(:user) { create(:user1) }
     let!(:ally) { create(:user2) }
     let(:allyship) { Allyship.where(user_id: user.id, ally_id: ally.id)[0] }
@@ -86,49 +75,18 @@ describe Allyship do
       Allyship.create(user_id: ally.id, ally_id: user.id, status: 'accepted')
     end
 
-    it 'deletes the allyship' do
-      expect { allyship.destroy }.to change { Allyship.count }
-    end
+    it { expect { subject }.to change { Allyship.count }.from(3).to(0) }
 
     context 'with viewer' do
       before do
-        Moment.create(category: [1],
-                      name: 'Presentation for ENGL 101',
-                      mood: [1, 2],
-                      why: 'I am presenting in front of' \
-                      'my classmates and I am worried I' \
-                      ' will make a fool out of myself',
-                      comment: true,
-                      user_id: user.id,
-                      viewers: [ally.id])
-
-        Moment.create(category: [1],
-                      name: 'Presentation for ENGL 101',
-                      mood: [1, 2],
-                      why: 'I am presenting in front of' \
-                      'my classmates and I am worried I' \
-                      ' will make a fool out of myself',
-                      comment: true,
-                      user_id: ally.id,
-                      viewers: [user.id])
-
-        Strategy.create(category: [1],
-                        name: 'I am a name!',
-                        description: 'I am a description!',
-                        comment: true,
-                        user_id: user.id,
-                        viewers: [ally.id])
-
-        Strategy.create(category: [1],
-                        name: 'I am a name!',
-                        description: 'I am a description!',
-                        comment: true,
-                        user_id: ally.id,
-                        viewers: [user.id])
+        create_moment(user, ally)
+        create_moment(ally, user)
+        create_strategy(user, ally)
+        create_strategy(ally, user)
       end
 
       it 'deletes viewer' do
-        expect { allyship.destroy }
+        expect { subject }
           .to change { user.moments.first.viewers.count }.from(1).to(0)
           .and change { user.strategies.first.viewers.count }.from(1).to(0)
       end
@@ -136,43 +94,44 @@ describe Allyship do
 
     context 'with allyship request notifications' do
       before do
-        Notification.import([
-                              build(
-                                :notification,
-                                user: user,
-                                uniqueid: "new_ally_request_#{ally.id}"
-                              ),
-                              build(
-                                :notification,
-                                user: user,
-                                uniqueid: "accepted_ally_request_#{ally.id}"
-                              ),
-                              build(
-                                :notification,
-                                user: ally,
-                                uniqueid: "new_ally_request_#{user.id}"
-                              ),
-                              build(
-                                :notification,
-                                user: ally,
-                                uniqueid: "accepted_ally_request_#{user.id}"
-                              ),
-                              build(
-                                :notification,
-                                user: user,
-                                uniqueid: "new_ally_request_#{ally.id + 10}"
-                              ),
-                              build(
-                                :notification,
-                                user: ally,
-                                uniqueid: "new_ally_request_#{user.id + 10}"
-                              )
-                            ])
+        Notification.import(
+          [
+            build(:notification, user: user, uniqueid: "new_ally_request_#{ally.id}"),
+            build(:notification, user: user, uniqueid: "accepted_ally_request_#{ally.id}"),
+            build(:notification, user: ally, uniqueid: "new_ally_request_#{user.id}"),
+            build(:notification, user: ally, uniqueid: "accepted_ally_request_#{user.id}"),
+            build(:notification, user: user, uniqueid: "new_ally_request_#{ally.id + 10}"),
+            build(:notification, user: ally, uniqueid: "new_ally_request_#{user.id + 10}")
+          ]
+        )
       end
 
-      it 'deletes the ally notifications' do
-        expect { allyship.destroy }.to change { Notification.count }.from(6).to(2)
-      end
+      it { expect { subject }.to change { Notification.count }.from(6).to(2) }
+    end
+
+    def create_moment(user, viewer)
+      Moment.create(
+        category: [1],
+        name: 'Presentation for ENGL 101',
+        mood: [1, 2],
+        why: 'I am presenting in front of' \
+        'my classmates and I am worried I' \
+        ' will make a fool out of myself',
+        comment: true,
+        user_id: user.id,
+        viewers: [viewer.id]
+      )
+    end
+
+    def create_strategy(user, viewer)
+      Strategy.create(
+        category: [1],
+        name: 'I am a name!',
+        description: 'I am a description!',
+        comment: true,
+        user_id: user.id,
+        viewers: [viewer.id]
+      )
     end
   end
 end
