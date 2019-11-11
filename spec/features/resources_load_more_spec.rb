0b878b4bfd42c155920e3aa96e5affcf2bd26bed
Benjamin Wacha <bmwachajr>
2019-11-11 13:39:34 +0300

    fix 2 space indentation

--- spec/features/resources_load_more_spec.rb
+++ spec/features/resources_load_more_spec.rb
@@ -1,35 +1,34 @@
 describe "Load more resources", js: true do
-    let(:user) { create :user }
-    feature 'load more feature' do
-        context 'clicking the load more button' do
-            before :each do
-                visit('/resources')
-            end
+  let(:user) { create :user }
+  feature 'load more feature' do
+    context 'clicking the load more button' do
+      before :each do
+        visit('/resources')
+      end
 
-            it "loads 3 resources" do
-                expect(page).to have_text '3 of 120 resources'
-                expect(page).to have_selector('.Resources__gridThreeItem___1Lm4E', count: 3)
-                expect(page).to have_button('Load more')
-            end
+      it "loads 3 resources" do
+        expect(page).to have_text '3 of 120 resources'
+        expect(page).to have_selector('.Resources__gridThreeItem___1Lm4E', count: 3)
+        expect(page).to have_button('Load more')
+      end
 
-            it 'loads more resources' do
-                2.times do
-                    click_button('Load more')
-                end
+      it 'loads more resources' do
+        2.times do
+          click_button('Load more')
+        end
 
-                expect(page).to have_text '9 of 120 resources'
-                expect(page).to have_selector('.Resources__gridThreeItem___1Lm4E', count: 9)
+        expect(page).to have_text '9 of 120 resources'
+        expect(page).to have_selector('.Resources__gridThreeItem___1Lm4E', count: 9)
+      end
 
-            end
-
-            it 'loads all resources and the load more button vanishes' do
-                39.times do
-                    click_button('Load more')
-                end
-                expect(page).to have_text '120 of 120 resources'
-                expect(page).to have_selector('.Resources__gridThreeItem___1Lm4E', count: 120)
-                expect(page).not_to have_button('Load more')
-            end
+      it 'loads all resources and the load more button vanishes' do
+        39.times do
+          click_button('Load more')
+        end
+        expect(page).to have_text '120 of 120 resources'
+        expect(page).to have_selector('.Resources__gridThreeItem___1Lm4E', count: 120)
+        expect(page).not_to have_button('Load more')
+      end
         end
     end
 end
