9833d66549e74a1f643c4b5338b2d749989d4d90
Benjamin Wacha <bmwachajr>
2019-11-12 14:32:16 +0300

    update spec

--- spec/features/resources_load_more_spec.rb
+++ spec/features/resources_load_more_spec.rb
@@ -8,7 +8,7 @@ describe "Load more resources", js: true do
 
       it "loads 3 resources" do
         expect(page).to have_text '3 of 120 resources'
-        expect(page).to have_selector('.Resources__gridThreeItem___1Lm4E', count: 3)
+        expect(page).to have_selector('.Resource', count: 3)
         expect(page).to have_button('Load more')
       end
 
@@ -18,7 +18,7 @@ describe "Load more resources", js: true do
         end
 
         expect(page).to have_text '9 of 120 resources'
-        expect(page).to have_selector('.Resources__gridThreeItem___1Lm4E', count: 9)
+        expect(page).to have_selector('.Resource', count: 9)
       end
 
       it 'loads all resources and the load more button vanishes' do
@@ -26,7 +26,7 @@ describe "Load more resources", js: true do
           click_button('Load more')
         end
         expect(page).to have_text '120 of 120 resources'
-        expect(page).to have_selector('.Resources__gridThreeItem___1Lm4E', count: 120)
+        expect(page).to have_selector('.Resource', count: 120)
         expect(page).not_to have_button('Load more')
       end
         end
