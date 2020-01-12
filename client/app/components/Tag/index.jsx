f77cf2b9b1778d2d7061b2d6efefc1b5f98a0130
Julia Nguyen <tojulianguyen@gmail.com>
2020-01-11 17:55:51 -0800

    Use cursor: pointer on Tag component only if clickable

--- client/app/components/Tag/index.jsx
+++ client/app/components/Tag/index.jsx
@@ -10,20 +10,25 @@ type Props = {
   onClick?: Function,
 };
 
+const labelClassNames = ({
+  dark, normal, secondary, onClick,
+}) => `tag ${css.tag} ${dark ? css.dark : ''} ${normal ? css.normal : ''} ${
+  secondary ? css.secondary : ''
+} ${onClick ? css.clickable : ''}`;
+
 export const Tag = (props: Props) => {
   const {
     dark, normal, label, secondary, onClick,
   } = props;
-  const labelClassNames = `tag ${css.tag} ${dark ? css.dark : ''} ${
-    normal ? css.normal : ''
-  } ${secondary ? css.secondary : ''}`;
   return (
     <div
       onClick={onClick ? () => onClick(label) : null}
       role="button"
       tabIndex={0}
       onKeyDown={onClick ? () => onClick(label) : null}
-      className={labelClassNames}
+      className={labelClassNames({
+        dark, normal, secondary, onClick,
+      })}
     >
       {label}
     </div>
