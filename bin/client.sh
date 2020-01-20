be1fe7b580b28eed8b1cb31612f038c0c2bea958
Julia Nguyen <tojulianguyen@gmail.com>
2020-01-20 13:36:19 -0800

    Consolidate all resources in a single resources.json

--- bin/client.sh
+++ bin/client.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-# client.sh
-# This script is run from Procfile.dev (which is run from bin/start_app
 
+# This script is run from Procfile.dev (which is run from bin/start_app
+#
 # Make sure we're in the source directory by checking for the existence
 # of ./public (note that we're checking the relative path, not the absolute).
 #
 # bin/start_app should change to the source directory, so
 # this should never happen unless something very unusual is wrong
+
 if [ ! -d public ]; then
 {
   echo "Error in setup"
