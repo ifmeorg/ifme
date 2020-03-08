0b9ab1972f7eec5c38af16c1786babe3160cfd18
Aleksander Pentchev <alek.pentchev@gmail.com>
2020-03-08 14:09:40 +0100

    issue_1656 - forms - check eslint disable impact on circleci

--- client/app/components/Form/index.jsx
+++ client/app/components/Form/index.jsx
@@ -111,8 +111,6 @@ export function Form(props: Props) {
   });
 
   const renderForm = (): Node => {
-    // action prop destructuring fails due to unresolved eslint-plugin-react issue
-    // eslint-disable-next-line react/prop-types
     const { action } = props;
     const { form } = css;
 
