2d62617249adddc89c308cb2b4f8b6960c4a840c
Julia Nguyen <tojulianguyen@gmail.com>
2020-03-21 19:57:06 -0700

    Test for aria-label in InputPassword

--- client/app/components/Input/__tests__/InputPassword.spec.jsx
+++ client/app/components/Input/__tests__/InputPassword.spec.jsx
@@ -8,9 +8,11 @@ describe('InputPassword', () => {
   it('toggles show password button correctly', () => {
     const wrapper = mount(component);
     expect(wrapper.find('input').props().type).toEqual('password');
-    expect(wrapper.find('i').props().className).toEqual('fa fa-eye-slash');
+    expect(wrapper.find('i').props().className).toEqual('fa fa-eye');
+    expect(wrapper.find('button').props()['aria-label']).toEqual('Show password');
     wrapper.find('button').simulate('click');
     expect(wrapper.find('input').props().type).toEqual('text');
-    expect(wrapper.find('i').props().className).toEqual('fa fa-eye');
+    expect(wrapper.find('i').props().className).toEqual('fa fa-eye-slash');
+    expect(wrapper.find('button').props()['aria-label']).toEqual('Hide password');
   });
 });
