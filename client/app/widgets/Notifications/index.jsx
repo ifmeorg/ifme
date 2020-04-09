6b7a7f39f56261e7bc72f0dc9f8e13deb8d90237
manasa2850 <manasakashyap28@gmail.com>
2020-04-09 11:23:44 +0530

    circleci

--- client/app/widgets/Notifications/index.jsx
+++ client/app/widgets/Notifications/index.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import renderHTML from 'react-render-html';
 import { Modal } from '../../components/Modal';
