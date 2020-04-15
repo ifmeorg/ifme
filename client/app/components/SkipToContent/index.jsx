995fdcc23d185cdaca0107016dd2f8725e508323
discombobulateme <pcultural@gmail.com>
2020-04-15 16:46:32 +0200

    created branch and added component

---
+++ client/app/components/SkipToContent/index.jsx
@@ -0,0 +1,9 @@
+// @flow
+import React from 'react';
+import css from './SkipToContent.scss';
+
+export const SkipToContent = () => {
+  return (
+    <a className={css.test} href="#mainContent">Skip to main content</a>
+  );
+};
