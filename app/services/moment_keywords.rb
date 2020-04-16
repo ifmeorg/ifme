9b27d5d73177523d4a9c2c67a6ee6d0e45442c28
stephanie-warmenhoven <steph_war@yahoo.ca>
2020-04-16 12:55:00 -0700

    [#1713] Refactored code to make it dry

--- app/services/moment_keywords.rb
+++ app/services/moment_keywords.rb
@@ -10,41 +10,39 @@ class MomentKeywords
     extract_keywords(@moment.categories)
     extract_keywords(@moment.moods)
     extract_keywords(@moment.strategies)
-    extract_moment_name
-    extract_moment_why
-    extract_moment_fix
+    @moment_keywords.push(extract_name, extract_why, extract_fix)
     remove_special_chars
     downcase_keywords
   end
 
   private
 
-  def strip_tags(str)
+  def strip_html(str)
     ActionController::Base.helpers.strip_tags(str)
   end
 
   def extract_keywords(array)
     array.each do |item|
       @moment_keywords.push(item['name'].split,
-                            strip_tags(item['description']).split)
+                            strip_html(item['description']).split)
     end
   end
 
-  def extract_moment_name
-    @moment_keywords.push(@moment.name.split)
+  def extract_name
+    @moment.name.split
   end
 
-  def extract_moment_why
-    @moment_keywords.push(strip_tags(@moment.why).split)
+  def extract_why
+    strip_html(@moment.why).split
   end
 
-  def extract_moment_fix
-    @moment_keywords.push(strip_tags(@moment.fix).split)
+  def extract_fix
+    strip_html(@moment.fix).split
   end
 
   def remove_special_chars
     @moment_keywords = @moment_keywords.flatten.each do |keyword|
-      keyword.tr!('-', ' ')
+      keyword.gsub!('-' || '_', ' ')
       keyword.gsub!(/[^\p{Alpha} -]/, '')
     end
   end
