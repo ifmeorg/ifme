f50e0da09a4d3840bf0ba863ca96664c6b0956fb
stephanie-warmenhoven <steph_war@yahoo.ca>
2020-04-15 23:05:57 -0700

    [#1713] Test if ResourceRecommendation match Moment keywords

---
+++ spec/services/resource_recommendation_spec.rb
@@ -0,0 +1,16 @@
+describe ResourceRecommendation do
+    let(:moment) {FactoryBot.build(:moment,
+                                       name: 'SELF-INJURY@',
+                                       why: 'text',
+                                       fix: 'text' )}
+  
+    subject(:resources) { ResourceRecommendation.new(moment).resources}
+  
+    describe 'A test for resources method from ResourceRecommendation class' do
+      it 'returns recommended resources based on moment keywords' do
+        available_resource = [{'tags'=> ["self_injury"]}]
+        allow(JSON).to receive(:parse) { available_resource }
+        expect(resources).to eq(available_resource)
+      end
+    end
+  end
