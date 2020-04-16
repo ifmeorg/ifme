6b5c4e866066662f678ba45bb1d23d35bb498445
stephanie-warmenhoven <steph_war@yahoo.ca>
2020-04-15 22:40:24 -0700

    [#1713] Private method to extract all resources

--- spec/services/resource_recommendation.rb
+++ spec/services/resource_recommendation.rb
@@ -3,4 +3,10 @@ class ResourceRecommendation
         @moment = moment
         @moment_keywords = []
     end
+
+    private
+
+    def all_resources
+        JSON.parse(File.read(Rails.root.join('doc', 'pages', 'resources.json')))
+    end
 end
