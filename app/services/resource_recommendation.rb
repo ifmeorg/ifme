621639f1910d2734e80c5ea69143ee232f3b8633
stephanie-warmenhoven <steph_war@yahoo.ca>
2020-04-15 22:55:27 -0700

    [#1713] Moved class file into correct directory

---
+++ app/services/resource_recommendation.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class ResourceRecommendation
+    def initialize(moment)
+        @moment = moment
+        @moment_keywords = []
+    end
+
+    def resources
+        @moment_keywords = MomentKeywords.new(@moment).extract_moment_keywords
+        all_resources.select do |resource|
+          tags = resource['tags'].flat_map do |tag|
+            tag.tr('_', ' ')
+          end
+          (tags & @moment_keywords).any?
+        end
+    end
+
+    private
+
+    def all_resources
+        JSON.parse(File.read(Rails.root.join('doc', 'pages', 'resources.json')))
+    end
+end
