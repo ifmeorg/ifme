c7c336fb043d5b3d1e3dbf0363307c9144590f22
stephanie-warmenhoven <steph_war@yahoo.ca>
2020-04-16 12:25:55 -0700

    [#1713] Rubocop code clean-up

--- app/services/moment_keywords.rb
+++ app/services/moment_keywords.rb
@@ -6,48 +6,48 @@ class MomentKeywords
     @moment_keywords = []
   end
 
-def extract_moment_keywords
-  extract_keywords(@moment.categories)
-  extract_keywords(@moment.moods)
-  extract_keywords(@moment.strategies)
-  extract_moment_name
-  extract_moment_why
-  extract_moment_fix
-  remove_special_chars
-  downcase_keywords
-end
-
-private
-
-def strip_tags(str)
-  ActionController::Base.helpers.strip_tags(str)
-end
-
-def extract_keywords(array)
-  array.each do |item|
-    @moment_keywords.push(item['name'].split,
-                          strip_tags(item['description']).split)
+  def extract_moment_keywords
+    extract_keywords(@moment.categories)
+    extract_keywords(@moment.moods)
+    extract_keywords(@moment.strategies)
+    extract_moment_name
+    extract_moment_why
+    extract_moment_fix
+    remove_special_chars
+    downcase_keywords
   end
-end
 
-def extract_moment_name
-  @moment_keywords.push(@moment.name.split)
-end
+  private
 
-def extract_moment_why
-  @moment_keywords.push(strip_tags(@moment.why).split)
-end
-
-def extract_moment_fix
-  @moment_keywords.push(strip_tags(@moment.fix).split)
-end
-
-def remove_special_chars
-  @moment_keywords = @moment_keywords.flatten.each do |keyword|
-    keyword.tr!('-', ' ')
-    keyword.gsub!(/[^\p{Alpha} -]/, '')
+  def strip_tags(str)
+    ActionController::Base.helpers.strip_tags(str)
   end
-end
+
+  def extract_keywords(array)
+    array.each do |item|
+      @moment_keywords.push(item['name'].split,
+                            strip_tags(item['description']).split)
+    end
+  end
+
+  def extract_moment_name
+    @moment_keywords.push(@moment.name.split)
+  end
+
+  def extract_moment_why
+    @moment_keywords.push(strip_tags(@moment.why).split)
+  end
+
+  def extract_moment_fix
+    @moment_keywords.push(strip_tags(@moment.fix).split)
+  end
+
+  def remove_special_chars
+    @moment_keywords = @moment_keywords.flatten.each do |keyword|
+      keyword.tr!('-', ' ')
+      keyword.gsub!(/[^\p{Alpha} -]/, '')
+    end
+  end
 
   def downcase_keywords
     @moment_keywords = @moment_keywords.map(&:downcase)
