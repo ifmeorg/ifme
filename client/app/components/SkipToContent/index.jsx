36629447bdeb7eaa7a58bbd16bdef8565180caca
discombobulateme <pcultural@gmail.com>
2020-04-16 17:23:56 +0200

    added a variable instead of a fixed id for href

--- client/app/components/SkipToContent/index.jsx
+++ client/app/components/SkipToContent/index.jsx
@@ -2,8 +2,14 @@
 import React from 'react';
 import css from './SkipToContent.scss';
 
-export const SkipToContent = () => {
-  return (
-    <a className={css.test} href="#mainContent">Skip to main content</a>
-  );
-};
+function SkipToContent(props) {
+  return <a className={css.test} href={"#", props.id}>Skip to main content</a>;
+}
+
+export default SkipToContent;
+
+// export const SkipToContent = () => {
+//   return (
+//     <a className={css.test} href="#mainContent">Skip to main content</a>
+//   );
+// };
