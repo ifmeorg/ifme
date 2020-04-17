2da4dab9e013c4d2b04da5a7bbd4dd6a6d81a54f
discombobulateme <pcultural@gmail.com>
2020-04-17 19:24:22 +0200

    fixed props, deletes notes

--- client/app/components/SkipToContent/index.jsx
+++ client/app/components/SkipToContent/index.jsx
@@ -2,14 +2,14 @@
 import React from 'react';
 import css from './SkipToContent.scss';
 
-function SkipToContent(props) {
-  return <a className={css.test} href={"#", props.id}>Skip to main content</a>;
+export type Props = {
+  id?: string,
+};
+
+function SkipToContent(props: Props) {
+
+  return <a className={css.skipToContent} href={`#${props.id}`}>Skip to main content</a>;
 }
 
 export default SkipToContent;
 
-// export const SkipToContent = () => {
-//   return (
-//     <a className={css.test} href="#mainContent">Skip to main content</a>
-//   );
-// };
