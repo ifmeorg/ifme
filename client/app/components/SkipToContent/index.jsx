1365cc1eed14a535fe150ec673c6b0f582332b7d
discombobulateme <pcultural@gmail.com>
2020-04-27 19:36:50 +0200

    fixed issues

--- client/app/components/SkipToContent/index.jsx
+++ client/app/components/SkipToContent/index.jsx
@@ -4,14 +4,17 @@ import css from './SkipToContent.scss';
 import { I18n } from '../../libs/i18n';
 
 export type Props = {
-  id?: string,
+  id: string,
 };
 
 function SkipToContent(props: Props) {
-  <div aria-label={I18n.t('navigation.skip_to_main_content')} /> 
+  const { id } = props;
 
-  return <a className={css.skipToContent} href={`#${props.id}`}>Skip to main content</a>;
+  return (
+    <a className={css.skipToContent} href={`#${id}`}>
+      {I18n.t('navigation.skip_to_main_content')}
+    </a>
+  );
 }
 
 export default SkipToContent;
-
