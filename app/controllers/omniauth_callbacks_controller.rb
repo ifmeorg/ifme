4748eefc00465c03f554ba3075422d77a2cc3742
Manasa2850 <43354059+Manasa2850@users.noreply.github.com>
2020-05-07 14:40:50 +0530

    make find_auth_translation() private

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -11,6 +11,14 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   private
+  
+  def find_auth_translation(provider)
+    if provider == 'google'
+      return t('omniauth.google')
+    else if provider == 'facebook'
+      return t('navigation.facebook')
+    end
+  end
 
   def user
     @user ||= User.find_for_oauth(request.env['omniauth.auth'])
