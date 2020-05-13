61c4502557cfc52bc8abd2d0de2bbd2a39e7fc05
Gayathri Venkatesh <gayaa2010@gmail.com>
2020-05-13 10:25:23 +0530

    Update 20200224201131_create_moment_categories_join_table.rb

--- db/migrate/20200224201131_create_moment_categories_join_table.rb
+++ db/migrate/20200224201131_create_moment_categories_join_table.rb
@@ -1,12 +1,12 @@
-class CreateMomentStrategiesJoinTable < ActiveRecord::Migration[5.2]
+class CreateMomentCategoriesJoinTable < ActiveRecord::Migration[5.2]
   def change
-    create_table :moments_strategies do |t|
+    create_table :moments_categories do |t|
       t.integer :moment_id
-      t.integer :strategy_id
+      t.integer :category_id
     end
 
-    add_index :moments_strategies, [:moment_id, :strategy_id], unique: true
-    add_foreign_key :moments_strategies, :moments
-    add_foreign_key :moments_strategies, :strategies
+    add_index :moments_categories, [:moment_id, :category_id], unique: true
+    add_foreign_key :moments_categories, :moments
+    add_foreign_key :moments_categories, :categories
   end
 end
