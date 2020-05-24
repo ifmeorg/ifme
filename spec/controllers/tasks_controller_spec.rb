6d400008eef11330a82bd97464b5c92852a9bc01
Gayathri Venkatesh <gayaa2010@gmail.com>
2020-05-24 15:53:52 +0530

    Remove search tests

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -9,21 +9,6 @@ describe TasksController do
 
     context 'when the user is logged in' do
       include_context :logged_in_user
-
-      context 'when search params are provided' do
-        before { get :index, params: { search: 'test' } }
-
-        it 'renders the index template' do
-          expect(response).to render_template('index')
-        end
-      end
-
-      context 'when no search params are provided' do
-        it 'renders the index template' do
-          get :index
-          expect(response).to render_template('index')
-        end
-      end
     end
 
     context 'when the user is not logged in' do
