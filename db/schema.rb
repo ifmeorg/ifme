dfa599df54741656f23d8efbd6ab5d1e9e205ad4
Manasa2850 <43354059+Manasa2850@users.noreply.github.com>
2020-05-25 12:38:45 +0530

    Remove todolists

--- db/schema.rb
+++ db/schema.rb
@@ -279,13 +279,6 @@ ActiveRecord::Schema.define(version: 2020_05_09_110917) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "todolists", force: :cascade do |t|
-    t.boolean "finished"
-    t.date "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
