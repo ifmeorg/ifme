8feea12cccbb45f8442a1cdee52c83d0fdfcaef8
guillermo saavedra <gs8rj@virginia.edu>
2020-05-30 22:25:51 -0400

    Adds InputRadio component to new/edit medication form

---
+++ client/app/components/Input/__tests__/InputRadioGroup.spec.jsx
@@ -0,0 +1,30 @@
+// @flow
+import React from 'react';
+import { shallow} from 'enzyme';
+import { act } from 'react-dom/test-utils';
+import { InputRadioGroup } from '../InputRadioGroup';
+import { InputMocks } from '../../../mocks/InputMocks';
+
+const {
+  id, name, value, options,
+} = InputMocks.inputRadioProps;
+
+describe('InputRadioGroup', () => {
+  beforeEach(() => {
+    jest.spyOn(window, 'alert');
+  });
+
+  it('sets default radio button to first option', () => {
+    const wrapper = shallow(
+      <InputRadioGroup
+        name={name}
+        id={id}
+        value={value}
+        options={options}
+      />,
+    );
+    expect(
+      wrapper.find('input[id="some-option-one-id"][type="radio"]').props().defaultChecked,
+    ).toBe(true);
+  });
+});
