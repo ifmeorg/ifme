51081046fb3ba1b3916ad5ee0e0660281d0849c7
Julia Nguyen <tojulianguyen@gmail.com>
2020-06-05 16:06:40 -0700

    Setup contacts instance variable for CarePlanController

--- app/controllers/care_plan_controller.rb
+++ app/controllers/care_plan_controller.rb
@@ -2,5 +2,6 @@
 class CarePlanController < ApplicationController
   def index
     @bookmarked_strategies = current_user.strategies.where(bookmarked: true)
+    @contacts = current_user.care_plan_contacts.order(name: :desc)
   end
 end
