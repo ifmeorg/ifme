9aab43d13ddd5638c5017821e4239cea0ae3dee4
Aleksander Pentchev <alek.pentchev@gmail.com>
2020-06-13 18:14:16 +0200

    issue_1758 - StoryContainer spec file

--- client/app/components/BaseContainer/__tests__/StoryContainer.spec.jsx
+++ client/app/components/BaseContainer/__tests__/StoryContainer.spec.jsx
@@ -1,14 +1,12 @@
 // @flow
-import { render } from 'enzyme';
 import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import { StoryContainer } from '../StoryContainer';
 
 describe('StoryContainer', () => {
   it('renders correctly', () => {
-    let wrapper = null;
-    expect(() => {
-      wrapper = render(<StoryContainer data={[]} />);
-    }).not.toThrow();
-    expect(wrapper).not.toBeNull();
+    const { container } = render(<StoryContainer data={[]} />);
+    expect(container.firstChild).not.toBeNull();
   });
 });
