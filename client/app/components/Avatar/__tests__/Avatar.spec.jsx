8e9f63fda5063b928ca0bb57849049c23bfbe18f
Aleksander Pentchev <alek.pentchev@gmail.com>
2020-06-13 18:10:42 +0200

    issue_1758 - Avatar - remove unused import

--- client/app/components/Avatar/__tests__/Avatar.spec.jsx
+++ client/app/components/Avatar/__tests__/Avatar.spec.jsx
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { Avatar } from '../index';
 
