2b01d662988404ab3bcd5f71505e185b57102f54
discombobulateme <pcultural@gmail.com>
2020-06-22 12:25:29 +0200

    add sophias solution

--- client/app/components/SkipToContent/index.jsx
+++ client/app/components/SkipToContent/index.jsx
@@ -1,29 +1,17 @@
 // @flow
 import React from 'react';
+
 import css from './SkipToContent.scss';
 import { I18n } from '../../libs/i18n';
-import { Link } from 'react-router-dom';
 
 export type Props = {
   id: string,
 };
 
-function SkipToContent(props: Props) {
-  const { id } = props;
-  const [scroll, setScroll] = useState(false);
-  const aToScroll = document.getElementById(id).useCallback(() => {
-    setIsFocused(true);
-  }, []);
-
-  return (
-    <a
-      className={css.skipToContent}
-      onClick={aToScroll}
-      href={`#${id}`}
-      >
-      {I18n.t('navigation.skip_to_main_content')}
-    </a>
-  );
-}
+const SkipToContent = ({ id }: Props) => (
+  <a className={css.skipToContent} href={`#${id}`}>
+    {I18n.t('navigation.skip_to_main_content')}
+  </a>
+);
 
 export default SkipToContent;
