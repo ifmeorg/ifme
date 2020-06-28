39a4c18895dcf83c7dc84478ca742de355626a21
Aleksander Pentchev <alek.pentchev@gmail.com>
2020-06-28 20:20:19 +0200

    issue_1758 - rewritten ChartControl; increased Chart accessibility

--- client/app/components/Chart/__tests__/ChartControl.spec.jsx
+++ client/app/components/Chart/__tests__/ChartControl.spec.jsx
@@ -1,34 +1,31 @@
-/**
- * @flow
- * Enzyme samples: https://gist.github.com/richardscarrott/d89b37aff55ccc504193335198e676d1
- */
-import { shallow } from 'enzyme';
+// @flow
+
+import { render } from '@testing-library/react';
 import React from 'react';
 import { ChartControl } from '../ChartControl';
 
 describe('ChartControl', () => {
   it('renders', () => {
-    let wrapper = null;
-    expect(() => {
-      // shallow render only lists the first level of components
-      wrapper = shallow(
-        <ChartControl
-          types={['Moments']}
-          initialParams={{
-            type: 'Moments',
-            data: {
-              Moments: {
-                '2013-02-10 00:00:00 -0800': 11,
-                '2013-02-11 00:00:00 -0800': 6,
-              },
+    const { container, getByRole } = render(
+      <ChartControl
+        types={['Moments']}
+        initialParams={{
+          type: 'Moments',
+          data: {
+            Moments: {
+              '2013-02-10 00:00:00 -0800': 11,
+              '2013-02-11 00:00:00 -0800': 6,
             },
-          }}
-        />,
-      );
-    }).not.toThrow();
+          },
+        }}
+      />,
+    );
 
-    expect(wrapper).not.toBeNull();
-    expect(wrapper.find('ChartControlButton').length).toEqual(1);
-    expect(wrapper.find('Chart').length).toEqual(1);
+    const chartControlBtn = getByRole('button');
+    const chart = getByRole('img');
+
+    expect(container.firstChild).not.toBeNull();
+    expect(chartControlBtn).toBeInTheDocument();
+    expect(chart).toBeInTheDocument();
   });
 });
--- client/app/components/Chart/index.jsx
+++ client/app/components/Chart/index.jsx
@@ -16,9 +16,13 @@ type chartShape = {
 const colorSchemes = ['#6D0839', '#66118', '#7F503F', '#775577', '#CCAADD'];
 
 export function Chart({ chartType, ...props }: chartShape) {
-  return chartType === 'Line' ? (
-    <LineChart {...props} colors={colorSchemes} />
-  ) : (
-    <AreaChart {...props} colors={colorSchemes} />
+  return (
+    <div role="img" aria-label="Canvas chart">
+      {chartType === 'Line' ? (
+        <LineChart {...props} colors={colorSchemes} />
+      ) : (
+        <AreaChart {...props} colors={colorSchemes} />
+      )}
+    </div>
   );
 }
