86c8b562c2271fd1624615f40f8973d44acbadad
Cory Thompson <public@coryjthompson.com>
2020-07-22 09:01:52 +1000

    Add SameSite cookie attribute

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_ifme_session'
+Rails.application.config.session_store :cookie_store, key: '_ifme_session', same_site: :lax
