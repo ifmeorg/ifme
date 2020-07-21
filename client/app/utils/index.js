f8ae3c16ee0cab8c6739bebeb4a70e1ee8a31f92
Cory Thompson <public@coryjthompson.com>
2020-07-21 22:32:31 +1000

    Fix formatting

--- client/app/utils/index.js
+++ client/app/utils/index.js
@@ -7,12 +7,12 @@ const randomString = () => Math.random().toString(36).substring(2, 15)
 const setCsrfToken = () => {
   const tokenDom = document.querySelector('meta[name=csrf-token]');
   if (tokenDom) {
-     const csrfToken = tokenDom.getAttribute('content');
-     axios.defaults.headers.common['X-CSRF-Token'] = csrfToken;
+    const csrfToken = tokenDom.getAttribute('content');
+    axios.defaults.headers.common['X-CSRF-Token'] = csrfToken;
   }
-}
+};
 
 export const Utils = {
   randomString,
-  setCsrfToken
+  setCsrfToken,
 };
