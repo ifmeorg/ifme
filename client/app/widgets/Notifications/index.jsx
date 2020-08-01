0244391029721b2ef4434b2636fe876c3b4fd668
Siddharth Kumar Verma <sidkv.skv@outlook.com>
2020-08-01 23:03:17 +0530

    Changed class to function for using hooks (#1773)
    * Changed class to function for using hooks
    
    * Error fixed
    
    * Error fixed
    
    * done suggested changes
    
    * Requested changes done
    
    * Requested changes done
    
    * changes done

--- client/app/widgets/Notifications/index.jsx
+++ client/app/widgets/Notifications/index.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import renderHTML from 'react-render-html';
 import Modal from '../../components/Modal';
@@ -18,25 +18,34 @@ export type State = {
   signedInKey?: number,
 };
 
-export class Notifications extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { notifications: '', alreadyMounted: false, open: false };
-  }
+export const Notifications = ({
+  element,
+}: Props) => {
+  const [notifications, setnotifications] = useState('');
+  const [alreadyMounted, setalreadyMounted] = useState(false);
+  const [open, setopen] = useState(false);
+  const [signedInKey, setsignedInKey] = useState();
+  const [modalKey, setmodalKey] = useState();
 
-  componentDidMount() {
-    this.fetchNotifications();
-  }
+  const setBody = (paramsNotifications: string[]) => {
+    let updatedNotifications = '';
+    paramsNotifications.forEach((item: string) => {
+      updatedNotifications += `<div>${item}</div>`;
+    });
+    setnotifications(updatedNotifications);
+  };
 
-  changeTitle = (count: number) => {
+  const changeTitle = (count: number) => {
     let { title } = window.document;
     const eliminate = `${title.substr(0, title.indexOf(') '))})`;
     title = title.replace(eliminate, '');
     window.document.title = count === 0 ? title : `(${count}) ${title}`;
   };
 
-  getPusherKey = (signedInKey: number) => {
-    this.setState({ signedInKey });
+  let fetchNotifications;
+
+  const getPusherKey = (paramSignedInKey: number) => {
+    setsignedInKey(paramSignedInKey);
     const metaPusherKey = Array.from(
       window.document.getElementsByTagName('meta'),
     ).filter((item) => item.getAttribute('name') === 'pusher-key')[0];
@@ -45,17 +54,9 @@ export class Notifications extends React.Component<Props, State> {
     const channel = pusher.subscribe(`private-${signedInKey}`);
     channel.bind('new_notification', (response: any) => {
       if (response && response.data) {
-        this.fetchNotifications();
+        fetchNotifications();
       }
     });
-  };
-
-  setBody = (notifications: string[]) => {
-    let updatedNotifications = '';
-    notifications.forEach((item: string) => {
-      updatedNotifications += `<div>${item}</div>`;
-    });
-    this.setState({ notifications: updatedNotifications });
   };
 
   fetchNotifications = () => {
@@ -70,66 +71,72 @@ export class Notifications extends React.Component<Props, State> {
           }
           return axios.get('/notifications/fetch_notifications');
         }
-        return -1;
-      })
-      .then((response: any) => {
-        if (response && response.data && response.data.fetch_notifications) {
-          this.changeTitle(response.data.fetch_notifications.length);
-          this.setBody(response.data.fetch_notifications);
-          if (!alreadyMounted && response.data.fetch_notifications.length > 0) {
-            this.setState({
-              alreadyMounted: true,
-              open: true,
-              modalKey: Utils.randomString(),
-            });
-          }
+      }
+      return -1;
+    })
+    .then((response: any) => {
+      if (response && response.data && response.data.fetch_notifications) {
+        changeTitle(response.data.fetch_notifications.length);
+        setBody(response.data.fetch_notifications);
+        if (!alreadyMounted && response.data.fetch_notifications.length > 0) {
+          setalreadyMounted(true);
+          setopen(true);
+          setmodalKey(Utils.randomString());
         }
-      });
-  };
+      }
+    });
 
-  clearNotifications = () => {
+  const clearNotifications = () => {
     axios.delete('/notifications/clear').then((response: any) => {
       if (response) {
-        this.changeTitle(0);
-        this.setState({ open: false, modalKey: Utils.randomString() });
-        this.fetchNotifications();
+        changeTitle(0);
+        setopen(false);
+        setmodalKey(Utils.randomString());
+        fetchNotifications();
       }
     });
   };
 
-  displayNotifications = () => {
-    const { notifications } = this.state;
-    return (
-      <div aria-live="polite">
-        {renderHTML(notifications)}
-        <button
-          type="button"
-          className="buttonDarkS smallMarginTop"
-          onClick={this.clearNotifications}
-        >
-          {I18n.t('notifications.clear')}
-        </button>
-      </div>
-    );
-  };
+  const displayNotifications = () => (
+    <div aria-live="polite">
+      {renderHTML(notifications)}
+      <button
+        type="button"
+        className="buttonDarkS smallMarginTop"
+        onClick={clearNotifications}
+      >
+        {I18n.t('notifications.clear')}
+      </button>
+    </div>
+  );
 
-  render() {
-    const { element } = this.props;
-    const { notifications, open, modalKey } = this.state;
-    return (
-      <Modal
-        element={element}
-        elementId="notificationsElement"
-        title={I18n.t('notifications.plural')}
-        body={
-          notifications.length
-            ? this.displayNotifications()
-            : I18n.t('notifications.none')
-        }
-        openListener={this.fetchNotifications}
-        open={open}
-        modalKey={modalKey}
-      />
-    );
-  }
-}
+  useEffect(() => {
+    fetchNotifications();
+  }, []);
+
+  return (
+    <Modal
+      element={element}
+      elementId="notificationsElement"
+      title={I18n.t('notifications.plural')}
+      body={
+        notifications.length
+          ? displayNotifications()
+          : I18n.t('notifications.none')
+      }
+      openListener={fetchNotifications}
+      open={open}
+      modalKey={modalKey}
+    />
+  );
+};
+
+// There's a [bug](https://github.com/shakacode/react_on_rails/issues/1198) with React on Rails,
+// so we'll need to do this in order to render multiple components with hooks on the same page.
+export default ({
+  element,
+}: Props) => (
+  <Notifications
+    element={element}
+  />
+);
