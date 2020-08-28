0176edea30a4d2666598d240ee753082cafa60ab
Ben Pham <bennguyenpham@hotmail.com>
2020-08-27 23:21:38 -0700

    refactors let! into arrange step of tests - I personally rather not use let if there's only 1 it declaration   - since there's only 1 it statement, no point in using let!   - if you definitely need that variable, define it in the it block - using regular let to lazy instantiate the variables would fail   - not all of the paths seem to call the let variables directly

--- spec/requests/groups/membership_spec.rb
+++ spec/requests/groups/membership_spec.rb
@@ -7,12 +7,16 @@ RSpec.describe 'Membership', type: :request do
 
   describe '#create' do
     context 'new group member for existing group' do
-      let(:leader_user) { create :user2 }
-      let(:group_leader) { create :group_member, user_id: leader_user.id, leader: true }
-      let(:group) { group_leader.group }
-
       it 'adds new member' do
+        # Arrange
+        leader_user = create :user2
+        group_leader = create :group_member, user_id: leader_user.id, leader: true
+        group = group_leader.group
+
+        # Act
         post group_membership_path(group_id: group.id)
+
+        # Assert
         expect(response).to redirect_to(group_path(group))
         expect(flash[:notice]).to eq(
           'You have joined this group.'
@@ -22,7 +26,10 @@ RSpec.describe 'Membership', type: :request do
 
     context 'new group member for non-existent group' do
       it "doesn't new member" do
+        # Act
         post group_membership_path(group_id: 9000)
+
+        # Assert
         expect(response).to redirect_to(groups_path)
         expect(flash[:alert]).to eq(
           "You cannot join a group that doesn't exist."
@@ -33,13 +40,17 @@ RSpec.describe 'Membership', type: :request do
 
   describe '#kick' do
     context 'when current_user is a leader' do
-      let!(:group_leader) { create :group_member, user_id: current_user.id, leader: true }
-      let!(:group) { group_leader.group }
-      let!(:another_user) { create :user2 }
-      let!(:another_member) { create :group_member, user_id: another_user.id, leader: false, group: group }
-
       it 'can kick another group member out' do
+        # Arrange
+        group_leader = create :group_member, user_id: current_user.id, leader: true
+        group = group_leader.group
+        another_user = create :user2
+        another_member = create :group_member, user_id: another_user.id, leader: false, group: group
+
+        # Act
         delete kick_group_membership_path(group_id: group.id, member_id: another_user.id)
+
+        # Assert
         expect(response).to redirect_to(groups_path)
         expect(flash[:notice]).to eq(
           "You have removed #{another_user.name} from #{group.name}"
@@ -48,13 +59,17 @@ RSpec.describe 'Membership', type: :request do
     end
 
     context 'when current_user is not a leader' do
-      let!(:group_member) { create :group_member, user_id: current_user.id, leader: false }
-      let!(:group) { group_member.group }
-      let!(:another_user) { create :user2 }
-      let!(:another_member) { create :group_member, user_id: another_user.id, leader: false, group: group }
-
       it 'cannot kick another group member out' do
+        # Arrange
+        group_member = create :group_member, user_id: current_user.id, leader: false
+        group = group_member.group
+        another_user = create :user2
+        another_member = create :group_member, user_id: another_user.id, leader: false, group: group
+
+        # Act
         delete kick_group_membership_path(group_id: group.id, member_id: another_user.id)
+
+        # Assert
         expect(response).to redirect_to(groups_path)
         expect(flash[:alert]).to eq(
           'You cannot kick a group member because you are not a leader.'
@@ -65,13 +80,17 @@ RSpec.describe 'Membership', type: :request do
 
   describe '#leave' do
     context 'when current_user is not a leader' do
-      let!(:leader_user) { create :user2 }
-      let!(:leader_member) { create :group_member, user_id: leader_user.id, leader: true }
-      let!(:another_member) { create :group_member, user_id: current_user.id, leader: false }
-      let!(:group) { another_member.group }
-
       it 'can leave group' do
+        # Arrange
+        leader_user = create :user2
+        leader_member = create :group_member, user_id: leader_user.id, leader: true
+        another_member = create :group_member, user_id: current_user.id, leader: false
+        group = another_member.group
+
+        # Act
         delete group_membership_path(group_id: group.id)
+
+        # Assert
         expect(response).to redirect_to(groups_path)
         expect(flash[:notice]).to eq(
           "You have left #{group.name}"
@@ -80,13 +99,18 @@ RSpec.describe 'Membership', type: :request do
     end
 
     context 'when current_user is a leader' do
-      let!(:group_member) { create :group_member, user_id: current_user.id, leader: true }
+      let(:group_member) { create :group_member, user_id: current_user.id, leader: true }
 
       it 'can leave the group when user is not the only leader' do
+        # Arrange
         group = group_member.group
         another_user = create :user2
         another_member = create :group_member, user_id: another_user.id, leader: true, group: group
+
+        # Act
         delete group_membership_path(group_id: group.id)
+
+        # Assert
         expect(response).to redirect_to(groups_path)
         expect(flash[:notice]).to eq(
           "You have left #{group.name}"
@@ -94,7 +118,10 @@ RSpec.describe 'Membership', type: :request do
       end
 
       it 'cannot leave the group when user is the only leader' do
+        # Act
         delete group_membership_path(group_id: group_member.group.id)
+
+        # Assert
         expect(response).to redirect_to(groups_path)
         expect(flash[:alert]).to eq(
           'You cannot leave the group, you are the only leader.'
