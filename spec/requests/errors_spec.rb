b1a5e1ce31ea16e6ebda7223ccb1a6e050ce6cce
Julia Nguyen <tojulianguyen@gmail.com>
2020-10-01 21:08:45 -0700

    Updates to errors_spec.rb

--- spec/requests/errors_spec.rb
+++ spec/requests/errors_spec.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-RSpec.describe ErrorsController, type: :request do
+RSpec.describe 'Error', type: :request do
   describe 'GET #not_found' do
     it 'returns http 404' do
-      get '/errors/not_found'
+      get errors_not_found_path
       expect(response).to have_http_status(404)
     end
   end
 
   describe 'GET #internal_server_error' do
     it 'returns http 500' do
-      get '/errors/internal_server_error'
+      get errors_internal_server_error_path
       expect(response).to have_http_status(500)
     end
   end
