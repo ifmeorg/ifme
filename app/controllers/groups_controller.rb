49eca5a74d7193e8d1601417c15b36b6f067aa59
Andrew Schutt <schutt.andrew@protonmail.com>
2020-10-06 08:53:54 -0500

    Add rack-mini-profiler gem

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -81,7 +81,7 @@ class GroupsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_group
-    @group = Group.includes(:leaders).friendly.find(params[:id])
+    @group = Group.friendly.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to_path(groups_path)
   end
