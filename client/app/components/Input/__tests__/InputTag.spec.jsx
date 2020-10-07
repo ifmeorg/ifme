7a4006ccfcf311b43ae91645526cc90d3ed9bc88
Udayan Shevade <udayan.shevade@gmail.com>
2020-10-06 23:26:51 -0700

    [1792] migrate InputTag tests to use RTL

--- client/app/components/Input/__tests__/InputTag.spec.jsx
+++ client/app/components/Input/__tests__/InputTag.spec.jsx
@@ -1,42 +1,65 @@
 // @flow
-import { mount } from 'enzyme';
-import { act } from 'react-dom/test-utils';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { InputMocks } from 'mocks/InputMocks';
 
 const component = InputMocks.createInput(InputMocks.inputTagProps);
+const { checkboxes } = InputMocks.inputTagProps;
 
 describe('InputTag', () => {
   it('type in value and select it', () => {
-    const wrapper = mount(component);
-    const id = `#${InputMocks.inputTagProps.checkboxes[1].id}`;
-    const value = { value: 'Two' };
-    expect(wrapper.find('.tagMenu').exists()).toEqual(true);
-    act(() => {
-      wrapper.find('.tagAutocomplete').simulate('change', { target: value });
-    });
-    wrapper.find('.tagAutocomplete').simulate('focus');
-    expect(wrapper.find(id).exists()).toEqual(false);
-    wrapper.find('.tagLabel').at(0).simulate('click');
-    expect(wrapper.find(id).exists()).toEqual(true);
+    const [, { label: checkboxLabelTwo }] = checkboxes;
+    render(component);
+
+    const combobox = screen.getByRole('combobox');
+    const input = screen.getByRole('textbox');
+
+    expect(combobox).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+
+    // simulate searching for an option
+    userEvent.type(input, 'Two');
+    userEvent.click(input);
+
+    // at this point the checkbox for the option should still not be visible
+    expect(
+      screen.queryByRole('checkbox', { name: checkboxLabelTwo }),
+    ).not.toBeInTheDocument();
+    // but after selecting the option, its associated checkbox should exist
+    userEvent.click(screen.getByRole('option', { name: checkboxLabelTwo }));
+    expect(
+      screen.getByRole('checkbox', { name: checkboxLabelTwo }),
+    ).toBeInTheDocument();
   });
 
   it('un-check existing value and retype and select it', () => {
-    const wrapper = mount(component);
-    const id = `input#${InputMocks.inputTagProps.checkboxes[0].id}`;
-    const value = { value: 'One' };
-    expect(wrapper.find(id).exists()).toEqual(true);
-    act(() => {
-      wrapper.find(id).prop('onChange')({ currentTarget: { checked: false } });
-    });
-    wrapper.update();
-    expect(wrapper.find(id).exists()).toEqual(false);
-    act(() => {
-      wrapper.find('.tagAutocomplete').simulate('change', { target: value });
-    });
-    wrapper.find('.tagAutocomplete').simulate('focus');
-    expect(wrapper.find('.tagMenu').exists()).toEqual(true);
-    expect(wrapper.find(id).exists()).toEqual(false);
-    wrapper.find('.tagLabel').at(0).simulate('click');
-    expect(wrapper.find(id).exists()).toEqual(true);
+    const [{ label: checkboxLabelOne }] = checkboxes;
+    render(component);
+
+    const combobox = screen.getByRole('combobox');
+    const input = screen.getByRole('textbox');
+
+    expect(combobox).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+
+    // toggle off the target option's checkbox, so we can test re-selecting it below
+    const checkbox = screen.getByRole('checkbox', { name: checkboxLabelOne });
+    expect(checkbox).toBeInTheDocument();
+    userEvent.click(checkbox);
+    // after toggling it, the checkbox should disappear
+    expect(
+      screen.queryByRole('checkbox', { name: checkboxLabelOne }),
+    ).not.toBeInTheDocument();
+
+    // search for the option and re-select it
+    userEvent.type(input, 'One');
+    userEvent.click(input);
+    expect(
+      screen.queryByRole('checkbox', { name: checkboxLabelOne }),
+    ).not.toBeInTheDocument();
+    userEvent.click(screen.getByRole('option', { name: checkboxLabelOne }));
+    expect(
+      screen.getByRole('checkbox', { name: checkboxLabelOne }),
+    ).toBeInTheDocument();
   });
 });
