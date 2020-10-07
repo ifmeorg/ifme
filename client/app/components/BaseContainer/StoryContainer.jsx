e5da5de8f951c848cffcfe1e2631786d0ce3ca22
brittonjenner <britton.jenner@gmail.com>
2020-10-06 20:14:32 -0400

    Refactor StoryContainer to be more concise

--- client/app/components/BaseContainer/StoryContainer.jsx
+++ client/app/components/BaseContainer/StoryContainer.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React, { StatelessFunctionalComponent } from 'react';
+import React from 'react';
 import { Story } from 'components/Story';
 import type { Props as StoryProps } from 'components/Story';
 
@@ -7,29 +7,26 @@ export type Props = {
   data: StoryProps[],
 };
 
-const StoryContainer = (props): StatelessFunctionalComponent<Props> => {
-  const { data } = props;
-  return (
-    <div className="gridTwo">
-      {data.map((storyProps) => (
-        <div className="gridTwoItemBoxLight" key={storyProps.link}>
-          <Story
-            date={storyProps.date}
-            categories={storyProps.categories}
-            moods={storyProps.moods}
-            storyType={storyProps.storyType}
-            storyBy={storyProps.storyBy}
-            actions={storyProps.actions}
-            draft={storyProps.draft}
-            name={storyProps.name}
-            link={storyProps.link}
-            body={storyProps.body}
-            medicationBody={storyProps.medicationBody}
-          />
-        </div>
-      ))}
-    </div>
-  );
-};
+const StoryContainer = ({ data }: Props) => (
+  <div className="gridTwo">
+    {data.map((storyProps) => (
+      <div className="gridTwoItemBoxLight" key={storyProps.link}>
+        <Story
+          date={storyProps.date}
+          categories={storyProps.categories}
+          moods={storyProps.moods}
+          storyType={storyProps.storyType}
+          storyBy={storyProps.storyBy}
+          actions={storyProps.actions}
+          draft={storyProps.draft}
+          name={storyProps.name}
+          link={storyProps.link}
+          body={storyProps.body}
+          medicationBody={storyProps.medicationBody}
+        />
+      </div>
+    ))}
+  </div>
+);
 
 export default StoryContainer;
