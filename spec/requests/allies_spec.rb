6beea039d08921432d8e1aa508f8363644a18cd7
andrewchang <andrewc@hotdoc.com.au>
2020-10-10 16:11:28 +1100

    test allies index

--- spec/requests/allies_spec.rb
+++ spec/requests/allies_spec.rb
@@ -10,9 +10,21 @@ describe AlliesController do
 
     context 'when user is logged in' do
       before { sign_in user }
+      let (:incoming_ally) { create(:user, name: 'Stella') }
+      let (:outgoing_ally) { create(:user, name: 'Sam') }
+      let (:invited_ally) { create(:user, invited_by: user) }
 
-      it 'returns a successful response when hit' do
-        expect(subject).to be_successful
+      it 'returns a successful response' do
+        subject
+        expect(response).to be_successful
+      end
+
+      it 'correctly includes pending and accepted allies' do
+        allow(user).to receive(:allies_by_status).with(:accepted).and_return([ally])
+        allow(user).to receive(:allies_by_status).with(:pending_from_user).and_return([incoming_ally])
+        allow(user).to receive(:allies_by_status).with(:pending_from_ally).and_return([outgoing_ally])
+        subject
+        expect(response.body).to include('Stella') && include('Marco') && include('Sam')
       end
     end
 
