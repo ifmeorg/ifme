c3b4b9da012506632ad211fe62062ed7939f336b
Jorge Alejandro Vargas Martinez <36525675+jvargas98@users.noreply.github.com>
2020-10-10 14:27:34 -0500

    Change care_plan request tests (#1896)
    Improve request test for care_plan following DAMP principle

--- spec/requests/care_plan_spec.rb
+++ spec/requests/care_plan_spec.rb
@@ -4,30 +4,23 @@ RSpec.describe "Care Plan", type: :request do
   describe '#index' do
     context 'when user is signed in' do
       let(:user) { create(:user) }
-      before do
-        sign_in user
-        get care_plan_path
-      end
-
-      it 'renders template' do
-        expect(response).to be_successful
-      end
+      before { sign_in user }
 
       it 'response is successful' do
-        expect(response).to have_http_status(:ok)
-        expect(response.content_type).to eq('text/html; charset=utf-8')
+        get care_plan_path
         expect(response).to be_successful
       end
 
-      context 'when there are bookmared strategies' do
+      context 'when there are bookmarked strategies' do
         let!(:strategy) { create(:strategy, user: user, bookmarked: true) }
-        before { get care_plan_path }
 
-        it 'assigns @bookmarked_strategies' do
+        it 'assigns bookmarked_strategies' do
+          get care_plan_path
           expect(response.body).to include(strategy.name)
         end
       end
     end
+
     context 'when user is not signed in' do
       before { get care_plan_path }
 
@@ -36,7 +29,6 @@ RSpec.describe "Care Plan", type: :request do
       end
 
       it 'is redirected to sign in' do
-        expect(response).to have_http_status(:redirect)
         expect(response).to redirect_to(new_user_session_path)
       end
     end
