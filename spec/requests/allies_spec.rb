6bc4c05dbd54515b2794b710d297ab70c0708918
Andrew Chang <50430618+andrewhchang@users.noreply.github.com>
2020-10-11 11:38:17 +1100

    Refactor/allies spec (#1899)
    * change allies controller spec to request spec
    
    * test allies index
    
    * test allies add_allies path
    
    * test allies remove_allies path
    
    * formatting and linting

--- spec/requests/allies_spec.rb
+++ spec/requests/allies_spec.rb
@@ -6,30 +6,25 @@ describe AlliesController do
   let(:notification) { double(:notification) }
 
   describe '#index' do
-    subject { get :index }
+    subject { get allies_path }
 
     context 'when user is logged in' do
-      include_context :logged_in_user
+      before { sign_in user }
+      let (:incoming_ally) { create(:user, name: 'Stella') }
+      let (:outgoing_ally) { create(:user, name: 'Sam') }
+      let (:invited_ally) { create(:user, invited_by: user) }
 
-      it 'renders the correct template' do
-        expect(subject).to render_template(:index)
+      it 'returns a successful response' do
+        subject
+        expect(response).to be_successful
       end
 
-      it 'sets the correct instance variables' do
-        incoming_ally = create(:user, name: 'Stella')
-        outgoing_ally = create(:user, name: 'Sam')
-        invited_ally = create(:user, invited_by: user)
-
+      it 'correctly includes pending and accepted allies' do
         allow(user).to receive(:allies_by_status).with(:accepted).and_return([ally])
         allow(user).to receive(:allies_by_status).with(:pending_from_user).and_return([incoming_ally])
         allow(user).to receive(:allies_by_status).with(:pending_from_ally).and_return([outgoing_ally])
-
         subject
-        expect(assigns(:page_search)).to eq true
-        expect(assigns(:accepted_allies)).to eq [ally]
-        expect(assigns(:incoming_ally_requests)).to eq [incoming_ally]
-        expect(assigns(:outgoing_ally_requests)).to eq [outgoing_ally]
-        expect(assigns(:invited_allies)).to eq [invited_ally]
+        expect(response.body).to include('Stella') && include('Marco') && include('Sam')
       end
     end
 
@@ -42,7 +37,7 @@ describe AlliesController do
 
   describe 'POST #add' do
     context 'when user is logged in' do
-      include_context :logged_in_user
+      before { sign_in user }
 
       let(:notification_params) do
         {
@@ -58,22 +53,23 @@ describe AlliesController do
         }
       end
 
-      subject { post :add, params: { ally_id: ally.id } }
+      subject { post add_allies_path, params: { ally_id: ally.id } }
 
       context 'with an existing ally request' do
         let!(:allyship) do
           Allyship.create(
-            user_id: user.id,
-            ally_id: ally.id,
-            status: :pending_from_ally
+            user_id: user.id, ally_id: ally.id, status: :pending_from_ally
           )
         end
 
         let!(:notification) do
-          create(:notification, uniqueid: "new_ally_request_#{ally.id}", user_id: user.id)
+          create(
+            :notification,
+            uniqueid: "new_ally_request_#{ally.id}", user_id: user.id
+          )
         end
 
-        let!(:notification_type) { 'accepted_ally_request' }
+        let(:notification_type) { 'accepted_ally_request' }
 
         it 'updates the allyship status to "accepted"' do
           subject
@@ -85,8 +81,9 @@ describe AlliesController do
         end
 
         it 'creates an accepted allyship request notification' do
-          expect(Notification).to receive(:create).with(notification_params)
+          allow(Notification).to receive(:create).with(notification_params)
           subject
+          expect(Notification).to have_received(:create).with(notification_params)
         end
 
         it 'sends an email notification to the ally' do
@@ -97,9 +94,13 @@ describe AlliesController do
             type: notification_type.to_s,
             uniqueid: "#{notification_type}_#{user.id}"
           }.to_json
-          allow(NotificationMailer).to receive(:notification_email).with(ally.id.to_s, data).and_return(notification)
-          expect(notification).to receive(:deliver_now)
+          allow(NotificationMailer).to receive(:notification_email).with(
+            ally.id.to_s,
+            data
+          ).and_return(notification)
+          allow(notification).to receive(:deliver_now)
           subject
+          expect(notification).to have_received(:deliver_now)
         end
       end
 
@@ -125,7 +126,7 @@ describe AlliesController do
     end
 
     context 'when user is not logged in' do
-      before { post :add }
+      before { post add_allies_path }
 
       it_behaves_like :with_no_logged_in_user
     end
@@ -135,18 +136,18 @@ describe AlliesController do
     let!(:allyship) { double(:allyship) }
 
     context 'when user is logged in' do
-      include_context :logged_in_user
+      before { sign_in user }
 
       it 'deletes the allyship' do
         allow(Allyship).to receive(:where).and_return(allyship)
         expect(allyship).to receive(:destroy_all)
-        delete :remove, params: { ally_id: ally.id }
+        post remove_allies_path, params: { ally_id: ally.id }
         expect(response).to redirect_to allies_path
       end
     end
 
     context 'when user is not logged in' do
-      before { delete :remove }
+      before { post remove_allies_path }
       it_behaves_like :with_no_logged_in_user
     end
   end
