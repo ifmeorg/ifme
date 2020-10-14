e5239fba033e8efee42e02d19d45533c9fbf4f32
Ashwitha B G <ashwithabg95@gmail.com>
2020-10-14 11:29:54 +0530

    Refactor notification controller spec to notification request spec (#1903)
    * Refactor notification controller spec to notification request spec
    
    * Change the message in context for notification tests.
    
    * Remove duplication of header in notification spec.
    
    * specify format for all HTML contexts

--- spec/requests/notifications_spec.rb
+++ spec/requests/notifications_spec.rb
@@ -1,42 +1,46 @@
 # frozen_string_literal: true
 
-RSpec.describe NotificationsController, type: :controller do
+describe 'Notification', type: :request do
+  let(:previous_page) { 'http://example.com/previous' }
+  let(:format) { 'text/html' }
+  let(:headers) do
+    {
+      'HTTP_REFERER' => previous_page,
+      'ACCEPT' => format
+    }
+  end
+
   describe '#destroy' do
-    let(:user) { FactoryBot.create(:user1) }
-    let(:other_user) { FactoryBot.create(:user2) }
+    let(:user) { create(:user1) }
+    let(:other_user) { create(:user2) }
     let(:notification_owner) { user }
-    let!(:notification) do
-      FactoryBot.create(:notification, user: notification_owner)
-    end
+    let!(:notification) { create(:notification, user: notification_owner) }
 
     context 'when the user is signed in' do
-      let(:previous_page) { 'http://example.com/previous' }
       let(:notification_id) { notification.id }
-      let(:format) { 'html' }
 
       before do
-        allow(controller).to receive(:current_user).and_return(user)
-        request.env['HTTP_REFERER'] = previous_page
+        sign_in user
 
-        delete :destroy, format: format, params: { id: notification_id }
+        delete notification_path(id: notification_id), headers: headers
       end
 
-      context 'and the notification to be deleted exists' do
-        context 'and the notification belongs to the user' do
+      context 'when notification to be deleted exists' do
+        context 'when notification belongs to the user' do
           it 'deletes the notification' do
             expect(Notification.find_by(id: notification_id)).to be_nil
           end
 
-          context 'and the requested format is html' do
-            let(:format) { 'html' }
+          context 'when requested format is html' do
+            let(:format) { 'text/html' }
 
             it 'redirects the user back to where they were before' do
               expect(response).to redirect_to previous_page
             end
           end
 
-          context 'and the requested format is json' do
-            let(:format) { 'json' }
+          context 'when requested format is json' do
+            let(:format) { 'application/json' }
 
             it 'renders a HEAD response with :no_content' do
               expect(response).to have_http_status 204
@@ -44,23 +48,23 @@ RSpec.describe NotificationsController, type: :controller do
           end
         end
 
-        context 'and the notification does not belong to the user' do
+        context 'when notification does not belong to the user' do
           let(:notification_owner) { other_user }
 
           it 'does not delete the notification' do
             expect(Notification.find_by(id: notification_id)).to_not be_nil
           end
 
-          context 'and the requested format is html' do
-            let(:format) { 'html' }
+          context 'when requested format is html' do
+            let(:format) { 'text/html' }
 
             it 'redirects the user back to where they were before' do
               expect(response).to redirect_to previous_page
             end
           end
 
-          context 'and the requested format is json' do
-            let(:format) { 'json' }
+          context 'when requested format is json' do
+            let(:format) { 'application/json' }
 
             it 'renders a HEAD response with :no_content' do
               expect(response).to have_http_status 204
@@ -69,19 +73,19 @@ RSpec.describe NotificationsController, type: :controller do
         end
       end
 
-      context 'and the notification to be deleted does not exist' do
+      context 'when notification to be deleted does not exist' do
         let(:notification_id) { 'something-fake' }
 
-        context 'and the requested format is html' do
-          let(:format) { 'html' }
+        context 'when requested format is html' do
+          let(:format) { 'text/html' }
 
           it 'redirects the user back to where they were before' do
             expect(response).to redirect_to previous_page
           end
         end
 
-        context 'and the requested format is json' do
-          let(:format) { 'json' }
+        context 'when requested format is json' do
+          let(:format) { 'application/json' }
 
           it 'renders a HEAD response with :no_content' do
             expect(response).to have_http_status 204
@@ -91,22 +95,22 @@ RSpec.describe NotificationsController, type: :controller do
     end
 
     context 'when the user is not signed in' do
-      before do
-        allow(controller).to receive(:user_signed_in?).and_return(false)
+      let(:invalid_id) { 'foo' }
 
-        delete :destroy, format: format, params: { id: 'foo' }
+      before do
+        delete notification_path(id: invalid_id), headers: headers
       end
 
-      context 'and the requested format is html' do
-        let(:format) { 'html' }
+      context 'when requested format is html' do
+        let(:format) { 'text/html' }
 
         it 'redirects to the new_user_session_path' do
           expect(response).to redirect_to new_user_session_path
         end
       end
 
-      context 'and the requested format is json' do
-        let(:format) { 'json' }
+      context 'when requested format is json' do
+        let(:format) { 'application/json' }
 
         it 'renders a HEAD response with :no_content' do
           expect(response).to have_http_status 204
@@ -118,14 +122,10 @@ RSpec.describe NotificationsController, type: :controller do
   describe '#clear' do
     let(:user) { FactoryBot.create(:user1) }
     let(:other_user) { FactoryBot.create(:user2) }
-    let!(:other_user_notification) do
-      FactoryBot.create(:notification, user: other_user)
-    end
+    let!(:other_user_notification) { create(:notification, user: other_user) }
 
     context 'when the user is signed in' do
-      before do
-        allow(controller).to receive(:current_user).and_return(user)
-      end
+      before { sign_in user }
 
       context 'when the user has notifications' do
         let!(:notification) do
@@ -139,27 +139,30 @@ RSpec.describe NotificationsController, type: :controller do
         it 'deletes all notifications belonging to the current user' do
           expect(Notification.where(user_id: user.id).count).to eq(2)
 
-          delete :clear
+          delete clear_notifications_url
+
           expect(Notification.where(user_id: user.id).count).to eq(0)
         end
 
         it 'does not delete notifications belonging to other users' do
           expect(Notification.where(user_id: other_user.id).count).to eq(1)
 
-          delete :clear
+          delete clear_notifications_url
+
           expect(Notification.where(user_id: other_user.id).count).to eq(1)
         end
       end
 
       context 'when the user does not have notifications' do
         it 'does does not delete any notifications' do
-          delete :clear
+          delete clear_notifications_url
+
           expect(Notification.where(user_id: user.id)).to be_empty
         end
       end
 
       it 'renders nothing' do
-        delete :clear
+        delete clear_notifications_url
 
         expect(response).to have_http_status 200
         expect(response.body).to be_empty
@@ -168,21 +171,19 @@ RSpec.describe NotificationsController, type: :controller do
 
     context 'when the user is not signed in' do
       before do
-        allow(controller).to receive(:user_signed_in?).and_return(false)
-
-        delete :clear, format: format
+        delete clear_notifications_url, headers: headers
       end
 
-      context 'and the requested format is html' do
-        let(:format) { 'html' }
+      context 'when requested format is html' do
+        let(:format) { 'text/html' }
 
         it 'redirects to the new_user_session_path' do
           expect(response).to redirect_to new_user_session_path
         end
       end
 
-      context 'and the requested format is json' do
-        let(:format) { 'json' }
+      context 'when requested format is json' do
+        let(:format) { 'application/json' }
 
         it 'renders a HEAD response with :no_content' do
           expect(response).to have_http_status 204
@@ -192,29 +193,24 @@ RSpec.describe NotificationsController, type: :controller do
   end
 
   describe '#fetch_notifications' do
-    let(:user) { FactoryBot.create(:user1) }
-    let(:other_user) { FactoryBot.create(:user2) }
-    let!(:other_user_notification) do
-      FactoryBot.create(:notification, user: other_user)
-    end
+    let(:user) { create(:user1) }
+    let(:other_user) { create(:user2) }
+    let!(:other_user_notification) { create(:notification, user: other_user) }
 
     context 'when the user is signed in' do
-      let!(:notification) do
-        FactoryBot.create(:notification, user: user)
+      let!(:notification) { create(:notification, user: user) }
+      let!(:notification_two) { create(:notification, user: user) }
+      let(:notification_link) do
+        '<a id="MyString" href="/moments/1">Julia Nguyen commented "Hello" on typename</a>'
       end
-
-      let!(:notification_two) do
-        FactoryBot.create(:notification, user: user)
-      end
-
-      let (:notification_link) { '<a id="MyString" href="/moments/1">Julia Nguyen commented "Hello" on typename</a>' }
       let(:expected_result) do
         { fetch_notifications: [notification_link, notification_link] }.to_json
       end
 
       before do
-        allow(controller).to receive(:current_user).and_return(user)
-        get :fetch_notifications
+        sign_in user
+
+        get fetch_notifications_notifications_url
       end
 
       it 'returns JSON with the users notifications' do
@@ -224,21 +220,19 @@ RSpec.describe NotificationsController, type: :controller do
 
     context 'when the user is not signed in' do
       before do
-        allow(controller).to receive(:user_signed_in?).and_return(false)
-
-        get :fetch_notifications, format: format
+        get fetch_notifications_notifications_url, headers: headers
       end
 
-      context 'and the requested format is html' do
-        let(:format) { 'html' }
+      context 'when requested format is html' do
+        let(:format) { 'text/html' }
 
         it 'redirects to the new_user_session_path' do
           expect(response).to redirect_to new_user_session_path
         end
       end
 
-      context 'and the requested format is json' do
-        let(:format) { 'json' }
+      context 'when requested format is json' do
+        let(:format) { 'application/json' }
 
         it 'renders a HEAD response with :no_content' do
           expect(response).to have_http_status 204
@@ -252,8 +246,8 @@ RSpec.describe NotificationsController, type: :controller do
 
     context 'when the user is signed in' do
       before do
-        allow(controller).to receive(:current_user).and_return(user)
-        get :signed_in
+        sign_in user
+        get signed_in_notifications_url
       end
 
       it "returns the user's id" do
@@ -263,21 +257,19 @@ RSpec.describe NotificationsController, type: :controller do
 
     context 'when the user is not signed in' do
       before do
-        allow(controller).to receive(:user_signed_in?).and_return(false)
-
-        get :signed_in, format: format
+        get signed_in_notifications_url, headers: headers
       end
 
-      context 'and the requested format is html' do
-        let(:format) { 'html' }
+      context 'when requested format is html' do
+        let(:format) { 'text/html' }
 
         it 'redirects to the new_user_session_path' do
           expect(response).to redirect_to new_user_session_path
         end
       end
 
-      context 'and the requested format is json' do
-        let(:format) { 'json' }
+      context 'when requested format is json' do
+        let(:format) { 'application/json' }
 
         it 'renders a HEAD response with :no_content' do
           expect(response).to have_http_status 204
