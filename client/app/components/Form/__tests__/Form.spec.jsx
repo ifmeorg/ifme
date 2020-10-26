5b1794d39b1cde9241cd5454146aa25c002a741f
cory cathearll <ccatherall15@gmail.com>
2020-10-26 13:08:05 -0400

    fixed some typos

--- client/app/components/Form/__tests__/Form.spec.jsx
+++ client/app/components/Form/__tests__/Form.spec.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { InputMocks } from 'mocks/InputMocks';
-import { Form } from 'components/Form/index';
+import Form from 'components/Form/index';
 
 // TODO (julianguyen): Include InputTextarea after writing stubs for pell editor
 
@@ -22,26 +22,26 @@ const getComponent = () => (
 );
 
 describe('Form', () => {
-  const {
-    getByRole, getByText, queryByRole, getByPlaceholderText,
-  } = screen;
+  const { getByRole, queryByRole, getByPlaceholderText } = screen;
 
   it('it renders properly', () => {
     render(getComponent());
     expect(getByPlaceholderText('Some Text Placeholder')).toBeInTheDocument();
-    expect(getByText('alert')).toBeInTheDocument();
+    expect(
+      getByRole('button', { name: 'Some Submit Value' }),
+    ).toBeInTheDocument();
   });
 
   it('has no alert message when textfield has value and submit clicked', () => {
     render(getComponent());
     userEvent.type(getByPlaceholderText('Some Text Placeholder'), 'randomName');
-    userEvent.click(getByText('alert'));
+    userEvent.click(getByRole('button', { name: 'Some Submit Value' }));
     expect(queryByRole('alert')).not.toBeInTheDocument();
   });
 
   it('displays alert message on submit with empty input', () => {
     render(getComponent());
-    userEvent.click(getByText('alert'));
+    userEvent.click(getByRole('button', { name: 'Some Submit Value' }));
     expect(getByRole('alert')).toBeInTheDocument();
   });
 });
