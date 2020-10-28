d66d554075224b8685ab25a31795c89dce700acb
cory cathearll <ccatherall15@gmail.com>
2020-10-28 11:24:57 -0400

    migrating story tests from enzyme to rtl

--- client/app/components/Story/__tests__/Story.spec.jsx
+++ client/app/components/Story/__tests__/Story.spec.jsx
@@ -1,53 +1,59 @@
 // @flow
-import { render } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { Story } from 'components/Story';
 
-describe('Story', () => {
+describe('story', () => {
+  const { getByText, getByRole } = screen;
   describe('has no options', () => {
     it('renders correctly', () => {
-      let wrapper = null;
-      expect(() => {
-        wrapper = render(<Story name="Real Moment" link="some-url" />);
-      }).not.toThrow();
-      expect(wrapper).not.toBeNull();
+      render(<Story name="Real Moment" link="some-url" />);
+      expect(getByText('Real Moment')).toBeInTheDocument();
+      expect(getByRole('link')).toBeInTheDocument();
     });
   });
-
   describe('has all options', () => {
     it('renders correctly', () => {
-      let wrapper = null;
-      expect(() => {
-        wrapper = render(
-          <Story
-            actions={{
-              edit: { link: 'some-url', name: 'Edit' },
-              delete: {
-                link: 'some-url',
-                name: 'Delete',
-                dataMethod: 'delete',
-                dataConfirm: 'Are you sure?',
-              },
-              viewers: 'blah',
-            }}
-            name="Real Moment"
-            link="some-url"
-            categories={[
-              { name: 'Family', slug: '/family' },
-              { name: 'Friends', slug: '/friends' },
-            ]}
-            moods={[
-              { name: 'Nervous', slug: '/nervous' },
-              { name: 'Excited', slug: '/excited' },
-            ]}
-            date="Created 2 Days ago"
-            draft="Draft"
-            storyType="Some Type"
-            storyBy={{ author: 'Some Person' }}
-          />,
-        );
-      }).not.toThrow();
-      expect(wrapper).not.toBeNull();
+      render(
+        <Story
+          actions={{
+            edit: { link: 'some-url', name: 'Edit' },
+            delete: {
+              link: 'some-url',
+              name: 'Delete',
+              dataMethod: 'delete',
+              dataConfirm: 'Are you sure?',
+            },
+            viewers: 'blah',
+          }}
+          name="Real Moment"
+          link="some-url"
+          categories={[
+            { name: 'Family', slug: '/family' },
+            { name: 'Friends', slug: '/friends' },
+          ]}
+          moods={[
+            { name: 'Nervous', slug: '/nervous' },
+            { name: 'Excited', slug: '/excited' },
+          ]}
+          date="Created 2 Days ago"
+          draft="Draft"
+          storyType="Some Type"
+          storyBy={{ author: 'Some Person' }}
+        />,
+      );
+      expect(getByText('Draft')).toBeInTheDocument();
+      expect(getByText('Real Moment')).toBeInTheDocument();
+      expect(getByText('Created 2 Days ago')).toBeInTheDocument();
+      expect(getByText('Family')).toBeInTheDocument();
+      expect(getByText('Friends')).toBeInTheDocument();
+      expect(getByText('Nervous')).toBeInTheDocument();
+      expect(getByText('Excited')).toBeInTheDocument();
+      expect(getByText('Some Person')).toBeInTheDocument();
+      expect(getByText('Some Type')).toBeInTheDocument();
+      expect(getByText('Edit')).toBeInTheDocument();
+      expect(getByText('Delete')).toBeInTheDocument();
+      expect(getByText('blah')).toBeInTheDocument();
     });
   });
 });
--- client/app/components/Story/__tests__/StoryActions.spec.jsx
+++ client/app/components/Story/__tests__/StoryActions.spec.jsx
@@ -1,63 +1,66 @@
 // @flow
-import { render } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { StoryActions } from 'components/Story/StoryActions';
 
-describe('StoryActions', () => {
-  let wrapper = null;
+describe('storyActions', () => {
+  const { getByText } = screen;
   describe('with create google calendar action', () => {
     it('renders correctly', () => {
-      expect(() => {
-        wrapper = render(
-          <StoryActions
-            actions={{
-              edit: { link: 'some-url', name: 'Edit' },
-              delete: {
-                link: 'some-url',
-                name: 'Delete',
-                dataMethod: 'delete',
-                dataConfirm: 'Are you sure?',
-              },
-              add_to_google_cal: {
-                link: 'some-url',
-                name: 'Add to google calendar',
-                dataMethod: 'post',
-              },
-              report: { link: 'some-url', name: 'Report' },
-              viewers: 'blah',
-            }}
-          />,
-        );
-      }).not.toThrow();
-      expect(wrapper).not.toBeNull();
+      render(
+        <StoryActions
+          actions={{
+            edit: { link: 'some-url', name: 'Edit' },
+            delete: {
+              link: 'some-url',
+              name: 'Delete',
+              dataMethod: 'delete',
+              dataConfirm: 'Are you sure?',
+            },
+            add_to_google_cal: {
+              link: 'some-url',
+              name: 'Add to google calendar',
+              dataMethod: 'post',
+            },
+            report: { link: 'some-url', name: 'Report' },
+            viewers: 'testViewer',
+          }}
+        />,
+      );
+      expect(getByText('Add to google calendar')).toBeInTheDocument();
+      expect(getByText('Edit')).toBeInTheDocument();
+      expect(getByText('Delete')).toBeInTheDocument();
+      expect(getByText('Report')).toBeInTheDocument();
+      expect(getByText('testViewer')).toBeInTheDocument();
     });
   });
-
   describe('with remove google calendar action', () => {
     it('renders correctly', () => {
-      expect(() => {
-        wrapper = render(
-          <StoryActions
-            actions={{
-              edit: { link: 'some-url', name: 'Edit' },
-              delete: {
-                link: 'some-url',
-                name: 'Delete',
-                dataMethod: 'delete',
-                dataConfirm: 'Are you sure?',
-              },
-              remove_from_google_cal: {
-                link: 'some-url',
-                name: 'Remove from google calendar',
-                dataMethod: 'delete',
-              },
-              report: { link: 'some-url', name: 'Report' },
-              viewers: 'blah',
-            }}
-          />,
-        );
-      }).not.toThrow();
-      expect(wrapper).not.toBeNull();
+      render(
+        <StoryActions
+          actions={{
+            edit: { link: 'some-url', name: 'Edit' },
+            delete: {
+              link: 'some-url',
+              name: 'Delete',
+              dataMethod: 'delete',
+              dataConfirm: 'Are you sure?',
+            },
+            remove_from_google_cal: {
+              link: 'some-url',
+              name: 'Remove from google calendar',
+              dataMethod: 'delete',
+            },
+            report: { link: 'some-url', name: 'Report' },
+            viewers: 'testViewer',
+          }}
+        />,
+      );
+      expect(getByText('Remove from google calendar')).toBeInTheDocument();
+      expect(getByText('Edit')).toBeInTheDocument();
+      expect(getByText('Delete')).toBeInTheDocument();
+      expect(getByText('Report')).toBeInTheDocument();
+      expect(getByText('testViewer')).toBeInTheDocument();
     });
   });
 });
