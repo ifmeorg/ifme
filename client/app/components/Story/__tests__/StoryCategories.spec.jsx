8409d42fb643648ae832a2904b60314be7b87b09
cory cathearll <ccatherall15@gmail.com>
2020-10-28 12:18:22 -0400

    migrate StoryCategories.spec and StoryDate.spec to RTL

--- client/app/components/Story/__tests__/StoryCategories.spec.jsx
+++ client/app/components/Story/__tests__/StoryCategories.spec.jsx
@@ -1,16 +1,15 @@
 // @flow
-import { render } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { StoryCategories } from 'components/Story/StoryCategories';
 
 describe('StoryCategories', () => {
+  const { getByText } = screen;
+
   it('renders correctly', () => {
-    let wrapper = null;
-    expect(() => {
-      wrapper = render(
-        <StoryCategories categories={[{ name: 'Family', slug: '/family' }]} />,
-      );
-    }).not.toThrow();
-    expect(wrapper).not.toBeNull();
+    render(
+      <StoryCategories categories={[{ name: 'Family', slug: '/family' }]} />,
+    );
+    expect(getByText('Family')).toBeInTheDocument();
   });
 });
