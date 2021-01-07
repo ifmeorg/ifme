746c1ef5ed445b3532994f5a76511551eb2bcd15
sebassebas1313 <62571526+sebassebas1313@users.noreply.github.com>
2021-01-07 22:01:25 +0000

    refactor #update_resource to return boolean (#1948)
    * refactor #update_resource to return boolean
    
    * change registrations_spec in order to not test devise imp
    
    Co-authored-by: sebastian cifuentes <secifuentes@deloitte.pt>

--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/BlockLength
 RSpec.describe 'Registrations', type: :request do
-
-  shared_examples "sucessful password update" do
-    let(:update_params) { { password: new_password, password_confirmation: new_password } }
+  shared_examples 'successfully redirects' do
     before do
       sign_in user
       put user_registration_path, params: update_params
@@ -12,17 +11,40 @@ RSpec.describe 'Registrations', type: :request do
     it { expect(response).to redirect_to(edit_user_registration_path) }
   end
 
-  describe "update password" do
-    let(:new_password) { "foobar_w!th_@" }
+  describe '#update' do
 
-    context "when the user is logged in with Google OAuth" do
+    let(:update_params){{}}
+
+    context 'when the user is logged in with Google OAuth' do
       let(:user) { create(:user_oauth, provider: 'google_oauth2') }
-      it_behaves_like "sucessful password update"
+      it_behaves_like 'successfully redirects'
     end
 
-    context "when the user is logged in with Facebook" do
+    context 'when the user is logged in with Facebook' do
       let(:user) { create(:user_oauth, provider: 'facebook') }
-      it_behaves_like "sucessful password update"
+      it_behaves_like 'successfully redirects'
+    end
+
+    context 'when the user is logged in with password' do
+      let(:user) { create(:user, provider: nil) }
+      let(:new_password) { 'foobar_w!th_@' }
+
+      context 'with valid params' do
+        let(:update_params) do
+          { user: { name: user.name, email: user.email, password_confirmation: new_password,
+                    password: new_password, current_password: user.password} }
+        end
+        it_behaves_like 'successfully redirects'
+      end
+
+      context 'with invalid params' do
+        it 'does not redirects' do
+          sign_in user
+          put user_registration_path, params: {}
+          expect(response).not_to redirect_to(edit_user_registration_path)
+        end
+      end
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
