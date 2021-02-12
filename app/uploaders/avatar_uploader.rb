27363d04ee84e51775a4eb9a3fbe73c34d5e98c1
arun.p <arun.p@qburst.com>
2021-02-12 11:07:20 +0530

    add white list of extensions for image

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -45,7 +45,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-    %w(jpg jpeg gif png)
+    %w[jpg jpeg gif png]
   end
 
   # Override the filename of the uploaded files:
