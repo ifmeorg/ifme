6ede8faaf607a4ebbab2a561187c1737fc2f7c24
Bruno Casali <brunoocasali@gmail.com>
2021-10-04 23:16:12 -0300

    Add confirmable to devise model
    Related to:
    https://github.com/ifmeorg/ifme/pull/1966
    https://github.com/ifmeorg/ifme/pull/1965

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_28_174852) do
+ActiveRecord::Schema.define(version: 2021_10_05_010735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -307,6 +307,11 @@ ActiveRecord::Schema.define(version: 2021_03_28_174852) do
     t.text "third_party_avatar"
     t.integer "failed_attempts", default: 0, null: false
     t.datetime "locked_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invitations_count"], name: "index_users_on_invitations_count"
