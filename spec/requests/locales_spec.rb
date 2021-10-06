dfe8bfac4b78aed4f3fc64e14e6dac20d4a4ea11
Bruno Casali <brunoocasali@gmail.com>
2021-10-06 19:34:54 -0300

    Remove controller spec for locales & Replace with request one
    Closes https://github.com/ifmeorg/ifme/issues/1826

--- spec/requests/locales_spec.rb
+++ spec/requests/locales_spec.rb
@@ -1,28 +1,25 @@
-# frozen_string_literal: true
-
-RSpec.describe LocalesController, type: :controller do
+describe 'Locales', type: :request do
   let(:user) { create(:user, locale: nil) }
 
   Rails.configuration.i18n.available_locales.each do |locale|
-    describe "GET ##{locale}" do
+    describe "GET #set_initial_locale for #{locale}" do
       context 'when the user is logged in' do
         before { sign_in user }
 
         it 'sets a locale cookie when prompted' do
-          get :set_initial_locale, params: { locale: locale.to_s }
+          get "/#{locale}"
 
-          expect(user.reload.locale).to eq(locale.to_s)
           expect(response).to redirect_to(root_path)
+          expect(user.reload.locale).to eq(locale.to_s)
         end
       end
 
       context 'when user is not logged' do
         it 'sets a locale cookie when prompted' do
-          get :set_initial_locale, params: { locale: locale.to_s }
+          get "/#{locale}"
 
-          expect(response).to be_redirect
-          expect(response.cookies['locale']).to eq(locale.to_s)
           expect(response).to redirect_to(root_path)
+          expect(response.cookies['locale']).to eq(locale.to_s)
         end
       end
     end
