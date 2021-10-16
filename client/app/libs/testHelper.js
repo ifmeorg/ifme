87cea449bf993c20b35fc6e0babf4ffa3dbfabaa
Julia Nguyen <tojulianguyen@gmail.com>
2021-10-16 17:07:47 -0400

    [#2031] Fix Error: Not implemented: HTMLFormElement.prototype.submit issue

--- client/app/libs/testHelper.js
+++ client/app/libs/testHelper.js
@@ -32,4 +32,22 @@ Object.defineProperty(window.location, 'origin', {
   value: 'https://if-me.org',
 });
 
+global.originalLogError = global.console.error;
+
+global.console.error = (...args) => {
+  /**
+   * Avoid jsdom error message after submitting a form
+   * https://github.com/jsdom/jsdom/issues/1937
+   */
+  const errorMessage = "Not implemented: HTMLFormElement.prototype.submit";
+
+  if (args && args[0].includes(errorMessage)) {
+    return false;
+  }
+
+  global.originalLogError(...args);
+
+  return true;
+};
+
 export { React, TestUtils };
