0cc7eb3f22744e3c1d9d70685ee9f46d723cd4ca
Julia Nguyen <tojulianguyen@gmail.com>
2021-11-18 23:37:27 -0500

    [#2052] Fix improper access control leads to admin self-banning

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -47,7 +47,9 @@ class NotificationsController < ApplicationController
     uniqueid = notification[:uniqueid]
     data = convert_to_hash(notification[:data])
     # In case data[:name] is invalid
-    data[:email] = User.find(data[:user_id]).email
+    if data[:user_id]
+      data[:email] = User.find(data[:user_id]).email
+    end
     case data[:type]
     when /comment/ then comment_link(uniqueid, data)
     when /accepted_ally_request/ then accepted_ally_link(uniqueid, data)
