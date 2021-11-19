febd65cad0ab92dae9c3db926e4ae97bca97bfe0
Julia Nguyen <tojulianguyen@gmail.com>
2021-11-19 00:31:15 -0500

    [#2052] Rubocop linting

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -47,9 +47,7 @@ class NotificationsController < ApplicationController
     uniqueid = notification[:uniqueid]
     data = convert_to_hash(notification[:data])
     # In case data[:name] is invalid
-    if data[:user_id]
-      data[:email] = User.find(data[:user_id]).email
-    end
+    data[:email] = User.find(data[:user_id]).email if data[:user_id]
     case data[:type]
     when /comment/ then comment_link(uniqueid, data)
     when /accepted_ally_request/ then accepted_ally_link(uniqueid, data)
