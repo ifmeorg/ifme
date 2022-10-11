720d81697ff9eac785b1b9a5e6f26706f7a18c15
Lee Mulvey <lmulvey@me.com>
2022-10-10 21:20:53 -0600

    feat(Header): improve keyboard accessibility with Header

--- client/app/components/Header/__tests__/Header.spec.jsx
+++ client/app/components/Header/__tests__/Header.spec.jsx
@@ -4,6 +4,13 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Header from 'components/Header';
 
+function setup(jsx) {
+  return {
+    user: userEvent.setup(),
+    ...render(jsx),
+  };
+}
+
 const component = (
   <Header
     home={{ name: 'Home', url: '/some-path' }}
@@ -16,27 +23,46 @@ const component = (
 
 describe('Header', () => {
   it('renders correctly', () => {
-    render(component);
+    setup(component);
 
     expect(screen.getByRole('banner')).toBeInTheDocument();
     expect(screen.getByRole('navigation')).toBeInTheDocument();
     expect(screen.getByRole('link', { name: /home/i })).toBeInTheDocument();
     expect(screen.getByRole('link', { name: /link 1/i })).toBeInTheDocument();
     expect(screen.getByRole('link', { name: /link 1/i })).toHaveClass(
-      'headerActiveLink',
+      'headerActiveLink'
     );
     expect(screen.getByRole('link', { name: /link 2/i })).toBeInTheDocument();
   });
 
-  it('toggles hamburger correctly', () => {
-    const { container } = render(component);
+  it('toggles hamburger correctly', async () => {
+    const { container, user } = setup(component);
     const hamburger = container.querySelector('#headerHamburger');
-    userEvent.click(hamburger);
+    await user.click(hamburger);
 
     const mobile = container.querySelector('#headerMobile');
     expect(mobile).toBeInTheDocument();
 
-    userEvent.click(hamburger);
+    await user.click(hamburger);
     expect(mobile).not.toBeInTheDocument();
+  });
+
+  it('toggles hamburger from keyboard input when expected', async () => {
+    const { container, user } = setup(component);
+    const hamburger = container.querySelector('#headerHamburger');
+
+    await user.keyboard('{Tab}{Tab}');
+    expect(hamburger).toHaveFocus();
+    await user.keyboard('{Enter}');
+    expect(container.querySelector('#headerMobile')).toBeInTheDocument();
+    await user.keyboard('{Enter}');
+    expect(container.querySelector('#headerMobile')).not.toBeInTheDocument();
+    await user.keyboard('[Space]');
+    expect(container.querySelector('#headerMobile')).toBeInTheDocument();
+    await user.keyboard('[Space]');
+    expect(container.querySelector('#headerMobile')).not.toBeInTheDocument();
+
+    await user.keyboard('{Tab}');
+    expect(container.querySelector('#headerMobile')).not.toBeInTheDocument();
   });
 });
