f34c0c69e8583f0c192ee0e0617e0a6eb3150520
srpurba <srpurba@myseneca.ca>
2022-10-22 03:03:05 -0400

    eslint fixes

--- client/app/utils/__tests__/index.test.js
+++ client/app/utils/__tests__/index.test.js
@@ -1,13 +1,13 @@
-import { randomString, setCsrfToken, getPusher } from '../index';
 import { JSDOM } from 'jsdom';
+import { randomString, setCsrfToken, getPusher } from '../index';
 
-
-describe('randomString, setCsrfToken, getPusher tests ', () => {
+describe('randomString ', () => {
   it('should return random string', () => {
     expect(typeof randomString()).toBe('string');
   });
+});
 
-
+describe('setCsrfToken', () => {
   it('should be equal to pusher key and cluster when getPusher is called', () => {
     const dom = new JSDOM(`
       <html>
@@ -20,16 +20,18 @@ describe('randomString, setCsrfToken, getPusher tests ', () => {
       </html>
     `);
 
-
-    const metaPusherKey = Array.from(dom.window.document.getElementsByTagName('meta'))
-      .filter((item) => item.getAttribute('name') === 'pusher-key')[0];
-    const metaPusherCluster = Array.from(dom.window.document.getElementsByTagName('meta'))
-      .filter((item) => item.getAttribute('name') === 'pusher-cluster')[0];
+    const metaPusherKey = Array.from(
+      dom.window.document.getElementsByTagName('meta'),
+    ).filter((item) => item.getAttribute('name') === 'pusher-key')[0];
+    const metaPusherCluster = Array.from(
+      dom.window.document.getElementsByTagName('meta'),
+    ).filter((item) => item.getAttribute('name') === 'pusher-cluster')[0];
 
     const pusher = getPusher();
 
     expect(pusher.key).toBe(metaPusherKey.getAttribute('content'));
-    expect(pusher.config.cluster).toBe(metaPusherCluster.getAttribute('content'));
-
-  }
-}
+    expect(pusher.config.cluster).toBe(
+      metaPusherCluster.getAttribute('content'),
+    );
+  });
+});
