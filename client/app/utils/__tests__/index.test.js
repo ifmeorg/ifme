b73134df6c2954cb232c3c811973c8f535cd3032
srpurba <srpurba@myseneca.ca>
2022-10-24 02:54:41 -0400

    fix:Utils test - usage of imports

--- client/app/utils/__tests__/index.test.js
+++ client/app/utils/__tests__/index.test.js
@@ -1,20 +1,19 @@
-const { randomString, getPusher } = require('../index');
+import Utils from '../index';
 
 describe('Utils', () => {
   describe('randomString ', () => {
     it('should return random string', () => {
-      expect(typeof randomString()).toBe('string');
+      expect(typeof Utils.randomString()).toBe('string');
     });
   });
 
   describe('getPusher', () => {
     it('should return null if window.Pusher is not defined', () => {
-      const pusher = getPusher();
-      expect(pusher).toBe(null);
+      expect(Utils.getPusher()).toBe(null);
     });
     it('should return new window.Pusher if window.Pusher is defined', () => {
       window.Pusher = jest.fn();
-      const pusher = getPusher();
+      const pusher = Utils.getPusher();
       expect(pusher).toBeInstanceOf(window.Pusher);
     });
   });
--- client/app/utils/index.js
+++ client/app/utils/index.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 import renderHTML from 'react-render-html';
 import { sanitize } from 'dompurify';
 
-export const randomString = (): string => Math.random().toString(36).substring(2, 15)
+const randomString = (): string => Math.random().toString(36).substring(2, 15)
   + Math.random().toString(36).substring(2, 15);
 
-export const setCsrfToken = (): void => {
+const setCsrfToken = (): void => {
   const tokenDom = document.querySelector('meta[name=csrf-token]');
   if (tokenDom) {
     const csrfToken = tokenDom.getAttribute('content');
@@ -14,7 +14,7 @@ export const setCsrfToken = (): void => {
   }
 };
 
-export const getPusher = (): Object | null => {
+const getPusher = (): Object | null => {
   if (window.Pusher) {
     const metaPusherKey = Array.from(
       window.document.getElementsByTagName('meta'),
@@ -29,7 +29,7 @@ export const getPusher = (): Object | null => {
   return null;
 };
 
-export const renderContent = (content: string | any): any => {
+const renderContent = (content: string | any): any => {
   if (typeof content === 'string') {
     return renderHTML(sanitize(content));
   }
