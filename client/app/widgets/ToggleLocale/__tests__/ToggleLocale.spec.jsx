966860b9cf1d7a9185923dea760ad81999abe4c1
Julia Nguyen <tojulianguyen@gmail.com>
2024-11-24 23:39:02 -0800

    Fix Axios mocks in ToggleLocale.spec.jsx

--- client/app/widgets/ToggleLocale/__tests__/ToggleLocale.spec.jsx
+++ client/app/widgets/ToggleLocale/__tests__/ToggleLocale.spec.jsx
@@ -13,20 +13,21 @@ describe('ToggleLocale', () => {
   beforeEach(() => {
     jest.spyOn(Cookies, 'get').mockImplementation(() => 'en');
     jest.spyOn(Cookies, 'set');
-    jest.spyOn(axios, 'post').mockImplementation(() => Promise.resolve());
   });
 
   it('does nothing if the previous locale is the same as the selected', async () => {
+    const axiosPostSpy = jest.spyOn(axios, 'post').mockImplementation(() => Promise.resolve());
     render(component);
     await userEvent.selectOptions(screen.getByRole('combobox'), 'en');
-    expect(axios.post).not.toHaveBeenCalled();
+    expect(axiosPostSpy).not.toHaveBeenCalled();
   });
 
   it('sets the locale cookie and makes a post request if the selected locale is different from the previous', async () => {
+    const axiosPostSpy = jest.spyOn(axios, 'post').mockImplementation(() => Promise.resolve());
     render(component);
     await userEvent.selectOptions(screen.getByRole('combobox'), 'es');
     expect(Cookies.set).toHaveBeenCalledWith('locale', 'es');
-    expect(axios.post).toHaveBeenCalledWith('/toggle_locale', {
+    expect(axiosPostSpy).toHaveBeenCalledWith('/toggle_locale', {
       locale: 'es',
     });
   });
